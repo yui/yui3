c01dd2632b83cbdd3b61811a171b72d596a5d5ff
Adam Moore <adamoore@yahoo-inc.com>
2008-01-30 21:49:18 +0000

    .

--- util/yui/src/js/Core.js
+++ util/yui/src/js/Core.js
@@ -322,7 +322,9 @@
         Y.bind = function(f, c) {
             var a = Y.array(arguments, 2, true);
             return function () {
+                // @todo bind args first, or function args first?
                 return f.apply(c || f, a.concat(Y.array(arguments, 0, true)));
+                //return f.apply(c || f, Y.array(arguments, 0, true).concat(a));
             };
         };
 
