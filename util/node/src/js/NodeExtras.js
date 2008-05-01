80762804672c29f4c9c5fc5de6dec2fae638be44
Matt Sweeney <msweeney@yahoo-inc.com>
2008-05-01 18:29:52 +0000

    now using getters/setters/methods to extend Node/NodeList

--- util/node/src/js/NodeExtras.js
+++ util/node/src/js/NodeExtras.js
@@ -12,6 +12,19 @@ YUI.add('nodeextras', function(Y) {
 
     Y.use('node');
 
+    var CLASS_NAME = 'className',
+        OFFSET_TOP = 'offsetTop',
+        POSITION = 'position',
+        FIXED = 'fixed',
+        RELATIVE = 'relative',
+        LEFT = 'left',
+        TOP = 'top',
+        NODE_TYPE = 'nodeType',
+        OFFSET_LEFT = 'offsetLeft',
+        GET_BOUNDING_CLIENT_RECT = 'getBoundingClientRect',
+        CONTAINS = 'contains',
+        COMPARE_DOCUMENT_POSITION = 'compareDocumentPosition';
+
     var regexCache = {};
     var getRegExp = function(str, flags) {
         flags = flags || '';
@@ -21,16 +34,36 @@ YUI.add('nodeextras', function(Y) {
         return regexCache[str + flags];
     };
 
-    var NodeExtras = {
+    Y.Node.getters({
+        /**
+         * Normalizes nodeInnerText and textContent. 
+         * @property text
+         * @type String
+         */
+        'text': function(node) {
+            return node.get('innerText') || node.get('textContent') || '';
+        },
+
+        /**
+         * A NodeList containing only HTMLElement child nodes 
+         * @property children
+         * @type NodeList
+         */
+        'children': function(node) {
+            return node.queryAll('> *');
+        }
+    });
+
+    Y.Node.methods({
         /**
          * Determines whether an HTMLElement has the given className.
          * @method hasClass
          * @param {String} className the class name to search for
          * @return {Boolean | Array} A boolean value or array of boolean values
          */
-        hasClass: function(className) {
+        hasClass: function(node, className) {
             var re = getRegExp('(?:^|\\s+)' + className + '(?:\\s+|$)');
-            return re.test(this.get('className'));
+            return re.test(node.get(CLASS_NAME));
         },
 
         /**
@@ -39,14 +72,14 @@ YUI.add('nodeextras', function(Y) {
          * @param {String} className the class name to add to the class attribute
          * @return {Boolean | Array} A pass/fail boolean or array of booleans
          */
-        addClass: function(className) {
-            if (this.hasClass(className)) {
+        addClass: function(node, className) {
+            if (node.hasClass(className)) {
                 return; // already present
             }
             
             //Y.log('addClass adding ' + className, 'info', 'Node');
             
-            this.set('className', Y.lang.trim([this.get('className'), className].join(' ')));
+            node.set(CLASS_NAME, Y.lang.trim([node.get(CLASS_NAME), className].join(' ')));
         },
 
         /**
@@ -55,19 +88,19 @@ YUI.add('nodeextras', function(Y) {
          * @param {String} className the class name to remove from the class attribute
          * @return {Boolean | Array} A pass/fail boolean or array of booleans
          */
-        removeClass: function(className) {
-            if (!className || !this.hasClass(className)) {
+        removeClass: function(node, className) {
+            if (!className || !node.hasClass(className)) {
                 return; // not present
             }                 
 
             //Y.log('removeClass removing ' + className, 'info', 'Node');
             
-            this.set('className',
-                    Y.lang.trim(this.get('className').replace(getRegExp('(?:^|\\s+)' +
+            node.set(CLASS_NAME,
+                    Y.lang.trim(node.get(CLASS_NAME).replace(getRegExp('(?:^|\\s+)' +
                             className + '(?:\\s+|$)'), ' ')));
 
-            if ( this.hasClass(className) ) { // in case of multiple adjacent
-                this.removeClass(className);
+            if ( node.hasClass(className) ) { // in case of multiple adjacent
+                node.removeClass(className);
             }
         },
 
@@ -79,37 +112,36 @@ YUI.add('nodeextras', function(Y) {
          * @param {String} newClassName the class name that will be replacing the old class name
          * @return {Boolean | Array} A pass/fail boolean or array of booleans
          */
-        replaceClass: function(newC, oldC) {
+        replaceClass: function(node, newC, oldC) {
             //Y.log('replaceClass replacing ' + oldC + ' with ' + newC, 'info', 'Node');
         
-            if ( !this.hasClass(oldC) ) {
-                this.addClass(newC); // just add it if nothing to replace
+            if ( !node.hasClass(oldC) ) {
+                node.addClass(newC); // just add it if nothing to replace
                 return; // NOTE: return
             }
         
             var re = getRegExp('(?:^|\\s+)' + oldC + '(?:\\s+|$)');
-            this.set('className', this.get('className').replace(re, ' ' + newC + ' '));
+            node.set(CLASS_NAME, node.get(CLASS_NAME).replace(re, ' ' + newC + ' '));
 
-            if ( this.hasClass(oldC) ) { // in case of multiple adjacent
-                this.replaceClass(oldC, newC);
+            if ( node.hasClass(oldC) ) { // in case of multiple adjacent
+                node.replaceClass(oldC, newC);
             }
 
-            this.set('className', Y.lang.trim(this.get('className'))); // remove any trailing spaces
+            node.set(CLASS_NAME, Y.lang.trim(node.get(CLASS_NAME))); // remove any trailing spaces
         },
 
         /**
          * Returns the previous sibling that is an HTMLElement. 
          * Returns the nearest HTMLElement sibling if no method provided.
-         * @method getPreviousSiblingBy
+         * @method previous
          * @param {Function} method A boolean function used to test siblings
          * that receives the sibling node being tested as its only argument
          * @return {Node} Node instance or null if not found
          */
-        previousSibling: function(method) {
-            var node = this;
+        previous: function(node, method) {
             while (node) {
                 node = node.get('previousSibling');
-                if ( node && node.get('nodeType') === 1 ) {
+                if ( node && node.get(NODE_TYPE) === 1 ) {
                     return node;
                 }
             }
@@ -119,16 +151,15 @@ YUI.add('nodeextras', function(Y) {
         /**
          * Returns the next HTMLElement sibling that passes the boolean method. 
          * Returns the nearest HTMLElement sibling if no method provided.
-         * @method getNextSiblingBy
+         * @method next
          * @param {Function} method A boolean function used to test siblings
          * that receives the sibling node being tested as its only argument
          * @return {Object} HTMLElement or null if not found
          */
-        nextSibling: function(method) {
-            var node = this;
+        next: function(node, method) {
             while (node) {
                 node = node.get('nextSibling');
-                if ( node && node.get('nodeType') === 1 ) {
+                if ( node && node.get(NODE_TYPE) === 1 ) {
                     return node;
                 }
             }
@@ -141,12 +172,12 @@ YUI.add('nodeextras', function(Y) {
          * @param {String | HTMLElement} needle The possible descendent
          * @return {Boolean} Whether or not this node is an ancestor of needle
          */
-        contains: function(needle) {
-            needle = Y.Doc.get(needle);
-            if (this.hasMethod('contains'))  {
-                return this.invoke('contains', needle);
-            } else if ( this.hasMethod('compareDocumentPosition') ) { // gecko
-                return !!(this.invoke('compareDocumentPosition', needle) & 16);
+        contains: function(node, needle) {
+            needle = Y.Node.get(needle);
+            if (node.hasMethod(CONTAINS))  {
+                return node.invoke(CONTAINS, needle);
+            } else if ( node.hasMethod(COMPARE_DOCUMENT_POSITION) ) { // gecko
+                return !!(node.invoke(COMPARE_DOCUMENT_POSITION, needle) & 16);
             }
         },
 
@@ -160,34 +191,32 @@ YUI.add('nodeextras', function(Y) {
          TODO: test inDocument/display
          */
         getXY: function() {
-            if (Y.Doc.get().get('documentElement').getBoundingClientRect) {
-                return function() {
-                    var doc = this.get('ownerDocument'),
-                        body = doc.get('body'),
-                        scrollLeft = Math.max(doc.get('scrollLeft'), body.get('scrollLeft')),
-                        scrollTop = Math.max(doc.get('scrollTop'), body.get('scrollTop')),
-                        box = this.invoke('getBoundingClientRect'),
-                        xy = [box.left, box.top];
+            if (Y.Node.get('document').get('documentElement').hasMethod(GET_BOUNDING_CLIENT_RECT)) {
+                return function(node) {
+                    var scrollLeft = node.get('docScrollX');
+                        scrollTop = node.get('docScrollY');
+                        box = node.invoke(GET_BOUNDING_CLIENT_RECT),
+                        xy = [box[LEFT], box[TOP]];
 
-                    if ((scrollTop || scrollLeft) && this.getStyle('position') != 'fixed') { // no scroll accounting for fixed
+                    if ((scrollTop || scrollLeft) && node.getStyle(POSITION) != FIXED) { // no scroll accounting for fixed
                         xy[0] += scrollLeft;
                         xy[1] += scrollTop;
                     }
                     return xy;
                 };
             } else {
-                return function(xy) { // manually calculate by crawling up offsetParents
-                    var xy = [this.get('offsetLeft'), this.get('offsetTop')];
+                return function(node) { // manually calculate by crawling up offsetParents
+                    var xy = [node.get(OFFSET_LEFT), node.get(OFFSET_TOP)];
 
-                    var parentNode = this;
+                    var parentNode = node;
                     while (parentNode = parentNode.get('offsetParent')) {
-                        xy[0] += parentNode.get('offsetLeft');
-                        xy[1] += parentNode.get('offsetTop');
+                        xy[0] += parentNode.get(OFFSET_LEFT);
+                        xy[1] += parentNode.get(OFFSET_TOP);
                     }
 
                     // account for any scrolled ancestors
-                    if (this.getStyle('position') != 'fixed') {
-                        parentNode = this;
+                    if (node.getStyle(POSITION) != FIXED) {
+                        parentNode = node;
                         var scrollTop, scrollLeft;
 
                         while (parentNode = parentNode.get('parentNode')) {
@@ -213,54 +242,52 @@ YUI.add('nodeextras', function(Y) {
          * @param {Array} xy Contains X & Y values for new position (coordinates are page-based)
          * @param {Boolean} noRetry By default we try and set the position a second time if the first fails
          */
-        setXY: function(xy, noRetry) {
-            var pos = this.getStyle('position'),
+        setXY: function(node, xy, noRetry) {
+            var pos = node.getStyle(POSITION),
                 delta = [ // assuming pixels; if not we will have to retry
-                    parseInt( this.getStyle('left'), 10 ),
-                    parseInt( this.getStyle('top'), 10 )
+                    parseInt( node.getStyle(LEFT), 10 ),
+                    parseInt( node.getStyle(TOP), 10 )
                 ];
         
             if (pos == 'static') { // default to relative
-                pos = 'relative';
-                this.setStyle('position', pos);
+                pos = RELATIVE;
+                node.setStyle(POSITION, RELATIVE);
             }
 
-            var currentXY = this.getXY();
+            var currentXY = node.getXY();
             if (currentXY === false) { // has to be part of doc to have xy
                 YAHOO.log('xy failed: node not available', 'error', 'Node');
                 return false; 
             }
             
             if ( isNaN(delta[0]) ) {// in case of 'auto'
-                delta[0] = (pos == 'relative') ? 0 : this.get('offsetLeft');
+                delta[0] = (pos == RELATIVE) ? 0 : node.get(OFFSET_LEFT);
             } 
             if ( isNaN(delta[1]) ) { // in case of 'auto'
-                delta[1] = (pos == 'relative') ? 0 : this.get('offsetTop');
+                delta[1] = (pos == RELATIVE) ? 0 : node.get(OFFSET_TOP);
             } 
 
             if (pos[0] !== null) {
-                this.setStyle('left', xy[0] - currentXY[0] + delta[0] + 'px');
+                node.setStyle(LEFT, xy[0] - currentXY[0] + delta[0] + 'px');
             }
 
             if (pos[1] !== null) {
-                this.setStyle('top', xy[1] - currentXY[1] + delta[1] + 'px');
+                node.setStyle(TOP, xy[1] - currentXY[1] + delta[1] + 'px');
             }
           
             if (!noRetry) {
-                var newXY = this.getXY();
+                var newXY = node.getXY();
 
                 // if retry is true, try one more time if we miss 
                if ( (xy[0] !== null && newXY[0] != xy[0]) || 
                     (xy[1] !== null && newXY[1] != xy[1]) ) {
-                   this.setXY(xy, true);
+                   node.setXY(xy, true);
                }
             }        
 
             Y.log('setXY setting position to ' + xy, 'info', 'Node');
         }
     
-    };
-
-    Y.mix(Y.Node, NodeExtras, 0, null, 4);
+    });
 
 }, '3.0.0');
