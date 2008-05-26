56f92cc2c0c6f3297a8e5c3902ce3f914014bba9
Adam Moore <adamoore@yahoo-inc.com>
2008-05-26 22:32:09 +0000

    .

---
+++ src/yui/Do.js
@@ -0,0 +1,177 @@
+
+YUI.add("aop", function(Y) {
+
+    var BEFORE = 0,
+        AFTER = 1;
+
+    Y.Do = {
+
+        objs: {},
+
+        // if 'c' context is supplied, apply remaining args to callback
+        before: function(fn, obj, sFn, c) {
+            var f = fn;
+            if (c) {
+                var a = [fn, c].concat(Y.array(arguments, 4, true));
+                f = Y.bind.apply(Y, a);
+            }
+            this._inject(BEFORE, f, obj, sFn);
+        },
+
+        // if 'c' context is supplied, apply remaining args to callback
+        after: function(fn, obj, sFn, c) {
+            var f = fn;
+            if (c) {
+                var a = [fn, c].concat(Y.array(arguments, 4, true));
+                f = Y.bind.apply(Y, a);
+            }
+            this._inject(AFTER, f, obj, sFn);
+        },
+
+        _inject: function(when, fn, obj, sFn) {
+
+            // object id
+            var id = Y.stamp(obj);
+
+            if (! this.objs[id]) {
+                // create a map entry for the obj if it doesn't exist
+                this.objs[id] = {};
+            }
+            var o = this.objs[id];
+
+            if (! o[sFn]) {
+                // create a map entry for the method if it doesn't exist
+                o[sFn] = new Y.Do.Method(obj, sFn);
+
+                // re-route the method to our wrapper
+                obj[sFn] = 
+                    function() {
+                        return o[sFn].exec.apply(o[sFn], arguments);
+                    };
+            }
+
+            // subscriber id
+            var sid = id + Y.stamp(fn) + sFn;
+
+            // register the callback
+            o[sFn].register(sid, fn, when);
+
+            return sid;
+
+        },
+
+        detach: function(sid) {
+            if (sid in this.before) {
+                delete this.before[id];
+            }
+            if (sid in this.after) {
+                delete this.after[id];
+            }
+        },
+
+        _unload: function(e, me) {
+
+        }
+    };
+
+    //////////////////////////////////////////////////////////////////////////
+
+    Y.Do.Method = function(obj, sFn) {
+        this.obj = obj;
+        this.methodName = sFn;
+        this.method = obj[sFn];
+        // this.before = [];
+        // this.after = [];
+        this.before = {};
+        this.after = {};
+    };
+
+    Y.Do.Method.prototype.register = function (sid, fn, when) {
+        if (when) {
+            // this.after.push(fn);
+            this.after[sid] = fn;
+        } else {
+            // this.before.push(fn);
+            this.before[sid] = fn;
+        }
+    };
+
+    Y.Do.Method.prototype.exec = function () {
+
+        var args = Y.array(arguments, 0, true), i, ret, newRet;
+
+        // for (i=0; i<this.before.length; ++i) {
+        for (i in this.before) {
+            ret = this.before[i].apply(this.obj, args);
+
+            // Stop processing if an Error is returned
+            if (ret && ret.constructor == Y.Do.Error) {
+                // this.logger.debug("Error before " + this.methodName + 
+                //      ": " ret.msg);
+                return ret.retVal;
+            // Check for altered arguments
+            } else if (ret && ret.constructor == Y.Do.AlterArgs) {
+                // this.logger.debug("Params altered before " + 
+                //      this.methodName + ": " ret.msg);
+                args = ret.newArgs;
+            }
+        }
+
+        // execute method
+        ret = this.method.apply(this.obj, args);
+
+        // execute after methods.
+        // for (i=0; i<this.after.length; ++i) {
+        for (i in this.after) {
+            newRet = this.after[i].apply(this.obj, args);
+            // Stop processing if an Error is returned
+            if (newRet && newRet.constructor == Y.Do.Error) {
+                // this.logger.debug("Error after " + this.methodName + 
+                //      ": " ret.msg);
+                return newRet.retVal;
+            // Check for a new return value
+            } else if (newRet && newRet.constructor == Y.Do.AlterReturn) {
+                // this.logger.debug("Return altered after " + 
+                //      this.methodName + ": " newRet.msg);
+                ret = newRet.newRetVal;
+            }
+        }
+
+        return ret;
+    };
+
+    //////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Return an Error object when you want to terminate the execution
+     * of all subsequent method calls
+     */
+    Y.Do.Error = function(msg, retVal) {
+        this.msg = msg;
+        this.retVal = retVal;
+    };
+
+    /**
+     * Return an AlterArgs object when you want to change the arguments that
+     * were passed into the function.  An example would be a service that scrubs
+     * out illegal characters prior to executing the core business logic.
+     */
+    Y.Do.AlterArgs = function(msg, newArgs) {
+        this.msg = msg;
+        this.newArgs = newArgs;
+    };
+
+    /**
+     * Return an AlterReturn object when you want to change the result returned
+     * from the core method to the caller
+     */
+    Y.Do.AlterReturn = function(msg, newRetVal) {
+        this.msg = msg;
+        this.newRetVal = newRetVal;
+    };
+
+    //////////////////////////////////////////////////////////////////////////
+
+// Y["Event"] && Y.Event.addListener(window, "unload", Y.Do._unload, Y.Do);
+
+}, "3.0.0");
