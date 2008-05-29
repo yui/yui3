441c9c47fd2505b17df07fade77220e010c0886a
Satyen Desai <sdesai@yahoo-inc.com>
2008-05-29 01:57:10 +0000

    Updated to use new names - Y.Lang, Y.Object

--- widget/plugins/Mouse.js
+++ widget/plugins/Mouse.js
@@ -1,64 +1,61 @@
-(function() {
+YUI.add("mouseplugin", function(Y) {
 
-    var M = function(Y) {
+    var P = Y.Plugin;
 
-        var P = Y.Plugin;
+    function Mouse(config) {
+        Mouse.superclass.constructor.apply(this, arguments);
+    }
 
-        function Mouse(config) {
-            Mouse.superclass.constructor.apply(this, arguments);
-        }
+    Mouse.EVENTS = [
+        'click',
+        'mouseup',
+        'mousedown',
+        'mouseover',
+        'mouseout',
+        'mousemove',
+        'dblclick'
+    ];
 
-        Mouse.EVENTS = [
-            'click',
-            'mouseup',
-            'mousedown',
-            'mouseover',
-            'mouseout',
-            'mousemove',
-            'dblclick'
-        ];
+    Mouse.NAME = "mouse";
+    Mouse.NS = "mouse";
 
-        Mouse.NAME = "mouse";
-        Mouse.NS = "mouse";
+    // Mouse has No Attributes
+    // Mouse.ATTRS = null;
 
-        // Mouse has No Attributes
-        // Mouse.ATTRS = null;
+    var proto = {
 
-        var proto = {
+        initializer: function(config) {
+            if (!this.owner.rendered) {
+                this.listen(this.owner, "render", Y.bind(this._initUI, this));
+            } else {
+                this._initUI();
+            }
+        },
 
-            initializer: function(config) {
-                if (!this.owner.rendered) {
-                    this.listen(this.owner, "render", Y.bind(this._initUI, this));
-                } else {
-                    this._initUI();
-                }
-            },
+        handler: function(evt) {
+            this.owner.fire(evt.type, evt);
+        },
 
-            handler: function(evt) {
-                this.owner.fire(evt.type, evt);
-            },
-
-            destructor: function() {
-                // If the plugin is being destroyed before render is called
-                if (this._handles) {
-                    for (var i = 0, len = this._handles.length; i < len; ++i) {
-                        this._handles[i].detach();
-                    }
-                }
-            },
-
-            _initUI: function() {
-                var root = this.owner._root;
-                this._handles = [];
-                for (var i = 0, len = Mouse.EVENTS.length; i < len; ++i) {
-                    this._handles.push(Y.on(Mouse.EVENTS[i], Y.bind(this.handler, this), root));
+        destructor: function() {
+            // If the plugin is being destroyed before render is called
+            if (this._handles) {
+                for (var i = 0, len = this._handles.length; i < len; ++i) {
+                    this._handles[i].detach();
                 }
             }
-        };
+        },
 
-        Y.extend(Mouse, P, proto);
-        P.Mouse = Mouse;
+        _initUI: function() {
+            var root = this.owner._root;
+            this._handles = [];
+            for (var i = 0, len = Mouse.EVENTS.length; i < len; ++i) {
+                this._handles.push(Y.on(Mouse.EVENTS[i], Y.bind(this.handler, this), root));
+            }
+        }
     };
 
-    YUI.add("mouseplugin", M, "3.0.0");
-})();
+    Y.extend(Mouse, P, proto);
+    P.Mouse = Mouse;
+
+}, "3.0.0");
+
