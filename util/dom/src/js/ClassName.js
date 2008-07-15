d37db711f1691ecce6f0f363660886a43c536597
Matt Sweeney <msweeney@yahoo-inc.com>
2008-07-15 23:06:36 +0000

    updated pr1

--- util/dom/src/js/ClassName.js
+++ util/dom/src/js/ClassName.js
@@ -1,87 +1,79 @@
+/**
+ * Add className management functionality to DOM.
+ * @class DOMClassName
+ *
+ */
 
-YUI.add('domclassname', function(Y) {
+var CLASS_NAME = 'className';
 
-    var CLASS_NAME = 'className';
+Y.mix(Y.DOM, {
+    /**
+     * Determines whether an HTMLElement has the given className.
+     * @method hasClass
+     * @param {String} className the class name to search for
+     * @return {Boolean | Array} A boolean value or array of boolean values
+     */
+    hasClass: function(node, className) {
+        var re = Y.DOM._getRegExp('(?:^|\\s+)' + className + '(?:\\s+|$)');
+        return re.test(node[CLASS_NAME]);
+    },
 
-    var regexCache = {};
-
-    var getRegExp = function(str, flags) {
-        flags = flags || '';
-        if (!regexCache[str + flags]) {
-            regexCache[str + flags] = new RegExp(str, flags);
+    /**
+     * Adds a class name to a given element or collection of elements.
+     * @method addClass         
+     * @param {String} className the class name to add to the class attribute
+     * @return {Boolean | Array} A pass/fail boolean or array of booleans
+     */
+    addClass: function(node, className) {
+        if (!Y.DOM.hasClass(node, className)) { // skip if already present 
+            node[CLASS_NAME] = Y.Lang.trim([node[CLASS_NAME], className].join(' '));
         }
-        return regexCache[str + flags];
-    };
+    },
 
-    Y.mix(Y.DOM, {
-        /**
-         * Determines whether an HTMLElement has the given className.
-         * @method hasClass
-         * @param {String} className the class name to search for
-         * @return {Boolean | Array} A boolean value or array of boolean values
-         */
-        hasClass: function(node, className) {
-            var re = getRegExp('(?:^|\\s+)' + className + '(?:\\s+|$)');
-            return re.test(node[CLASS_NAME]);
-        },
+    /**
+     * Removes a class name from a given element or collection of elements.
+     * @method removeClass         
+     * @param {String} className the class name to remove from the class attribute
+     * @return {Boolean | Array} A pass/fail boolean or array of booleans
+     */
+    removeClass: function(node, className) {
+        if (className && Y.DOM.hasClass(node, className)) {
+            node[CLASS_NAME] = Y.Lang.trim(node[CLASS_NAME].replace(Y.DOM._getRegExp('(?:^|\\s+)' +
+                            className + '(?:\\s+|$)'), ' '));
 
-        /**
-         * Adds a class name to a given element or collection of elements.
-         * @method addClass         
-         * @param {String} className the class name to add to the class attribute
-         * @return {Boolean | Array} A pass/fail boolean or array of booleans
-         */
-        addClass: function(node, className) {
-            if (!Y.DOM.hasClass(node, className)) { // skip if already present 
-                node[CLASS_NAME] = Y.Lang.trim([node[CLASS_NAME], className].join(' '));
-            }
-        },
-
-        /**
-         * Removes a class name from a given element or collection of elements.
-         * @method removeClass         
-         * @param {String} className the class name to remove from the class attribute
-         * @return {Boolean | Array} A pass/fail boolean or array of booleans
-         */
-        removeClass: function(node, className) {
-            if (className && Y.DOM.hasClass(node, className)) {
-                node[CLASS_NAME] = Y.Lang.trim(node[CLASS_NAME].replace(getRegExp('(?:^|\\s+)' +
-                                className + '(?:\\s+|$)'), ' '));
-
-                if ( Y.DOM.hasClass(node, className) ) { // in case of multiple adjacent
-                    Y.DOM.removeClass(node, className);
-                }
-
-                //Y.log('removeClass removing ' + className, 'info', 'Node');
-            }                 
-        },
-
-        /**
-         * Replace a class with another class for a given element or collection of elements.
-         * If no oldClassName is present, the newClassName is simply added.
-         * @method replaceClass  
-         * @param {String} oldClassName the class name to be replaced
-         * @param {String} newClassName the class name that will be replacing the old class name
-         * @return {Boolean | Array} A pass/fail boolean or array of booleans
-         */
-        replaceClass: function(node, oldC, newC) {
-            //Y.log('replaceClass replacing ' + oldC + ' with ' + newC, 'info', 'Node');
-            Y.DOM.addClass(node, newC);
-            Y.DOM.removeClass(node, oldC);
-        },
-
-        /**
-         * If the className exists on the node it is removed, if it doesn't exist it is added.
-         * @method toggleClass  
-         * @param {String} className the class name to be toggled
-         */
-        toggleClass: function(node, className) {
-            if (Y.DOM.hasClass(node, className)) {
+            if ( Y.DOM.hasClass(node, className) ) { // in case of multiple adjacent
                 Y.DOM.removeClass(node, className);
-            } else {
-                Y.DOM.addClass(node, className);
             }
-        }
-    });
 
-}, '3.0.0', { requires: [ 'dom'] });
+            //Y.log('removeClass removing ' + className, 'info', 'Node');
+        }                 
+    },
+
+    /**
+     * Replace a class with another class for a given element or collection of elements.
+     * If no oldClassName is present, the newClassName is simply added.
+     * @method replaceClass  
+     * @param {String} oldClassName the class name to be replaced
+     * @param {String} newClassName the class name that will be replacing the old class name
+     * @return {Boolean | Array} A pass/fail boolean or array of booleans
+     */
+    replaceClass: function(node, oldC, newC) {
+        //Y.log('replaceClass replacing ' + oldC + ' with ' + newC, 'info', 'Node');
+        Y.DOM.addClass(node, newC);
+        Y.DOM.removeClass(node, oldC);
+    },
+
+    /**
+     * If the className exists on the node it is removed, if it doesn't exist it is added.
+     * @method toggleClass  
+     * @param {String} className the class name to be toggled
+     */
+    toggleClass: function(node, className) {
+        if (Y.DOM.hasClass(node, className)) {
+            Y.DOM.removeClass(node, className);
+        } else {
+            Y.DOM.addClass(node, className);
+        }
+    }
+});
+
