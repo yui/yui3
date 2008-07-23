6250c6aa72a0d0b8d9316f86c406866cb390f48c
Satyen Desai <sdesai@yahoo-inc.com>
2008-07-23 04:34:44 +0000

    Reworded intro

--- templates/examples/data/attribute/attribute-event_intro.php
+++ templates/examples/data/attribute/attribute-event_intro.php
@@ -1,1 +1,1 @@
-Attribute change events are one of the main advantages of using attributes to maintain state for your objects. This example shows how you can listen for attribute change events and work with the event payload they recieve. 
+Attribute change events are one of the key benefits of using attributes to maintain state for your objects, instead of regular object properties. This example shows how you can listen for attribute change events and work with the event payload they receive. 
