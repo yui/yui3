41dc01af3ef825644776232b7e76db9a2dad094b
Matt Sweeney <msweeney@yahoo-inc.com>
2008-07-25 18:15:56 +0000

    fixed NodeList style import

--- build/node/node-debug.js
+++ build/node/node-debug.js
@@ -473,7 +473,7 @@ YUI.add('node', function(Y) {
 
             for (var i = 0, len = nodes.length; i < len; ++i) {
                 _nodes[_tmpNode._yuid] = nodes[i];
-                ret = _tmpNode[name].apply(nodes[i], arguments);
+                ret = _tmpNode[name].apply(_tmpNode, arguments);
                 if (ret !== _tmpNode) {
                     a[i] = ret;
                 }
@@ -1071,6 +1071,39 @@ YUI.add('node', function(Y) {
         'toggleClass'
     ]);
 /**
+ * Extended Node interface for managing regions.
+ * @module node-region
+ */
+
+var ATTR = ['region', 'viewportRegion'],
+    getNode = Y.Node.getDOMNode;
+
+Y.each(ATTR, function(v, n) {
+    Y.Node.getters(v, Y.Node.wrapDOMMethod(v));
+});
+
+Y.Node.addDOMMethods([
+    'inViewportRegion'
+]);
+
+// these need special treatment to extract 2nd node arg
+Y.Node.methods({
+    intersect: function(node1, node2, altRegion) {
+        if (node2 instanceof Y.Node) { // might be a region object
+            node2 = getNode(node2);
+        }
+        return Y.DOM.intersect(getNode(node1), node2, altRegion); 
+    },
+
+    inRegion: function(node1, node2, all, altRegion) {
+        if (node2 instanceof Y.Node) { // might be a region object
+            node2 = getNode(node2);
+        }
+        return Y.DOM.inRegion(getNode(node1), node2, all, altRegion); 
+    }
+});
+
+/**
  * This module applies adds support for positioning elements and
  * normalizes window size and scroll detection. 
  * @module node-screen
@@ -1141,39 +1174,6 @@ YUI.add('node', function(Y) {
      */
         'setY'
     ]);
-
-/**
- * Extended Node interface for managing regions.
- * @module node-region
- */
-
-var ATTR = ['region', 'viewportRegion'],
-    getNode = Y.Node.getDOMNode;
-
-Y.each(ATTR, function(v, n) {
-    Y.Node.getters(v, Y.Node.wrapDOMMethod(v));
-});
-
-Y.Node.addDOMMethods([
-    'inViewportRegion'
-]);
-
-// these need special treatment to extract 2nd node arg
-Y.Node.methods({
-    intersect: function(node1, node2, altRegion) {
-        if (node2 instanceof Y.Node) { // might be a region object
-            node2 = getNode(node2);
-        }
-        return Y.DOM.intersect(getNode(node1), node2, altRegion); 
-    },
-
-    inRegion: function(node1, node2, all, altRegion) {
-        if (node2 instanceof Y.Node) { // might be a region object
-            node2 = getNode(node2);
-        }
-        return Y.DOM.inRegion(getNode(node1), node2, all, altRegion); 
-    }
-});
 
 
 
