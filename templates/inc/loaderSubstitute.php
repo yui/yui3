1e916c430930b505f6f1321e17f8421a6e278bca
Adam Moore <adamoore@yahoo-inc.com>
2008-07-29 18:20:35 +0000

    removed all js includes besides yui

--- templates/inc/loaderSubstitute.php
+++ templates/inc/loaderSubstitute.php
@@ -1,18 +1,3 @@
 <link type="text/css" rel="stylesheet" href="<?php echo $buildpath."reset-fonts/reset-fonts.css" ?>" />
 
 <script type="text/javascript" src="<?php echo $buildpath."yui/yui-min.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."loader/loader-min.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."dom/dom-min.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."node/node-min.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."attribute/attribute-min.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."dump/dump-min.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."substitute/substitute-min.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."get/get-min.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."base/base.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."animation/animation.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."io/io-min.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."json/json-min.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."yuitest/yuitest-min.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."profiler/profiler-min.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."dd/dd-dragdrop-all.js" ?>"></script>
-<script type="text/javascript" src="<?php echo $buildpath."queue/queue-min.js" ?>"></script>
