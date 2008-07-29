e837e9b4ec933adca6a3de024a8b4f9dfcefa744
Adam Moore <adamoore@yahoo-inc.com>
2008-07-29 22:08:28 +0000

    seed update

--- templates/examples/data/event/event-simple_source.php
+++ templates/examples/data/event/event-simple_source.php
@@ -2,39 +2,37 @@
     
 <script language="javascript">
 
-(function() {
+YUI().use('node', function(Y) {
+        
+    //A function that pops up a "Hello World" alert:
+    var helloWorld = function(e) {
+        Y.log("helloWorld function firing.", "info", "example");
+        alert("Hello World!");
+    }
 
-var Y = YUI();
-	
-//A function that pops up a "Hello World" alert:
-var helloWorld = function(e) {
-	Y.log("helloWorld function firing.", "info", "example");
-	alert("Hello World!");
-}
+    //subscribe the helloWorld function as an event
+    //handler for the click event on the container
+    //div:
+    Y.on("click", helloWorld, "#container");
 
-//subscribe the helloWorld function as an event
-//handler for the click event on the container
-//div:
-Y.on("click", helloWorld, "#container");
+    //A function that pops up an alert and
+    //prevents the default behavior of the event
+    //for which it is a handler:
+    var interceptLink = function(e) {
+        e.preventDefault();
+        Y.log("You clicked on the second YUI link.", "info", "example");
+        alert("You clicked on the second YUI link.  Because *preventDefault* was called, this link will not navigate away from the page.");
+    }
 
-//A function that pops up an alert and
-//prevents the default behavior of the event
-//for which it is a handler:
-var interceptLink = function(e) {
-	e.preventDefault();
-	Y.log("You clicked on the second YUI link.", "info", "example");
-	alert("You clicked on the second YUI link.  Because *preventDefault* was called, this link will not navigate away from the page.");
-}
+    //subscribe our interceptLink function
+    //as a click handler for the second anchor
+    //element:
+    Y.on("click", interceptLink, "#secondA");
 
-//subscribe our interceptLink function
-//as a click handler for the second anchor
-//element:
-Y.on("click", interceptLink, "#secondA");
+    //log message to indicate that the example is ready:
+    Y.log("When you begin interacting with the example at left, you'll see log messages appear here.", "info", "example");
+});
 
-//log message to indicate that the example is ready:
-Y.log("When you begin interacting with the example at left, you'll see log messages appear here.", "info", "example");
-
-})();
 </script>
 
 <div id="container">
