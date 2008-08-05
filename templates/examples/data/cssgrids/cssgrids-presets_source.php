f1dfbe79ce63e825f8e4b0d22c897d7625d6e080
Nate Koechley <natek@yahoo-inc.com>
2008-08-05 07:43:09 +0000

    new grids examples

---
+++ templates/examples/data/cssgrids/cssgrids-presets_source.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>YUI 3.x: CSS Grids Page Width Example</title>
+    <link rel="stylesheet" href="<?php echo $buildDirectory ?>cssreset/reset.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo $buildDirectory ?>cssfonts/fonts.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo $buildDirectory ?>cssgrids/grids.css" type="text/css">
+</head>
+
+<body>
+<div class="d3 yui-t1">					
+	<div class="yui-main">
+		<div class="yui-b">
+			<p>.yui-main .yui-b Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+	<div class="yui-b">
+		<p>.yui-b Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adborum.</p>
+	</div>
+</div>
+<div class="d3 yui-t2">					
+	<div class="yui-main">
+		<div class="yui-b">
+			<p>.yui-main .yui-b Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+	<div class="yui-b">
+		<p>.yui-b Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimit anim id est laborum.</p>
+	</div>
+</div>
+<div class="d3 yui-t3">					
+	<div class="yui-main">
+		<div class="yui-b">
+			<p>.yui-main .yui-b Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+	<div class="yui-b">
+		<p>.yui-b Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad id est laborum.</p>
+	</div>
+</div>
+<div class="d3 yui-t4">					
+	<div class="yui-main">
+		<div class="yui-b">
+			<p>.yui-main .yui-b Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+	<div class="yui-b">
+		<p>.yui-b Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adm id est laborum.</p>
+	</div>
+</div>
+<div class="d3 yui-t5">					
+	<div class="yui-main">
+		<div class="yui-b">
+			<p>.yui-main .yui-b Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+	<div class="yui-b">
+		<p>.yui-b Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minilaborum.</p>
+	</div>
+</div>
+<div class="d3 yui-t6">					
+	<div class="yui-main">
+		<div class="yui-b">
+			<p>.yui-main .yui-b Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+	<div class="yui-b">
+		<p>.yui-b Loxcepteur sint occaecat cupidatat non proident, sunt in culpa qui n culpa qui n culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</div>
+
+</body>
+</html>
