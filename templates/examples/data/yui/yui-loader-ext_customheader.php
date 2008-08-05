86131289549e23b19947741e11cfea6e8a8d029d
Satyen Desai <sdesai@yahoo-inc.com>
2008-08-05 23:28:35 +0000

    Added css to fight back yui.css table/tr/th rules

--- templates/examples/data/yui/yui-loader-ext_customheader.php
+++ templates/examples/data/yui/yui-loader-ext_customheader.php
@@ -4,11 +4,29 @@
      correct order. -->
 <style type="text/css" id="styleoverrides">
 #cal1Cont {
-    background-color:#8DD5E7;
+    background-color:#004C6D;
 }
+
 #cal1Cont div.calheader {
     cursor: move;
 }
+
+#cal1Cont .yui-calendar {
+    width:auto;
+    background-color:transparent;
+}
+
+#cal1Cont .yui-calendar tr, #cal1Cont .yui-calendar th {
+    background-color:transparent;
+    vertical-align:middle;
+    text-transform:none;
+    color:#fff;
+}
+
+#cal1Cont .yui-calendar a:hover {
+    text-decoration:none;
+}
+
 #results {
     background-color:#8DD5E7;
     border:1px solid black;
