8dd1c2919cb16ee5def853c1837f46a23fdcf471
Satyen Desai <sdesai@yahoo-inc.com>
2008-08-05 19:52:01 +0000

    Attribute example proof pass

--- templates/examples/data/attribute/attribute-rw_description.php
+++ templates/examples/data/attribute/attribute-rw_description.php
@@ -1,8 +1,8 @@
 <h3>ReadOnly And WriteOnce</h3>
 
-<p>Attribute supports the ability to configure attributes to be <code>readOnly</code> or <code>writeOnce</code>. <code>readOnly</code> attributes cannot be set by the end user, either through initial values passed to the constructor, or by invoking the <code>set</code> method. <code>writeOnce</code> attributes can be set by the user, but only once, either during initialization, or through a call to <code>set</code> . Once a value is established for a <code>writeOnce</code> attribute, it cannot be reset to another value by the user.</p>
+<p>Attribute supports the ability to configure attributes to be <code>readOnly</code> or <code>writeOnce</code>. <code>readOnly</code> attributes cannot be set by the end user, either through initial values passed to the constructor, or by invoking the <code>set</code> method. <code>writeOnce</code> attributes on the other hand, can be set by the user, but only once, either during initialization or through a call to <code>set</code>. Once a value is established for a <code>writeOnce</code> attribute, it cannot be reset to another value by the user.</p>
 
-<h3>Setting Up ReadOnly And WriteOnce Attributes</h3>
+<h3>Configuring ReadOnly And WriteOnce Attributes</h3>
 
 <p>This example sets up a custom class, <code>MyClass</code>, with two attributes, <code>foo</code> and <code>bar</code>. <code>foo</code> is configured to be a <code>readOnly</code> attribute, and <code>bar</code> is configured to be a <code>writeOnce</code> attribute:</p>
 
@@ -28,7 +28,7 @@
 
 <h3>Attempting To Set Values</h3>
 
-<p>We first attempt to set values for both attributes in the contructor (used to intialize the attributes) and see that only <code>bar</code>, the <code>writeOnce</code> attribute, gets set to the user provided value:</p>
+<p>We first attempt to set values for both attributes in the constructor (used to intialize the attributes) and see that only <code>bar</code>, the <code>writeOnce</code> attribute, gets set to the user provided value:</p>
 
 <textarea name="code" class="JScript" rows="1" cols="60">
     var o1 = new MyClass({
@@ -44,7 +44,7 @@
     print(o1.get("bar"));
 </textarea>
 
-<p>We then attempt to set values for both attributes using <code>set</code>, and see that niether of the values change:</p>
+<p>We then attempt to set values for both attributes again, using <code>set</code>, and see that niether of the values are modified:</p>
 
 <textarea name="code" class="JScript" rows="1" cols="60">
     print("Try Setting Again", "header");
@@ -60,7 +60,7 @@
 
 <h3>Setting The State Of ReadOnly Values Internally</h3>
 
-<p>Although the user cannot update the value of <code>readOnly</code> attributes, it maybe neccessary for the host object to update it's value interally. The example shows how this can be done, using the private <code>_conf</code> state property on the host:</p>
+<p>Although the user cannot update the value of <code>readOnly</code> attributes, it maybe neccessary for the host object to update it's value internally. The example shows how this can be done, using the private <code>_conf</code> state property on the host:</p>
 
 <textarea name="code" class="JScript" rows="1" cols="60">
     MyClass.prototype.doSomething = function() {
