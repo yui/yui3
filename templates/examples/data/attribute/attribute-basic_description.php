c92ea1ea985d0d4480d251c3f68851f133543a56
Satyen Desai <sdesai@yahoo-inc.com>
2008-08-05 02:35:35 +0000

    Attribute Basic Example proof pass

--- templates/examples/data/attribute/attribute-basic_description.php
+++ templates/examples/data/attribute/attribute-basic_description.php
@@ -2,7 +2,7 @@
 
 <p>In this example, we'll show how you can use the Attribute utility to add managed attributes to your own object classes. Later examples will walk you through listening for attribute change events and some of the more advanced attribute configuration properties.</p>
 
-<h3>Setting Up Our YUI Instance</h3>
+<h3>Creating A YUI Instance</h3>
 <p>Before we get into attribute, a quick note on how we set up the instance of YUI we'll use for the examples. For all of the attribute examples, we'll setup our own instance of the YUI object and download the files we require on demand using the code pattern shown below:</p>
 
 <textarea name="code" class="JScript" cols="60" rows="1">
@@ -21,17 +21,17 @@
 </script>
 </textarea>
 
-<p>The call to <code>YUI({...})</code> will create and return a new instance of the global YUI object for us to use. However this instance does not yet have all the modules we need for the examples. The configuration object passed to <code>YUI()</code> is used to tell it how to download the source files.</p>
+<p>The call to <code>YUI()</code> will create and return a new instance of the global YUI object for us to use. However this instance does not yet have all the modules we need for the examples.</p>
 
-<p>To load the modules, we invoke <code>use()</code> and pass it the list of modules we'd like populated on our new YUI instance - in this case, "node" and "attribute". The YUI instance will pull down the source files for "node" and "attribute" if they don't already exist on the page (which they don't for the example pages). When the source files are done downloading, the callback function which we pass in as the 3rd argument to <code>use()</code> is invoked, and is passed our custom YUI instance, <code>Y</code>, populated with the classes which make up "node" and "attribute".</p>
+<p>To load the modules, we invoke <code>use()</code> and pass it the list of modules we'd like populated on our new YUI instance - in this case, "node" and "attribute". The YUI instance will pull down the source files for "node" and "attribute" if they don't already exist on the page. When the source files are done downloading, the callback function which we pass in as the 3rd argument to <code>use()</code> is invoked, and is passed our custom YUI instance, <code>Y</code>, populated with the classes which make up "node" and "attribute".</p>
 
-<p>This function is where we'll write all our example code.</p>
+<p>This callback function is where we'll write all our example code. By working inside the callback, we don't pollute the global namespace and we're also able to download the files we need on demand, rather than have them be on the page upfront.</p>
 
-<p>By working inside the callback, we don't pollute the global namespace and we're also able to download the files we need on demand, rather than have them be on the page upfront.</p>
+<p><em>NOTE:</em> The configuration object passed to <code>YUI()</code> when creating the instance, is used to speficy where we want the files downloaded from. The API documentation for the <a href="../api/YUI.html">YUI object</a>, provides more information about the configuration options available.</p>
 
-<h3>Setting Up Your Custom Class</h3>
+<h3>Defining Your Custom Class</h3>
 
-<p>The first step in the example is to create the constructor function for the new class, which we want to make into in a managed attribute provider. In our example, this class is called <code>MyClass</code>. We then augment this class with <code>Attribute</code> so that it recieves all of <code>Attribute's</code> methods, supporting managed attributes:</p>
+<p>The first step in the example is to create the constructor function for our new class, to which we want to add attribute support. In our example, this class is called <code>MyClass</code>. We then augment <code>MyClass</code> with the <code>Y.Attribute</code> class, so that it receives all of <code>Attribute's</code> methods:</p>
 
 <textarea name="code" class="JScript" cols="60" rows="1">
 <script type="text/javascript">
@@ -45,9 +45,9 @@
 
 <h3>Adding Attributes To Your Custom Class</h3>
 
-<p>We can now setup any attributes we need for <code>MyClass</code>. We can setup multiple attributes in one call using the using the <code>_initAtts</code> method (a protected method, designed to be used by the augmented class, as opposed to end users of your class). For the basic example we add 3 attributes - <code>foo, bar, and foobar</code>, and provide an initial  <code>value</code> for each. The same object literal we use to provide the value for the attribute, will also be used in the other examples to setup specific properties for the attribute such as <code>readOnly</code>, <code>writeOnce</code>, <code>validator</code> etc..</p>
+<p>We can now setup any attributes we need for <code>MyClass</code>. We can setup multiple attributes in one call using the using the <code>_initAtts</code> method (a protected method, designed to be used by the augmented class, as opposed to end users of your class). For the basic example we add 3 attributes - <code>foo</code>,<code>bar</code>, and <code>foobar</code>, and provide an initial  <code>value</code> for each. The same object literal we use to provide the value for the attribute, will also be used in the other examples to configure specific types of attributes using properties such as <code>readOnly</code>, <code>writeOnce</code>, <code>validator</code> etc..</p>
 
-<p>For convenience (not required), we define the set of attributes which <code>MyClass</code> supports, as a static property on our <code>MyClass</code> constructor. This static property is passed to <code>_initAtts</code> to setup the attributes, as part of <code>MyClass</code> construction. The complete definition for <code>MyClass</code> is shown below:
+<p>For convenience (not required), we define the set of attributes which <code>MyClass</code> supports as a static property on our <code>MyClass</code> constructor. This static property is passed to <code>_initAtts</code> to setup the attributes, as part of <code>MyClass's</code> constructor. The complete definition for <code>MyClass</code> is shown below:
 
 <textarea name="code" class="JScript" cols="60" rows="1">
 <script type="text/javascript">
@@ -78,11 +78,11 @@
 </script>
 </textarea>
 
-<p><strong>NOTE:</strong> The <code>_initAtts</code> method, in addition to the default attribute configurarion, also accepts an object literal (associative array) of name value pairs which can be used to over-ride the default values of the attributes. This is useful for classes which wish to allow the user the set the value of attributes as part of object construction, as shown by the use of the <code>cfg</code> argument above.</p>
+<p><strong>NOTE:</strong> The <code>_initAtts</code> method, in addition to the default attribute configurarion, also accepts an object literal (associative array) of name/value pairs which can be used to over-ride the default values of the attributes. This is useful for classes which wish to allow the user the set the value of attributes as part of object construction, as shown by the use of the <code>cfg</code> argument above.</p>
 
-<h3>Using Attributes On Instances Of MyClass</h3>
+<h3>Using MyClass's Attributes</h3>
 
-<p>Now that we have <code>MyClass</code> defined with a set of attributes it supports, we can get and set attribute values on instances of <code>MyClass</code>:
+<p>Now that we have <code>MyClass</code> defined with a set of attributes it supports, users can get and set attribute values on instances of <code>MyClass</code>:
 
 <textarea name="code" class="JScript" cols="60" rows="1">
 <script type="text/javascript">
@@ -111,7 +111,7 @@
 </script>
 </textarea>
 
-<p>For the second instance that we create in the example we set the values of the attributes, by providing values to the contructor:</p>
+<p>For the second instance that we create in the example we set the values of the attributes, using the contructor configuration argument:</p>
 
 <textarea name="code" class="JScript" cols="60" rows="1">
 <script type="text/javascript">
@@ -123,4 +123,4 @@
 </script>
 </textarea>
 
-<p>So, in this example, we see how we can setup a new class with Attribute support allowing the end user to set/get attribute values on the instances they create.</p>
+<p>So, in this example, we see how we can setup a new class with Attribute support allowing the end user to get and set attribute values on the instances they create.</p>
