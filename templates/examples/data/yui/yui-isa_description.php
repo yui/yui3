bd104dab5ca302e48a9b07cccec075952eb79c65
Adam Moore <adamoore@yahoo-inc.com>
2008-08-07 04:09:29 +0000

    tweak

--- templates/examples/data/yui/yui-isa_description.php
+++ templates/examples/data/yui/yui-isa_description.php
@@ -11,10 +11,10 @@ function(Y) {
 
 <h2>Checking types</h2>
 <p>In this example, we use a few of the type-checking methods available in
-<code>YAHOO.Lang</code> to test various types of data.</p>
+<code>Lang</code> to test various types of data.</p>
 
 <textarea name="code" class="JScript" cols="60" rows="1">
-// Test the input using YAHOO.lang type checking methods
+// Test the input using Y.Lang type checking methods
 var checkType = function (val) {
     return {
         'object'  : Y.Lang.isObject(val),
@@ -25,15 +25,15 @@ var checkType = function (val) {
 </textarea>
 
 <h2>Other type checking methods</h2>
-<p><code>YAHOO.lang</code> currently supports the following type checking methods:</p>
+<p><code>Lang</code> currently supports the following type checking methods:</p>
 <ul>
-    <li><a href="http://developer.yahoo.com/yui/docs/YAHOO.lang.html#isArray">isArray</a></li>
-    <li><a href="http://developer.yahoo.com/yui/docs/YAHOO.lang.html#isBoolean">isBoolean</a></li>
-    <li><a href="http://developer.yahoo.com/yui/docs/YAHOO.lang.html#isFunction">isFunction</a></li>
-    <li><a href="http://developer.yahoo.com/yui/docs/YAHOO.lang.html#isNull">isNull</a></li>
-    <li><a href="http://developer.yahoo.com/yui/docs/YAHOO.lang.html#isNumber">isNumber</a></li>
-    <li><a href="http://developer.yahoo.com/yui/docs/YAHOO.lang.html#isObject">isObject</a></li>
-    <li><a href="http://developer.yahoo.com/yui/docs/YAHOO.lang.html#isString">isString</a></li>
-    <li><a href="http://developer.yahoo.com/yui/docs/YAHOO.lang.html#isUndefined">isUndefined</a></li>
-    <li><a href="http://developer.yahoo.com/yui/docs/YAHOO.lang.html#isValue">isValue</a></li>
+    <li><a href="http://developer.yahoo.com/yui/api/Lang.html#isArray">isArray</a></li>
+    <li><a href="http://developer.yahoo.com/yui/api/Lang.html#isBoolean">isBoolean</a></li>
+    <li><a href="http://developer.yahoo.com/yui/api/Lang.html#isFunction">isFunction</a></li>
+    <li><a href="http://developer.yahoo.com/yui/api/Lang.html#isNull">isNull</a></li>
+    <li><a href="http://developer.yahoo.com/yui/api/Lang.html#isNumber">isNumber</a></li>
+    <li><a href="http://developer.yahoo.com/yui/api/Lang.html#isObject">isObject</a></li>
+    <li><a href="http://developer.yahoo.com/yui/api/Lang.html#isString">isString</a></li>
+    <li><a href="http://developer.yahoo.com/yui/api/Lang.html#isUndefined">isUndefined</a></li>
+    <li><a href="http://developer.yahoo.com/yui/api/Lang.html#isValue">isValue</a></li>
 </ul>
--- templates/examples/data/yui/yui-isa_intro.php
+++ templates/examples/data/yui/yui-isa_intro.php
@@ -1,3 +1,3 @@
 <p>The <a href="http://developer.yahoo.com/yui/yui/">YUI Global Object</a>
-includes several useful type-checking methods in the <code>YAHOO.Lang</code>
+includes several useful type-checking methods in the <code>Lang</code>
 object.  Click the "Check" button in each row to evaluate the data.</p>
