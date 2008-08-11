aaa1c2458d19d0dd11a190685351168ac94c01fa
Matt Sweeney <msweeney@yahoo-inc.com>
2008-08-11 23:48:59 +0000

    rolled in node-class src to avoid dupe submodule

--- src/node/node.js
+++ src/node/node.js
@@ -1,3 +1,5 @@
+YUI.add('node-base', function(Y) {
+
     /**
      * The Node Utility provides a DOM-like interface for interacting with DOM nodes.
      * @module node
@@ -984,3 +986,55 @@
     Y.NodeList = NodeList;
     Y.all = Y.Node.all;
     Y.get = Y.Node.get;
+/**
+ * Extended Node interface for managing classNames.
+ * @module node
+ * @for Node
+ */
+
+    Y.Node.addDOMMethods([
+        /**
+         * Determines whether the node has the given className.
+         * @method hasClass
+         * @param {String} className the class name to search for
+         * @return {Boolean} Whether or not the node has the given class. 
+         */
+        'hasClass',
+
+        /**
+         * Adds a class name to the node.
+         * @method addClass         
+         * @param {String} className the class name to add to the node's class attribute
+         * @chainable
+         */
+        'addClass',
+
+        /**
+         * Removes a class name from the node.
+         * @method removeClass         
+         * @param {String} className the class name to remove from the node's class attribute
+         * @chainable
+         */
+        'removeClass',
+
+        /**
+         * Replace a class with another class.
+         * If no oldClassName is present, the newClassName is simply added.
+         * @method replaceClass  
+         * @param {String} oldClassName the class name to be replaced
+         * @param {String} newClassName the class name that will be replacing the old class name
+         * @chainable
+         */
+        'replaceClass',
+
+        /**
+         * If the className exists on the node it is removed, if it doesn't exist it is added.
+         * @method toggleClass  
+         * @param {String} className the class name to be toggled
+         * @chainable
+         */
+        'toggleClass'
+    ]);
+
+
+}, '@VERSION@' ,{requires:['dom-base', 'selector']});
