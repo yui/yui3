d228c8ff667522e0309dd6e559078d698755ed56
Nate Koechley <natek@yahoo-inc.com>
2008-08-13 06:16:47 +0000

    reordered nav into four sections. new css for blog and group lists on home. correction to delicious mod include.

--- templates/inc/side.php
+++ templates/inc/side.php
@@ -9,19 +9,24 @@ $nav[]=array('docs',$docroot.'api/','YUI 3 API Docs', 'Instantly searchable API 
 $nav[]=array('sourceforge','http://sourceforge.net/projects/yui/','YUI 3 on Sourceforge (external)', 'The YUI Library can be downloaded from SourceForge');
 $nav[]=array('license','http://developer.yahoo.com/yui/3/license.html','YUI License (external)', 'YUI is free and open, offered under a BSD license.');
 
-$nav[]=array('', '', 'YUI 3 Utilities - Examples', ' YUI 3 Utilities - Functional Examples');
+$nav[]=array('', '', 'YUI 3 Core - Examples', ' YUI 3 Core - Functional Examples');
 $nav[]=array('yui',$docroot.'examples/yui/index.html','YUI (Global Prerequisite)', 'YUI (Global Prerequisite) - Functional Examples');
-$nav[]=array('animation',$docroot.'examples/anim/index.html','Animation', 'Animation - Functional Examples');
+$nav[]=array('event',$docroot.'examples/event/index.html','Event', 'Event - Functional Examples');
+$nav[]=array('node',$docroot.'examples/node/index.html','Node', 'Node - Functional Examples');
+
+$nav[]=array('', '', 'YUI 3 Component Infrastructure - Examples', ' YUI 3 Utilities - Functional Examples');
 $nav[]=array('attribute',$docroot.'examples/attribute/index.html','Attribute', 'Attribute - Functional Examples');
 $nav[]=array('base',$docroot.'examples/base/index.html','Base', 'Base - Functional Examples');
+
+$nav[]=array('', '', 'YUI 3 Utilities - Examples', ' YUI 3 Utilities - Functional Examples');
+$nav[]=array('animation',$docroot.'examples/anim/index.html','Animation', 'Animation - Functional Examples');
 $nav[]=array('cookie',$docroot.'examples/cookie/index.html','Cookie', 'Cookie - Functional Examples');
 $nav[]=array('dd',$docroot.'examples/dd/index.html','Drag &amp; Drop', 'Drag &amp; Drop - Functional Examples');
-$nav[]=array('event',$docroot.'examples/event/index.html','Event', 'Event - Functional Examples');
 $nav[]=array('get',$docroot.'examples/get/index.html','Get', 'Get - Functional Examples');
 $nav[]=array('io',$docroot.'examples/io/index.html','IO', 'IO - Functional Examples');
 $nav[]=array('json',$docroot.'examples/json/index.html','JSON', 'JSON (JavaScript Object Notation) - Functional Examples');
-$nav[]=array('node',$docroot.'examples/node/index.html','Node', 'Node - Functional Examples');
 $nav[]=array('queue',$docroot.'examples/queue/index.html','Queue', 'Queue - Functional Examples');
+
 
 $nav[]=array('', '', 'YUI 3 CSS - Examples', 'YUI CSS - Functional Examples');
 $nav[]=array('cssreset',$docroot.'examples/cssreset/index.html','CSS Reset', 'YUI CSS Reset - Functional Examples');
@@ -41,8 +46,7 @@ $nav[]=array('', '', 'YUI Articles on the YUI Website', '');
 $nav[]=array('faq','http://developer.yahoo.com/yui/articles/faq/','YUI FAQ (external)', 'Answers to Frequently Asked Questions about the YUI Library');
 $nav[]=array('gbs','http://developer.yahoo.com/yui/articles/gbs/','Graded Browser Support (external)', 'Yahoo!\'s philosophy of Graded Browser Support');
 $nav[]=array('reportingbugs','http://developer.yahoo.com/yui/articles/reportingbugs/','Bug Reports/Feature Requests (external)', 'Reporting Bugs and Making Feature Requests for YUI Components');
-$nav[]=array('hosting','http://developer.yahoo.com/yui/articles/hosting/','Serving YUI Files from Yahoo! (external)', 'Serve YUI source files from Yahoo! -- free, fast, and simple');
-$nav[]=array('security','http://developer.yahoo.com/security/','Security Best Practices (external)', 'Best practices for working with web services while protecting user privacy');
+$nav[]=array('hosting','http://developer.yahoo.com/yui/3/articles/hosting/','Serving YUI Files from Yahoo! (external)', 'Serve YUI source files from Yahoo! -- free, fast, and simple');
 ?>
 
 <div class="yui-b toc3" id="tocWrapper">
