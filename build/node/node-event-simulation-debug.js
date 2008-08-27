be8f350a6d9d1ecba4653b4d56053327980a1790
Matt Sweeney <msweeney@yahoo-inc.com>
2008-08-27 00:31:55 +0000

    initial build

---
+++ build/node/node-event-simulation-debug.js
@@ -0,0 +1,24 @@
+YUI.add('node-event-simulation', function(Y) {
+
+/*
+ * Functionality to simulate events.
+ * @submodule node-event-simulate
+ * @module node-event
+ */
+
+    /**
+     * Simulates an event on the node.
+     * @param {String} type The type of event to simulate (i.e., "click").
+     * @param {Object} options (Optional) Extra options to copy onto the event object.
+     * @return {void}
+     * @method simulate
+     * @static
+     * @for Y.Node
+     */     
+    Y.Node.prototype.simulate = function(type, options){
+        Y.Event.simulate(Y.Node.getDOMNode(this), type, options);
+    };
+
+
+
+}, '@VERSION@' ,{requires:['node-base']});
