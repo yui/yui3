5c34b88a71b9d26a32eec15fb880ca83e0ad792b
Todd Kloots <kloots@yahoo-inc.com>
2008-11-19 15:07:35 -0800

    Updated comments for API docs

--- src/classnamemanager/ClassNameManager.js
+++ src/classnamemanager/ClassNameManager.js
@@ -1,109 +1,81 @@
 /**
- * @module classnamemanager
- */
+* @module classnamemanager
+*/
+
+// String constants
+var CLASS_NAME_PREFIX = 'classNamePrefix',
+	CLASS_NAME_DELIMITER = 'classNameDelimiter';
+
+
+// Global config
 
 /**
- * A class for Widgets or classes that extend Base, providing: 
+ * Configuration property indicating the prefix for all CSS class names in this YUI instance.
+ *
+ * @property Y.config.classNamePrefix
+ * @type {String}
+ * @default "yui"
+ * @static
+ */
+Y.config[CLASS_NAME_PREFIX] = Y.config[CLASS_NAME_PREFIX] || 'yui';
+
+
+/**
+ * Configuration property indicating the delimiter used to compose all CSS class names in
+ * this YUI instance.
+ *
+ * @property Y.config.classNameDelimiter
+ * @type {String}
+ * @default "-"
+ * @static
+ */
+Y.config[CLASS_NAME_DELIMITER] = Y.config[CLASS_NAME_DELIMITER] || '-';
+
+
+
+// Class definition
+
+/**
+ * A singleton class providing: 
  * 
  * <ul>
  *    <li>Easy creation of prefixed class names</li>
  *    <li>Caching of previously created class names for improved performance.</li>
  * </ul>
- *
- * @class YUI.ClassNameManager
+ * 
+ * @class ClassNameManager
+ * @static 
  */
+Y.ClassNameManager = function () {
 
-if (!Y.CLASS_NAME_PREFIX) {
+	var sPrefix = Y.config[CLASS_NAME_PREFIX],
+		sDelimiter = Y.config[CLASS_NAME_DELIMITER],
+		classNames = {};
 
-	/**
-	 * String indicating the prefix for all CSS class names.
-	 *
-	 * @property YUI.CLASS_NAME_PREFIX
-	 * @type {String}
-	 * @static
-	 */
-	Y.CLASS_NAME_PREFIX = "yui-";
-}
+	return {
 
-// String constants
-var _HYPHEN = "-";
-
-Y.ClassNameManager = function() {
-
-};
-
-var ClassNameManager = Y.ClassNameManager;
-
-
-ClassNameManager.ATTRS = {
-
-	/**
-	* @attribute classNamePrefix
-	* @description String indicating the prefix for all class names.
-	* @default YUI.CLASS_NAME_PREFIX ("yui-")
-	* @type String
-	*/
-	classNamePrefix: {
+		/**
+		 * Returns a class name prefixed with the the value of the 
+		 * <code>Y.config.classNamePrefix</code> attribute + the provided strings.
+		 * Uses the <code>Y.config.classNameDelimiter</code> attribute to delimit the 
+		 * provided strings. E.g. Y.ClassNameManager.getClassName('foo','bar'); // yui-foo-bar
+		 * 
+		 * 
+		 * @method getClassName
+		 * @param {String}+ one or more classname bits to be joined and prefixed
+		 */
+		getClassName: function (c,x) {
 	
-		value: Y.CLASS_NAME_PREFIX,
-		writeOnce: true
-	
-	}
-
-};
-
-
-ClassNameManager.prototype = {
-
-	/**
-	 * The class name for the instance, by default set to the value of the 
-	 * <code>classNamePrefix</code> attribute and the <code>NAME</code> property.
-	 *
-	 * @property _className
-	 * @protected
-	 * @type {String}		 
-	 */            
-	_className: null,
-
-	/**
-	 * Collection of all of the class names used by the instance.
-	 *
-	 * @property _classNames
-	 * @protected
-	 * @type {Object}
-	 */            
-	_classNames: null,
-
-	/**
-	 * Returns a class name prefixed with the both the value of the 
-	 * <code>classNamePrefix</code> attribute and the <code>NAME</code> property.
-	 * 
-	 * @method getClassName
-	 * @param {String} classname
-	 */
-	getClassName: function (classname) {
-
-		if (!this._className) {
-			this._className = this.get("classNamePrefix") + this.constructor.NAME.toLowerCase();
-		}
-
-
-		if (!this._classNames) {
-			this._classNames = {};
-		}
-	
-
-		var oClassNames = this._classNames,
-			sClassName  = oClassNames[classname];
-
-
-		if (!sClassName) {
-			sClassName =  this._className + _HYPHEN + classname;
-			oClassNames[classname] = sClassName;
-		}
+			// Test for multiple classname bits
+			if (x) {
+				c = Y.Array(arguments,0,true).join(sDelimiter);
+			}
 		
-		return sClassName;				
+			// memoize in classNames map
+			return classNames[c] || (classNames[c] = sPrefix + sDelimiter + c);
 	
-	}
+		}
+	
+	};
 
-};
+}();
