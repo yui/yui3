fea1fdeda9ca4bdc5468507167d195fed002f971
Todd Kloots <kloots@yahoo-inc.com>
2008-11-20 13:07:51 -0800

    Updated for API docs

--- src/node-menunav/node-menunav.js
+++ src/node-menunav/node-menunav.js
@@ -1,5 +1,3 @@
-YUI.add('node-menunav', function(Y) {
-
 /**
 * <p>The MenuNav Node Plugin makes it easy to transform existing list-based markup into traditional, 
 * drop down navigational menus that are both accessible and easy to customize, and only require 
@@ -21,7 +19,7 @@ YUI.add('node-menunav', function(Y) {
 * <p>
 * <code>
 * var oMenuNav = Y.Node.get("#productsandservices");<br>
-* oMenuNav.plug(Y.Plugin.NodeMenuNav, { autoSubmenuDisplay: true });
+* oMenuNav.plug(Y.Plugin.NodeMenuNav, { mouseOutHideDelay: 1000 });
 * </code>
 * </p>
 * 
@@ -1893,5 +1891,3 @@ MenuNav.prototype = {
 Y.namespace('Plugin');
 
 Y.Plugin.NodeMenuNav = MenuNav;
-
-}, '@VERSION@' ,{requires:['node', 'classnamemanager']});
