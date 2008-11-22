e00d1e7172b8c22811e869d866be29af50456114
Satyen Desai <sdesai@yahoo-inc.com>
2008-11-21 19:11:24 -0800

    Widget build example, functionality complete

--- templates/examples/data/widget/widget-build_customheader.php
+++ templates/examples/data/widget/widget-build_customheader.php
@@ -1,1 +1,114 @@
-/* TODO */
+<style type="text/css">
+
+/* Standard Module Widget CSS */
+.yui-standardmodule-hidden {
+    display:none;
+}
+
+.yui-standardmodule {
+    padding:5px;
+    border:2px solid #000;
+    background-color:#eee;
+    margin:10px;
+}
+
+.yui-standardmodule-content {
+    padding:3px;
+    border:1px solid #666;
+}
+
+.yui-standardmodule-content .yui-widget-hd {
+    padding:5px;
+    border:2px solid #aa0000;
+    background-color:#fff;
+    overflow:auto;
+}
+
+.yui-standardmodule-content .yui-widget-bd {
+    padding:5px;
+    border:2px solid #0000aa;
+    background-color:#fff;
+    overflow:auto;
+}
+
+.yui-standardmodule-content .yui-widget-ft {
+    padding:5px;
+    border:2px solid #00aa00;
+    background-color:#fff;
+    overflow:auto;
+}
+
+/* Positionable Widget CSS */
+.yui-positionable {
+    position:absolute;
+    border:1px solid #7B7F4F;
+    background-color:#999966;
+    color:#fff;
+}
+
+.yui-positionable-content {
+    padding:5px;
+}
+
+.yui-positionable-hidden {
+    visibility:hidden;
+}
+
+/* Alignable Widget CSS */
+.yui-alignable {
+    position:absolute;
+    border:1px solid #000000;
+    background-color:#004C6D;
+    color:#fff;
+}
+
+.yui-alignable-content {
+    padding:5px;
+}
+
+.yui-alignable-hidden {
+    visibility:hidden;
+}
+
+
+/* Example Layout CSS */
+.widget-build-example {
+    border:1px solid #000;
+    background-color:#eee;
+    padding:5px;
+    margin:10px 0;
+}
+
+.widget-build-example button, .widget-build-example label, .widget-build-example select, .widget-build-example input {
+    margin-right:5px;
+}
+
+.widget-build-example button.fail {
+    color:#cc0000;
+    margin-left:10px;
+}
+
+.widget-build-example .filler {
+    color:#999;
+}
+
+#x, #y {
+    width:3em;
+}
+
+#widget2-example, #widget3-example {
+    height:15em;
+}
+
+#widget2-example select, #widget3-example select {
+    width:100%;
+}
+
+#alignment {
+    font-size:85%;
+}
+
+#alignment p {
+    color:#dddd00;
+}
+</style>
