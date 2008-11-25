8024e024aaff4833dc5a47762b9686aabae2f357
Satyen Desai <sdesai@yahoo-inc.com>
2008-11-24 19:02:12 -0800

    Widget extend Spinner Example, functionality complete

--- templates/examples/data/widget/widget-extend_customheader.php
+++ templates/examples/data/widget/widget-extend_customheader.php
@@ -1,1 +1,49 @@
-/* Widget Extend TODO */
+<style type="text/css">
+
+    .yui-spinner-hidden {
+        display:none;
+    }
+
+    .yui-spinner {
+        display:-moz-inline-stack;
+        display:inline-block;
+        zoom:1;
+        *display:inline;
+    }
+    
+    .yui-spinner-content {
+        padding:1px;
+    }
+
+    .yui-spinner-value {
+        width:2em;
+        height:1.6em;
+        text-align:right;
+        margin-right:0;
+        vertical-align:top;
+        border:1px solid #000;
+    }
+
+    .yui-spinner-increment, .yui-spinner-decrement {
+        width:1.5em;
+        overflow:hidden;
+        text-indent:-10000em;
+        border:1px solid #000;
+        margin:0;
+        height:1.9em;
+        *margin-top:1px;
+    }
+
+    .yui-spinner-increment {
+        background:#ddd url(<?php echo $assetsDirectory?>arrows.png) no-repeat 2px 4px;
+    }
+
+     .yui-spinner-decrement {
+        background:#ddd url(<?php echo $assetsDirectory?>arrows.png) no-repeat 2px -14px;
+     }
+
+    #widget-extend-example {
+        padding:5px;
+    }
+
+</style>
--- templates/examples/data/widget/widget-extend_source.php
+++ templates/examples/data/widget/widget-extend_source.php
@@ -1,5 +1,201 @@
+<div id="widget-extend-example">
+    <div id="numberField"><input type="text" class="yui-spinner-value" value="20" /></div>
+</div>
+
 <script type="text/javascript">
-YUI(<?php echo $yuiConfig ?>).use(<?php echo $requiredModules ?>, function(Y) {
-    // TODO
+var spinner;
+
+YUI(<?php echo getYUIConfig("filter:'raw'") ?>).use(<?php echo $requiredModules ?>, function(Y) {
+
+    var Lang = Y.Lang,
+        Widget = Y.Widget,
+        Node = Y.Node;
+
+    function Spinner(config) {
+        Spinner.superclass.constructor.apply(this, arguments);
+    }
+
+    Spinner.NAME = "spinner";
+
+    Spinner.ATTRS = {
+        min : {
+            value:0
+        },
+
+        max : {
+            value:100
+        },
+
+        value : {
+            value:0,
+            validator: function(val) {
+                return this._validateValue(val);
+            }
+        },
+
+        minorInc : {
+            value:1
+        },
+
+        majorInc : {
+            value:10
+        },
+
+        strings: {
+            value: {
+                increment: "Increment",
+                decrement: "Decrement"
+            }
+        }
+    };
+
+    Spinner.INPUT_CLASS = Y.ClassNameManager.getClassName(Spinner.NAME, "value");
+    Spinner.INPUT_TEMPLATE = '<input type="text" class="' + Spinner.INPUT_CLASS + '">';
+    Spinner.BTN_TEMPLATE = '<button type="button"></button>';
+
+    Spinner.HTML_PARSER = {
+        value: function (contentBox) {
+            var node = contentBox.query("." + Spinner.INPUT_CLASS);
+            return (node) ? parseInt(node.get("value")) : null;
+        }
+    };
+
+    Y.extend(Spinner, Widget, {
+
+        initializer: function() {
+            // Not doing anything special in initialization
+        },
+
+        destructor : function() {
+            // Widget's descructor will purge the boundingBox, 
+            // but if we wanted to we could implement additional 
+            // cleanup code here. e.g.:
+
+            this.inputNode = null;
+            this.incrementNode = null;
+            this.decrementNode = null;
+        },
+
+        renderUI : function() {
+            this._renderInput();
+            this._renderButtons();
+        },
+        
+        bindUI : function() {
+            this.after("valueChange", this._onValueChange);
+
+            var boundingBox = this.get("boundingBox");
+
+            // Looking for a key event which will fire continously across browsers while the key is held down
+            var keyEventSpec = (!Y.UA.opera) ? "down:" : "press:";
+            // 38, 40 = arrow up/down, 33, 34 = page up/down
+            keyEventSpec += "38, 40, 33, 34";
+
+            Y.on("key", Y.bind(this._delegateDirectionKeys, this), boundingBox, keyEventSpec);
+            Y.on("mousedown", Y.bind(this._delegateMouseDown, this), boundingBox);
+        },
+
+        syncUI : function() {
+            this._uiSetValue(this.get("value"));
+        },
+
+        _renderInput : function() {
+            var contentBox = this.get("contentBox");
+            var input = contentBox.query("." + Spinner.INPUT_CLASS);
+
+            if (!input) {
+                input = Node.create(Spinner.INPUT_TEMPLATE);
+                contentBox.appendChild(input);
+            }
+            this.inputNode = input;
+        },
+
+        _renderButtons : function() {
+            var contentBox = this.get("contentBox"),
+                strings = this.get("strings");
+
+            var inc = this._createButton(strings.increment, this.getClassName("increment"));
+            var dec = this._createButton(strings.decrement, this.getClassName("decrement"));
+
+            this.incrementNode = contentBox.appendChild(inc);
+            this.decrementNode = contentBox.appendChild(dec);
+        },
+
+        _createButton : function(text, className) {
+
+            var btn = Y.Node.create(Spinner.BTN_TEMPLATE);
+            btn.set("innerHTML", text);
+            btn.set("title", text);
+            btn.addClass(className);
+
+            return btn;
+        },
+
+        _delegateMouseDown : function(e) {
+            var node = e.target,
+                currVal = this.get("value");
+                minorInc = this.get("minorInc");
+
+            if (node.hasClass(this.getClassName("increment"))) {
+                this.set("value", currVal + minorInc);
+            } else if (node.hasClass(this.getClassName("decrement"))) {
+                this.set("value", currVal - minorInc);
+            }
+        },
+
+        _delegateDirectionKeys : function(e) {
+            var node = e.target;
+
+            e.preventDefault();
+
+            var currVal = this.get("value"),
+                newVal = currVal,
+                minorInc = this.get("minorInc"),
+                majorInc = this.get("majorInc");
+
+            switch (e.charCode) {
+                case 38:
+                    newVal += minorInc;
+                    break;
+                case 40:
+                    newVal -= minorInc;
+                    break;
+                case 33:
+                    newVal += majorInc;
+                    newVal = Math.min(newVal, this.get("max"));
+                    break;
+                case 34:
+                    newVal -= majorInc;
+                    newVal = Math.max(newVal, this.get("min"));
+                    break;
+            }
+
+            if (newVal !== currVal) {
+                this.set("value", newVal);
+            }
+        },
+
+        _onValueChange : function(e) {
+            this._uiSetValue(e.newVal);
+        },
+
+        _uiSetValue : function(val) {
+            this.inputNode.set("value", val);
+        },
+
+        _validateValue: function(val) {
+            var min = this.get("min"),
+                max = this.get("max");
+
+            return (Lang.isNumber(val) && val >= min && val <= max);
+        }
+    });
+
+    spinner = new Spinner({
+        contentBox: "#numberField",
+        max:100,
+        min:0
+    });
+    spinner.render();
 });
 </script>
