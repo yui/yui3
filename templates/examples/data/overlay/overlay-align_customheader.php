caa472bd131dd5997b1fe57b2ca4022a25fbd5f8
Satyen Desai <sdesai@yahoo-inc.com>
2008-11-25 15:15:07 -0800

    Adding overlay-align example

--- templates/examples/data/overlay/overlay-align_customheader.php
+++ templates/examples/data/overlay/overlay-align_customheader.php
@@ -1,1 +1,87 @@
-/* TODO */
+<style type="text/css">
+/* Overlay Look/Feel */
+.yui-overlay-content {
+    padding:2px;
+    border:1px solid #000;
+    background-color:#aaa;
+    font-size:93%;
+}
+
+.yui-overlay-content .yui-widget-hd {
+    font-weight:bold;
+    text-align:center;
+    padding:2px;
+    border:2px solid #aa0000;
+    background-color:#fff;
+}
+
+.yui-overlay-content .yui-widget-bd {
+    text-align:left;
+    padding:2px;
+    border:2px solid #0000aa;
+    background-color:#fff;
+}
+
+/* Example Layout CSS */
+.overlay-example {
+    position:relative;
+    border:1px solid #000;
+    background-color:#eee;
+    padding:5px;
+    height:25em;
+}
+
+.overlay-example button {
+    margin-left:1px;
+}
+
+.overlay-example .filler {
+    color:#999;
+}
+
+.align-box {
+    height:12em;
+    width:12em;
+    border:1px solid #000;
+    margin:0px;
+    text-align:center;
+}
+
+.align-box .title {
+    font-weight:bold;
+    color:#fff;
+    padding:2px;
+}
+
+#align1 {
+    position:static;
+    background-color:#0000cc;
+}
+
+#align2 {
+    position:relative;
+    top:-100px;
+    left:250px;
+    background-color:#00cc00;
+}
+
+#align3 {
+    position:absolute;
+    bottom:130px;
+    right:20px;
+    background-color:#cc0000;
+}
+
+#alignment {
+    margin-left:5px;
+    padding:3px;
+    display:inline;
+    background-color:#fff;
+    border:1px solid black;
+}
+
+#step {
+    font-size:85%;
+    margin-left:5px;
+}
+</style>
--- templates/examples/data/overlay/overlay-align_source.php
+++ templates/examples/data/overlay/overlay-align_source.php
@@ -1,5 +1,72 @@
+<div class="overlay-example" id="overlay-align">
+    <button type="button" id="align">Align Next</button><pre id="alignment"></pre><span id="step"></span>
+    <p class="filler">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc pretium quam eu mi varius pulvinar. Duis orci arcu, ullamcorper sit amet, luctus ut, interdum ac, quam. Pellentesque euismod. Nam tincidunt, purus in ultrices congue, urna neque posuere arcu, aliquam tristique purus sapien id nulla. Etiam rhoncus nulla at leo. Cras scelerisque nisl in nibh. Sed eget odio. Morbi elit elit, porta a, convallis sit amet, rhoncus non, felis. Mauris nulla pede, pretium eleifend, porttitor at, rutrum id, orci. Quisque non urna. Nulla aliquam rhoncus est.</p>
+    <div id="align1" class="align-box"><span class="title">id = #align1</span></div>
+    <div id="align2" class="align-box"><span class="title">id = #align2</span></div>
+    <div id="align3" class="align-box"><span class="title">id = #align3</span></div>
+</div>
+
 <script type="text/javascript">
 YUI(<?php echo $yuiConfig ?>).use(<?php echo $requiredModules ?>, function(Y) {
-    // TODO
+
+    /* Create Overlay from script, this time. No footer */
+    var overlay = new Y.Overlay({
+        width:"10em",
+        height:"10em",
+        headerContent: "Aligned Overlay",
+        bodyContent: "Click the 'Align Next' button to try a new alignment",
+        zIndex:2
+    });
+
+    /* Render it to #overlay-align element */
+    overlay.render("#overlay-align");
+
+    var alignment = Y.Node.get("#alignment");
+    var stepNumber = Y.Node.get("#step");
+
+    var steps = [
+        function() {
+            /* Center in #overlay-align */
+            overlay.set("align", {node:"#overlay-align", points:["cc", "cc"]});
+            alignment.set("innerHTML", 'align: {node:"#overlay-align", points:["cc", "cc"]}');
+        },
+        function() {
+            /* Align top-left corner of overlay, with top-right corner of #align1 */
+            overlay.set("align", {node:"#align1", points:["tl", "tr"]});
+            alignment.set("innerHTML", 'align: {node:"#align1", points:["tl", "tr"]}');
+        },
+        function() {
+            /* Center overlay in #align2 */
+            overlay.set("centered", "#align2");
+            alignment.set("innerHTML", 'centered: "#align2');
+        },
+        function() {
+            /* Align right-center edge of overlay with right-center edge of viewport */
+            overlay.set("align", {points:["rc", "rc"]});
+            alignment.set("innerHTML", 'align: {points:["rc", "rc"]} (viewport)');
+        },
+        function() {
+            /* Center overlay in viewport */
+           overlay.set("centered", true);
+            alignment.set("innerHTML", "centered: true (viewport)");
+        },
+        function() {
+            /* Align top-center edge of overlay with bottom-center edge of #align3 */
+            overlay.set("align", {node:"#align3", points:["tc", "bc"]});
+            alignment.set("innerHTML", 'align: {node:"#align3", points:["tc", "bc"]}');
+        }
+    ];
+
+    var step = 0;
+    var totalSteps = steps.length;
+    function alignNext() {
+        stepNumber.set("innerHTML", "Alignment step " + (step+1) + " of " + totalSteps);
+        steps[step]();
+        step = (++step)%(totalSteps);
+    }
+
+    alignNext();
+
+    Y.on("click", alignNext, "#align");
 });
 </script>
