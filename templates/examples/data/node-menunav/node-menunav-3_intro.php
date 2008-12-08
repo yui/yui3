d6865c8a702655eb0cb0190e7d614d4aeffcf78a
Todd Kloots <kloots@yahoo-inc.com>
2008-12-08 13:57:54 -0800

    Updates

--- templates/examples/data/node-menunav/node-menunav-3_intro.php
+++ templates/examples/data/node-menunav/node-menunav-3_intro.php
@@ -1,4 +1,4 @@
 <p>
 This example demonstrates how to create a traditional, two-column page layout 
-(<a href="../../grids/">using Grids</a>) with top navigation rendered like menu buttons.
+(<a href="../../cssgrids/">using Grids</a>) with top navigation rendered like menu buttons.
 </p>
