e3d4448afe662d1c91cf8786942efb767b95d47d
Jenny Han Donnelly <jennydonnelly@yahoo-inc.com>
2008-12-10 13:41:07 -0800

    Fleshing out DS base class. Moved Cache stuff out.

--- build/datasource/datasource.js
+++ build/datasource/datasource.js
@@ -117,7 +117,8 @@ Y.mix(DataSourceBase, {
     TYPE_TEXT: 5,
     
     /**
-     * Type is an HTML TABLE element. Data is parsed out of TR elements from all TBODY elements.
+     * Type is an HTML TABLE element. Data is parsed out of TR elements from all
+     * TBODY elements.
      *
      * @property TYPE_HTMLTABLE
      * @type Number
@@ -173,7 +174,8 @@ Y.mix(DataSourceBase, {
          * Converts data to type String.
          *
          * @method DS.Parser.parseString
-         * @param oData {String | Number | Boolean | Date | Array | Object} Data to parse.
+         * @param oData {String | Number | Boolean | Date | Array | Object}
+         * Data to parse.
          * The special values null and undefined will return null.
          * @return {String} A string, or null.
          * @static
@@ -200,8 +202,8 @@ Y.mix(DataSourceBase, {
          * Converts data to type Number.
          *
          * @method DS.Parser.parseNumber
-         * @param oData {String | Number | Boolean | Null} Data to convert. Beware, null
-         * returns as 0.
+         * @param oData {String | Number | Boolean | Null} Data to convert.
+         * Beware, null returns as 0.
          * @return {Number} A number, or null if NaN.
          * @static
          */
@@ -214,7 +216,6 @@ Y.mix(DataSourceBase, {
                 return number;
             }
             else {
-                YAHOO.log("Could not convert data " + lang.dump(oData) + " to type Number", "warn", this.toString());
                 return null;
             }
         },
@@ -243,7 +244,6 @@ Y.mix(DataSourceBase, {
                 return date;
             }
             else {
-                YAHOO.log("Could not convert data " + lang.dump(oData) + " to type Date", "warn", this.toString());
                 return null;
             }
         }
@@ -275,8 +275,10 @@ Y.mix(DataSourceBase, {
                 <li>TYPE_JSFUNCTION</li>
             </ul>
         * @type Number
+        * @default DataSourceBase.TYPE_UNKNOWN        
         */
         dataType: {
+            value: DataSourceBase.TYPE_UNKNOWN
         },
 
         /**
@@ -291,62 +293,102 @@ Y.mix(DataSourceBase, {
                 <li>TYPE_HTMLTABLE</li>
             </ul>
         * @type Number
+        * @default DataSourceBase.TYPE_UNKNOWN
         */
         responseType: {
+            value: DataSourceBase.TYPE_UNKNOWN
         },
 
         /**
         * @attribute responseSchema
-        * @description XXX
-        * @type XXX
+        * @description Response schema object literal takes a combination of
+        * the following properties:
+            <dl>
+                <dt>resultsList {String}</dt>
+                    <dd>Pointer to array of tabular data</dd>
+                <dt>resultNode {String}</dt>
+                    <dd>Pointer to node name of row data (XML data only)</dl>
+                <dt>recordDelim {String}</dt>
+                    <dd>Record delimiter (text data only)</dd>
+                <dt>fieldDelim {String}</dt>
+                    <dd>Field delimiter (text data only)</dd>
+                <dt>fields {String[] | Object []}</dt>
+                    <dd>Array of field names (aka keys), or array of object literals such as:
+                    {key:"fieldname", parser:DataSourceBase.parseDate}</dd>
+                <dt>metaFields {Object}</dt>
+                    <dd>Hash of field names (aka keys) to include in the 
+                    oParsedResponse.meta collection</dd>
+                <dt>metaNode {String}</dt>
+                    <dd>Name of the node under which to search for meta
+                    information in XML response data (XML data only)</dd>
+            </dl>
+        * @type Object
+        * @default {}        
         */
         responseSchema: {
+            value: {}
         },
-
-
-        /**
-        * @attribute maxCacheEntries
-        * @description XXX
-        * @type XXX
-        */
-        maxCacheEntries: {
-        },
-
 
         /**
         * @attribute ERROR_DATAINVALID
-        * @description XXX
-        * @type XXX
+        * @description Error message for invalid data responses.
+        * @type String
+        * @default "Invalid data"
         */
         ERROR_DATAINVALID: {
+            value: "Invalid data"
         },
 
 
         /**
         * @attribute ERROR_DATANULL
-        * @description XXX
-        * @type XXX
+        * @description Error message for null data responses.
+        * @type String
+        * @default "Null data"        
         */
         ERROR_DATANULL: {
+            value: "Null data"
         }
     }
 });
     
 Y.extend(DataSourceBase, Y.Base, {
         /**
-        * @private
+        * @property _queue
+        * @description Object literal to manage asynchronous request/response
+        * cycles enabled if queue needs to be managed (asyncMode/xhrConnMode):
+            <dl>
+                <dt>interval {Number}</dt>
+                    <dd>Interval ID of in-progress queue.</dd>
+                <dt>conn</dt>
+                    <dd>In-progress connection identifier (if applicable).</dd>
+                <dt>requests {Object[]}</dt>
+                    <dd>Array of queued request objects: {request:oRequest, callback:_xhrCallback}.</dd>
+            </dl>
+        * @type Object
+        * @default {interval:null, conn:null, requests:[]}    
+        * @private     
+        */
+        _queue: null,
+
+        /**
+        * @property _intervals
+        * @description Array of polling interval IDs that have been enabled,
+        * stored here to be able to clear all intervals.
+        * @private        
+        */
+        _intervals: null,
+
+        /**
         * @method _createEvents
-        * @description This method creates all the events for this Event Target and publishes them so we get Event Bubbling.
+        * @description This method creates all the events for this Event
+        * Target and publishes them so we get Event Bubbling.
+        * @private        
         */
         _initEvents: function() {
-            
             var events = [
-                "cacheFlushEvent",
-                "cacheRequestEvent",
-                "cacheResponseEvent",
                 "dataErrorEvent",
                 "requestEvent",
-                "responseCacheEvent",
                 "responseEvent",
                 "responseParseEvent"
             ];
@@ -367,17 +409,19 @@ Y.extend(DataSourceBase, Y.Base, {
         },
 
         /**
-        * @private
         * @method initializer
-        * @description Internal init() handler
+        * @description Internal init() handler.
+        * @private        
         */
         initializer: function() {
+            this._queue = {interval:null, conn:null, requests:[]};
+            this._intervals = [];
         },
 
         /**
-        * @private
         * @method destructor
-        * @description Internal destroy() handler
+        * @description Internal destroy() handler.
+        * @private        
         */
         destructor: function() {
         }
@@ -389,4 +433,4 @@ Y.extend(DataSourceBase, Y.Base, {
 
 
 
-}, '@VERSION@' );
+}, '@VERSION@' ,{requires:['base']});
