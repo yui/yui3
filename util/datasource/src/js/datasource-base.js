92c2550853fd8a44d4aa2b3fba3e428cbd5ec752
Jenny Han Donnelly <jennydonnelly@yahoo-inc.com>
2009-01-03 06:51:54 -0800

    Removing unused Attribute dataType.

--- util/datasource/src/js/datasource-base.js
+++ util/datasource/src/js/datasource-base.js
@@ -158,15 +158,6 @@ Y.extend(Base, Y.Base, {
     * @private     
     */
     _queue: null,
-
-    /**
-    * @property _intervals
-    * @description Array of polling interval IDs that have been enabled,
-    * stored here to be able to clear all intervals.
-    * @private        
-    */
-    _intervals: null,
-
     /**
     * @method initializer
     * @description Internal init() handler.
@@ -291,60 +282,6 @@ Y.extend(Base, Y.Base, {
         this.fire("requestEvent", {tId:tId, request:request,callback:callback});
         Y.log("Transaction " + tId + " sent request: " + Y.dump(request), "info", this.toString());
         return tId;
-    },
-
-    /**
-     * Sets up a polling mechanism to send requests at set intervals and forward
-     * responses to given callback.
-     *
-     * @method setInterval
-     * @param msec {Number} Length of interval in milliseconds.
-     * @param request {Object} Request object.
-     * @param callback {Object} An object literal with the following properties:
-     *     <dl>
-     *     <dt><code>success</code></dt>
-     *     <dd>The function to call when the data is ready.</dd>
-     *     <dt><code>failure</code></dt>
-     *     <dd>The function to call upon a response failure condition.</dd>
-     *     <dt><code>scope</code></dt>
-     *     <dd>The object to serve as the scope for the success and failure handlers.</dd>
-     *     <dt><code>argument</code></dt>
-     *     <dd>Arbitrary data that will be passed back to the success and failure handlers.</dd>
-     *     </dl> 
-     * @return {Number} Interval ID.
-     */
-    setInterval: function(msec, request, callback) {
-        if(LANG.isNumber(msec) && (msec >= 0)) {
-            Y.log("Enabling polling to live data for \"" + Y.dump(request) + "\" at interval " + msec, "info", this.toString());
-            var self = this,
-                id = setInterval(function() {
-                self._makeConnection(request, callback);
-            }, msec);
-            this._intervals.push(id);
-            return id;
-        }
-        else {
-            Y.log("Could not enable polling to live data for \"" + Y.dump(request) + "\" at interval " + msec, "info", this.toString());
-        }
-    },
-    
-    /**
-     * Disables polling mechanism associated with the given interval ID.
-     *
-     * @method clearInterval
-     * @param id {Number} Interval ID.
-     */
-    clearInterval: function(id) {
-        // Remove from tracker if there
-        var tracker = this._intervals || [],
-            i = tracker.length-1;
-            
-        for(; i>-1; i--) {
-            if(tracker[i] === id) {
-                tracker.splice(i,1);
-                clearInterval(id);
-            }
-        }
     },
 
     /**
