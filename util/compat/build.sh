cfe4b8ebf8befa9973d2acb157642cb1eef34df0
Adam Moore <adamoore@yahoo-inc.com>
2009-01-13 18:56:52 -0800

    removed util dir stuff

--- util/compat/build.sh
+++
@@ -1,9 +0,0 @@
-pushd ~/www/yui30/scripts/
-./update_cvs.sh
-popd
-ant
-cd build_tmp
-echo "YUI._setup();YUI.use('node', 'compat');" >> compat-debug.js
-echo "YUI._setup();YUI.use('node', 'compat');" >> compat-min.js
-echo "YUI._setup();YUI.use('node', 'compat');" >> compat.js
-cp *js ../../../build/compat
