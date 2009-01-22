6b508496498cabb13fe70b55dd7103c5ad46ceb5
gmoothart <gmoothart@gmail.com>
2009-01-21 22:09:13 -0800

    managed to get unit-tests running by explicitly adding array-extensions as a module, and relying on the yahoo-hosted pr2 files for unit-testing. I do not think everything is quite correct, but it works and I'm unfamiliar with the yui build process.

--- src/array-extensions/js/array-extensions.js
+++ src/array-extensions/js/array-extensions.js
@@ -1,67 +1,66 @@
 
-var Native = array.prototype;
-var A = Y.Array;
-Y.mix(A, {
-    /*
-    * mozilla methods:
-    * filter(select/findAll), every, map, reduce, reduceRight
-    *
-    * yui-functional additional methods:
-    * 
-    * detect/find, reject (negated select), grep, partition, zip 
-    */
+YUI.add('array-extensions', function(Y) {
+  var Native = Array.prototype;
+  var A = Y.Array;
+  Y.mix(A, {
+      /*
+      * mozilla methods:
+      * filter(select/findAll), every, map, reduce, reduceRight
+      *
+      * yui-functional additional methods:
+      * 
+      * detect/find, reject (negated select), grep, partition, zip 
+      */
 
-    filter: (Native.filter) ?
-        function(a, f, o) {
-            return Native.filter.call(a, f, o);
-        } :
-        function(a, f, o) {
-            var results = [];
-            A.each(a, function(item, i, a) {
-              if (f.call(o, item, i, a)) {
-                  results.push(item);
-              }
-            });
-
-            return results;
-        };
-
-    every: (Native.every) ?
-        function(a, f, o) {
-            return Native.every.call(a,f,o);
-        } :
-        function(a, f, o) {
-            var l = a.length;
-            for (var i = 0; i < l; i=i+1) {
-                if (!f.call(o, a[i], i, a)) {
-                    return false;
+      filter: (Native.filter) ?
+          function(a, f, o) {
+              return Native.filter.call(a, f, o);
+          } :
+          function(a, f, o) {
+              var results = [];
+              A.each(a, function(item, i, a) {
+                if (f.call(o, item, i, a)) {
+                    results.push(item);
                 }
-            }
+              });
 
-            return true;
-        };
+              return results;
+          },
 
-    map: (Native.map) ? 
-        function(a, f, o) {
-            return Native.map.call(a, f, o);
-        } :
-        function(a, f, o) {
-            var results = [];
-            A.each(a, function(item, i, a) {
-                results.push(f.call(o, item, i, a));
-            });
-            return results;
-        };
+      every: (Native.every) ?
+          function(a, f, o) {
+              return Native.every.call(a,f,o);
+          } :
+          function(a, f, o) {
+              var l = a.length;
+              for (var i = 0; i < l; i=i+1) {
+                  if (!f.call(o, a[i], i, a)) {
+                      return false;
+                  }
+              }
 
-    reduce: (Native.reduce) ?
-        function(a, init, f, o) {
-            return Native.reduce.call(a, f, init, o);
-        } :
-        function(a, f, init, o) {
-            
-        }
+              return true;
+          },
 
+      map: (Native.map) ? 
+          function(a, f, o) {
+              return Native.map.call(a, f, o);
+          } :
+          function(a, f, o) {
+              var results = [];
+              A.each(a, function(item, i, a) {
+                  results.push(f.call(o, item, i, a));
+              });
+              return results;
+          },
 
+      reduce: (Native.reduce) ?
+          function(a, init, f, o) {
+              return Native.reduce.call(a, f, init, o);
+          } :
+          function(a, f, init, o) {
+              
+          }
 
-
+  });
 });
