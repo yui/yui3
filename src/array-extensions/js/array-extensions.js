19144fe719c59d87f31e5b52dceb6d093526e2d7
Gabe Moothart <gmoothart@gmail.com>
2009-01-25 20:29:48 -0800

    added Y.Array.find, from yui-functional. Also changed unit test back so they test find ('some' is not the same as 'find' - oops)

--- src/array-extensions/js/array-extensions.js
+++ src/array-extensions/js/array-extensions.js
@@ -1,65 +1,76 @@
 
 YUI.add('array-extensions', function(Y) {
-  var Native = Array.prototype;
-  var A = Y.Array;
-  Y.mix(A, {
+    var Native = Array.prototype;
+    var A = Y.Array;
+    Y.mix(A, {
 
-      filter: (Native.filter) ?
-          function(a, f, o) {
-              return Native.filter.call(a, f, o);
-          } :
-          function(a, f, o) {
-              var results = [];
-              A.each(a, function(item, i, a) {
-                  if (f.call(o, item, i, a)) {
-                      results.push(item);
-                  }
-              });
+        filter: (Native.filter) ?
+            function(a, f, o) {
+                return Native.filter.call(a, f, o);
+            } :
+            function(a, f, o) {
+                var results = [];
+                A.each(a, function(item, i, a) {
+                    if (f.call(o, item, i, a)) {
+                        results.push(item);
+                    }
+                });
 
-              return results;
-          },
+                return results;
+            },
 
-      reject: 
-          function(a, f, o) {
-              return A.filter(a, function(item, i, a) {
-                  return !f.call(o, item, i, a);
-              });
-          },
+        reject: 
+            function(a, f, o) {
+                return A.filter(a, function(item, i, a) {
+                    return !f.call(o, item, i, a);
+                });
+            },
 
-      every: (Native.every) ?
-          function(a, f, o) {
-              return Native.every.call(a,f,o);
-          } :
-          function(a, f, o) {
-              var l = a.length;
-              for (var i = 0; i < l; i=i+1) {
-                  if (!f.call(o, a[i], i, a)) {
-                      return false;
-                  }
-              }
+        every: (Native.every) ?
+            function(a, f, o) {
+                return Native.every.call(a,f,o);
+            } :
+            function(a, f, o) {
+                var l = a.length;
+                for (var i = 0; i < l; i=i+1) {
+                    if (!f.call(o, a[i], i, a)) {
+                        return false;
+                    }
+                }
 
-              return true;
-          },
+                return true;
+            },
 
-      map: (Native.map) ? 
-          function(a, f, o) {
-              return Native.map.call(a, f, o);
-          } :
-          function(a, f, o) {
-              var results = [];
-              A.each(a, function(item, i, a) {
-                  results.push(f.call(o, item, i, a));
-              });
-              return results;
-          },
+        map: (Native.map) ? 
+            function(a, f, o) {
+                return Native.map.call(a, f, o);
+            } :
+            function(a, f, o) {
+                var results = [];
+                A.each(a, function(item, i, a) {
+                    results.push(f.call(o, item, i, a));
+                });
+                return results;
+            },
 
-      reduce: (Native.reduce) ?
-          function(a, init, f, o) {
-              return Native.reduce.call(a, f, init, o);
-          } :
-          function(a, f, init, o) {
-              
-          },
+        reduce: (Native.reduce) ?
+            function(a, init, f, o) {
+                return Native.reduce.call(a, f, init, o);
+            } :
+            function(a, f, init, o) {
+                
+            },
+
+        find:
+            function(a, f, o) {
+                var l = a.length;
+                for(var i=0; i < l; i++) {
+                    if (f.call(o, a[i], i, a)) {
+                        return a[i];
+                    }
+                }
+                return null;
+            },
 
       /**
         Function: grep
