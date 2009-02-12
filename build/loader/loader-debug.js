29c3b4d91eb7eaaae2c385f8b350ef13ea9f2b4b
Matt Sweeney <matt.sweeney@yahoo.com>
2009-02-11 16:27:50 -0800

    added event-simulate dependency for node-event-simulate

--- build/loader/loader-debug.js
+++ build/loader/loader-debug.js
@@ -189,7 +189,7 @@ var BASE = 'base',
                 },
 
                 'node-event-simulate': {
-                    requires: ['node-base']
+                    requires: ['node-base', 'event-simulate']
                 }
             }
         },
