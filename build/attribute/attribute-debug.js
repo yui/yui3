007d778311954420082ea0ea11f663d75d6a02c9
Satyen Desai <sdesai@yahoo-inc.com>
2009-04-06 18:28:58 -0700

    Support for get/set in attribute config removed (replaced with getter/setter about a week ago). Added config check to plugin

--- build/attribute/attribute-debug.js
+++ build/attribute/attribute-debug.js
@@ -109,8 +109,6 @@ YUI.add('attribute', function(Y) {
 
         DOT = ".",
         CHANGE = "Change",
-        GET = "get",
-        SET = "set",
         GETTER = "getter",
         SETTER = "setter",
         VALUE = "value",
@@ -281,7 +279,7 @@ YUI.add('attribute', function(Y) {
             }
 
             val = conf.get(name, VALUE);
-            getFn = conf.get(name, GETTER) || conf.get(name, GET);
+            getFn = conf.get(name, GETTER);
 
             val = (getFn) ? getFn.call(this, val) : val;
             val = (path) ? O.getValue(val, path) : val;
@@ -463,7 +461,7 @@ YUI.add('attribute', function(Y) {
                 name = e.attrName,
                 val = e.newVal,
                 valFn  = conf.get(name, VALIDATOR),
-                setFn = conf.get(name, SETTER) || conf.get(name, SET),
+                setFn = conf.get(name, SETTER),
                 storedVal,
                 retVal;
 
@@ -492,7 +490,7 @@ YUI.add('attribute', function(Y) {
                 // Honor set normalization
                 e.newVal = conf.get(name, VALUE);
             } else {
-                Y.log('Validation failed or Setter returned Attribute.INVALID_VALUE. State not updated and stopImmediatePropagation called for attribute: ' + name + ' , value:' + val, 'warn', 'attribute');
+                Y.log('Validation failed or setter returned Attribute.INVALID_VALUE. State not updated and stopImmediatePropagation called for attribute: ' + name + ' , value:' + val, 'warn', 'attribute');
 
                 // Prevent "after" listeners from being 
                 // invoked since nothing changed.
