e0d1c14c34c6edfb7d8dc22a1074018bd16462a9
Adam Moore <adamoore@yahoo-inc.com>
2009-05-01 14:03:44 -0700

    Added proposed submodule for event delegation

---
+++ src/node/js/node-event-delegate.js
@@ -0,0 +1,24 @@
+/*
+ * Functionality to make the node a delegated event container
+ * @module node
+ * @for Node
+ * @submodule node-event-delegate
+ */
+
+/**
+ * Functionality to make the node a delegated event container
+ * @event delegate
+ * @param type {string} 'delegate'
+ * @param fn {string} the function to execute
+ * @param el {string|node} the element that is the delegation container
+ * @param delegateType {string} the event type to delegate
+ * @param spec {string} a selector that must match the target of the event.
+ * @return {Event.Handle} the detach handle
+ * @for YUI
+ */
+Y.Node.prototype.delegate = function(type, fn, delegateType, spec) {
+    var a = Y.Array(arguments, 0, true);
+    a.splice(2, 0, Y.Node.getDOMNode(this));
+    return Y.on.apply(Y, a);
+};
+
