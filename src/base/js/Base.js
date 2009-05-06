7892f3d8ca60a683faeac9707e277bb47eec4126
Satyen Desai <sdesai@yahoo-inc.com>
2009-05-06 16:33:40 -0700

    Base Sugar: Added ability to subscribe to on/after event moments through the constructor. Minor perf tweak - use String.splice instead of String.split when we only need the attribute name from a complex ('x.y.z') set

--- src/base/js/Base.js
+++ src/base/js/Base.js
@@ -151,6 +151,15 @@
             // TODO: Look at why this needs to be done after publish.
             Y.Plugin.Host.call(this);
 
+            if (config) {
+                if (config.on) {
+                    this.on(config.on);
+                }
+                if (config.after) {
+                    this.after(config.after);
+                }
+            }
+
             this.fire(INIT, {cfg: config});
             return this;
         },
@@ -419,12 +428,11 @@
         get: function(name) {
 
             if (this._classCfgs) {
-                var attrName = name;
+                var attrName = name,
+                    iDot = name.indexOf(DOT);
 
-                if (name.indexOf(DOT) !== -1) {
-                    // TODO: Faster way to get attrName?
-                    var path = name.split(DOT);
-                    attrName = path.shift();
+                if (iDot !== -1) {
+                    attrName = name.slice(0, iDot);
                 }
 
                 if (this._classCfgs[attrName] && !this.attrAdded(attrName)) {
