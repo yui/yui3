a8240e5d8edb13039000a849e3b9ba2e50d93c55
Nicholas C. Zakas <nicholas.c.zakas@nczonline.net>
2009-05-10 15:58:48 -0700

    Implemented proper XML escaping in Y.Test.Format.XML

--- src/test/js/TestFormat.js
+++ src/test/js/TestFormat.js
@@ -1,6 +1,26 @@
     
     Y.namespace("Test.Format");
     
+    /* (intentionally not documented)
+     * Basic XML escaping method. Replaces quotes, less-than, greater-than,
+     * apostrophe, and ampersand characters with their corresponding entities.
+     * @param {String} text The text to encode.
+     * @return {String} The XML-escaped text.
+     */
+    function xmlEscape(text){
+    
+        return text.replace(/[<>"'&]/g, function(value){
+            switch(value){
+                case "<":   return "&lt;";
+                case ">":   return "&gt;";
+                case "\"":  return "&quot;";
+                case "'":   return "&apos;";
+                case "&":   return "&amp;";
+            }
+        });
+    
+    }
+    
     /**
      * Returns test results formatted as a JSON string. Requires JSON utility.
      * @param {Object} result The results object created by TestRunner.
@@ -24,10 +44,10 @@
     Y.Test.Format.XML = function(results) {
     
         var l = Y.Lang;
-        var xml = "<" + results.type + " name=\"" + results.name.replace(/"/g, "&quot;").replace(/'/g, "&apos;") + "\"";
+        var xml = "<" + results.type + " name=\"" + xmlEscape(results.name) + "\"";
         
         if (results.type == "test"){
-            xml += " result=\"" + results.result + "\" message=\"" + results.message + "\">";
+            xml += " result=\"" + xmlEscape(results.result) + "\" message=\"" + xmlEscape(results.message) + "\">";
         } else {
             xml += " passed=\"" + results.passed + "\" failed=\"" + results.failed + "\" ignored=\"" + results.ignored + "\" total=\"" + results.total + "\">";
             Y.Object.each(results, function(value, prop){
