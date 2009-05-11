007a64c4ed3bb91d51177749e957b28c958d7001
Jenny Han Donnelly <jennydonnelly@yahoo-inc.com>
2009-05-11 14:47:44 -0700

    Meta rollup build fixed due to updated rollup.xml in builder.

--- build/datatype/datatype.js
+++ build/datatype/datatype.js
@@ -1,3 +1,152 @@
+YUI.add('datatype-number-parse', function(Y) {
+
+/**
+ * The DataType utility provides a set of utility functions to operate on native
+ * JavaScript data types.
+ *
+ * @module datatype
+ */
+var LANG = Y.Lang;
+
+/**
+ * Parse number submodule.
+ *
+ * @class DataType.Number
+ * @submodule datatype-number-format
+ * @static
+ */
+Y.mix(Y.namespace("DataType.Number"), {
+    /**
+     * Converts data to type Number.
+     *
+     * @method parse
+     * @param data {String | Number | Boolean} Data to convert. The following
+     * values return as null: null, undefined, NaN, "".
+     * @return {Number} A number, or null.
+     * @static
+     */
+    parse: function(data) {
+        var number = (data === null) ? data : +data;
+        if(LANG.isNumber(number)) {
+            return number;
+        }
+        else {
+            return null;
+        }
+    }
+});
+
+// Add Parsers shortcut
+Y.namespace("Parsers").number = Y.DataType.Number.parse;
+
+
+
+}, '@VERSION@' );
+
+YUI.add('datatype-number-format', function(Y) {
+
+/**
+ * The DataType utility provides a set of utility functions to operate on native
+ * JavaScript data types.
+ *
+ * @module datatype
+ */
+var LANG = Y.Lang;
+
+/**
+ * Format number submodule.
+ *
+ * @class DataType.Number
+ * @submodule datatype-number-format
+ * @static
+ */
+Y.mix(Y.namespace("DataType.Number"), {
+     /**
+     * Takes a Number and formats to string for display to user.
+     *
+     * @method format
+     * @param data {Number} Number.
+     * @param config {Object} (Optional) Optional configuration values:
+     *  <dl>
+     *   <dt>prefix {String}</dd>
+     *   <dd>String prepended before each number, like a currency designator "$"</dd>
+     *   <dt>decimalPlaces {Number}</dd>
+     *   <dd>Number of decimal places to round. Must be a number 0 to 20.</dd>
+     *   <dt>decimalSeparator {String}</dd>
+     *   <dd>Decimal separator</dd>
+     *   <dt>thousandsSeparator {String}</dd>
+     *   <dd>Thousands separator</dd>
+     *   <dt>suffix {String}</dd>
+     *   <dd>String appended after each number, like " items" (note the space)</dd>
+     *  </dl>
+     * @return {String} Formatted number for display. Note, the following values
+     * return as "": null, undefined, NaN, "".
+     */
+    format: function(data, config) {
+        if(LANG.isNumber(data)) {
+            config = config || {};
+
+            var isNeg = (data < 0),
+                output = data + "",
+                decPlaces = config.decimalPlaces,
+                decSep = config.decimalSeparator || ".",
+                thouSep = config.thousandsSeparator,
+                decIndex,
+                newOutput, count, i;
+
+            // Decimal precision
+            if(LANG.isNumber(decPlaces) && (decPlaces >= 0) && (decPlaces <= 20)) {
+                // Round to the correct decimal place
+                output = data.toFixed(decPlaces);
+            }
+
+            // Decimal separator
+            if(decSep !== "."){
+                output = output.replace(".", decSep);
+            }
+
+            // Add the thousands separator
+            if(thouSep) {
+                // Find the dot or where it would be
+                decIndex = output.lastIndexOf(decSep);
+                decIndex = (decIndex > -1) ? decIndex : output.length;
+                // Start with the dot and everything to the right
+                newOutput = output.substring(decIndex);
+                // Working left, every third time add a separator, every time add a digit
+                for (count = 0, i=decIndex; i>0; i--) {
+                    if ((count%3 === 0) && (i !== decIndex) && (!isNeg || (i > 1))) {
+                        newOutput = thouSep + newOutput;
+                    }
+                    newOutput = output.charAt(i-1) + newOutput;
+                    count++;
+                }
+                output = newOutput;
+            }
+
+            // Prepend prefix
+            output = (config.prefix) ? config.prefix + output : output;
+
+            // Append suffix
+            output = (config.suffix) ? output + config.suffix : output;
+
+            return output;
+        }
+        // Not a Number, just return as string
+        else {
+            return data.toString();
+        }
+    }
+});
+
+
+
+}, '@VERSION@' );
+
+
+
+YUI.add('datatype-number', function(Y){}, '@VERSION@' ,{use:['datatype-number-parse', 'datatype-number-format']});
+
+
 YUI.add('datatype-date', function(Y) {
 
 /**
@@ -410,6 +559,117 @@ Y.DataType.Date.Locale["en-AU"] = Y.merge(YDateEn);
 
 
 }, '@VERSION@' );
+
+YUI.add('datatype-xml-parse', function(Y) {
+
+/**
+ * The DataType utility provides a set of utility functions to operate on native
+ * JavaScript data types.
+ *
+ * @module datatype
+ */
+var LANG = Y.Lang;
+
+/**
+ * Parse XML submodule.
+ *
+ * @class DataType.XML
+ * @submodule datatype-xml-parse
+ * @static
+ */
+ 
+Y.mix(Y.namespace("DataType.XML"), {
+    /**
+     * Converts data to type XMLDocument.
+     *
+     * @method parse
+     * @param data {String} Data to convert.
+     * @return {XMLDoc} XML Document.
+     * @static
+     */
+    parse: function(data) {
+        var xmlDoc = null;
+        if(LANG.isString(data)) {
+            try {
+                if(!LANG.isUndefined(DOMParser)) {
+                    xmlDoc = new DOMParser().parseFromString(data, "text/xml");
+                }
+            }
+            catch(e) {
+                try {
+                    if(!LANG.isUndefined(ActiveXObject)) {
+                            xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+                            xmlDoc.async = false;
+                            xmlDoc.loadXML(data);
+                    }
+                }
+                catch(ee) {
+                }
+            }
+        }
+        return xmlDoc;
+    }
+});
+
+// Add Parsers shortcut
+Y.namespace("Parsers").xml = Y.DataType.XML.parse;
+
+
+
+}, '@VERSION@' );
+
+YUI.add('datatype-xml-format', function(Y) {
+
+/**
+ * The DataType utility provides a set of utility functions to operate on native
+ * JavaScript data types.
+ *
+ * @module datatype
+ */
+var LANG = Y.Lang;
+
+/**
+ * Format XML submodule.
+ *
+ * @class DataType.XML
+ * @submodule datatype-xml-format
+ * @static
+ */
+Y.mix(Y.namespace("DataType.XML"), {
+    /**
+     * Converts data to type XMLDocument.
+     *
+     * @method format
+     * @param data {XMLDoc} Data to convert.
+     * @return {String} String.
+     * @static
+     */
+    format: function(data) {
+        try {
+            if(!LANG.isUndefined(XMLSerializer)) {
+                return (new XMLSerializer()).serializeToString(data);
+            }
+        }
+        catch(e) {
+            if(data.xml) {
+                return data.xml;
+            }
+            else {
+                return data.toString();
+            }
+        }
+    }
+});
+
+
+
+
+}, '@VERSION@' );
+
+
+
+YUI.add('datatype-xml', function(Y){}, '@VERSION@' ,{use:['datatype-xml-parse', 'datatype-xml-format']});
+
 
 
 
