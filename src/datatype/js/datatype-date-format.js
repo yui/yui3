aad83354a51620a64a2453eaf7b0e8460523a137
Jenny Han Donnelly <jennydonnelly@yahoo-inc.com>
2009-06-08 17:04:24 -0700

    Added datatype-date-parse module and tests. Updated build files.

--- src/datatype/js/datatype-date-format.js
+++ src/datatype/js/datatype-date-format.js
@@ -1,59 +1,3 @@
-YUI.add('datatype-date-parse', function(Y) {
-
-/**
- * The DataType utility provides a set of utility functions to operate on native
- * JavaScript data types.
- *
- * @module datatype
- */
-var LANG = Y.Lang;
-
-/**
- * Parse number submodule.
- *
- * @class DataType.Number
- * @submodule datatype-number-format
- * @static
- */
-Y.mix(Y.namespace("DataType.Date"), {
-    /**
-     * Converts data to type Date.
-     *
-     * @method parse
-     * @param data {String | Number} Data to convert. Values supported by the Date constructor are supported.
-     * @return {Date} A Date, or null.
-     * @static
-     */
-    parse: function(data) {
-        var date = null;
-
-        //Convert to date
-        if(!(LANG.isDate(data))) {
-            date = new Date(data);
-        }
-        else {
-            return date;
-        }
-
-        // Validate
-        if(LANG.isDate(date) && (date != "Invalid Date")) { // Workaround for bug 2527965
-            return date;
-        }
-        else {
-            return null;
-        }
-    }
-});
-
-// Add Parsers shortcut
-Y.namespace("Parsers").date = Y.DataType.Date.parse;
-
-
-
-}, '@VERSION@' );
-
-YUI.add('datatype-date-format', function(Y) {
-
 /**
  * The Date formatter utility implements strftime formatters for javascript based on the 
  * Open Group specification defined at
@@ -307,6 +251,7 @@ var Dt = {
 		oConfig = oConfig || {};
 		
 		if(!Y.Lang.isDate(oDate)) {
+			Y.log("format called without a date", "WARN", "datatype-date");
 			return Y.Lang.isValue(oDate) ? oDate : "";
 		}
 
@@ -317,6 +262,7 @@ var Dt = {
 		
 		// Make sure we have a definition for the requested locale, or default to en.
 		if(!Dt.Locale[sLocale]) {
+			Y.log("selected locale " + sLocale + " not found, trying alternatives", "WARN", "datatype-date");
 			var tmpLocale = sLocale.replace(/-[a-zA-Z]+$/, "");
 			if(tmpLocale in Dt.Locale) {
 				sLocale = tmpLocale;
@@ -325,6 +271,7 @@ var Dt = {
 			} else {
 				sLocale = "en";
 			}
+			Y.log("falling back to " + sLocale, "INFO", "datatype-date");
 		}
 
 		var aLocale = Dt.Locale[sLocale];
@@ -346,6 +293,7 @@ var Dt = {
 						return xPad(oDate[f[0]](), f[1]);
 					} // no break; (fall through to default:)
 				default:
+					Y.log("unrecognised replacement type, please file a bug (format: " + oConfig.format || Y.config.dateFormat + ")", "WARN", "datatype-date");
 					return m1;
 			}
 		};
@@ -365,107 +313,3 @@ var Dt = {
 };
 
 Y.mix(Y.namespace("DataType.Date"), Dt);
-
-/**
- * The Date.Locale class is a container for all localised date strings
- * used by Y.DataType.Date. It is used internally, but may be extended
- * to provide new date localisations.
- *
- * To create your own Locale, follow these steps:
- * <ol>
- *  <li>Find an existing locale that matches closely with your needs</li>
- *  <li>Use this as your base class.  Use Y.DataType.Date.Locale["en"] if nothing
- *   matches.</li>
- *  <li>Create your own class as an extension of the base class using
- *   Y.merge, and add your own localisations where needed.</li>
- * </ol>
- * See the Y.DataType.Date.Locale["en-US"] and Y.DataType.Date.Locale["en-GB"]
- * classes which extend Y.DataType.Date.Locale["en"].
- *
- * For example, to implement locales for French french and Canadian french,
- * we would do the following:
- * <ol>
- *  <li>For French french, we have no existing similar locale, so use
- *   Y.DataType.Date.Locale["en"] as the base, and extend it:
- *   <pre>
- *      Y.DataType.Date.Locale["fr"] = Y.merge(Y.DataType.Date.Locale, {
- *          a: ["dim", "lun", "mar", "mer", "jeu", "ven", "sam"],
- *          A: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"],
- *          b: ["jan", "f&eacute;v", "mar", "avr", "mai", "jun", "jui", "ao&ucirc;", "sep", "oct", "nov", "d&eacute;c"],
- *          B: ["janvier", "f&eacute;vrier", "mars", "avril", "mai", "juin", "juillet", "ao&ucirc;t", "septembre", "octobre", "novembre", "d&eacute;cembre"],
- *          c: "%a %d %b %Y %T %Z",
- *          p: ["", ""],
- *          P: ["", ""],
- *          x: "%d.%m.%Y",
- *          X: "%T"
- *      });
- *   </pre>
- *  </li>
- *  <li>For Canadian french, we start with French french and change the meaning of \%x:
- *   <pre>
- *      Y.DataType.Date.Locale["fr-CA"] = Y.merge(Y.DataType.Date.Locale["fr"], {
- *          x: "%Y-%m-%d"
- *      });
- *   </pre>
- *  </li>
- * </ol>
- *
- * With that, you can use your new locales:
- * <pre>
- *    var d = new Date("2008/04/22");
- *    Y.DataType.Date.format(d, { format: "%A, %d %B == %x", locale: "fr" });
- * </pre>
- * will return:
- * <pre>
- *    mardi, 22 avril == 22.04.2008
- * </pre>
- * And
- * <pre>
- *    Y.DataType.Date.format(d, {format: "%A, %d %B == %x", locale: "fr-CA" });
- * </pre>
- * Will return:
- * <pre>
- *   mardi, 22 avril == 2008-04-22
- * </pre>
- * @module datatype
- * @requires oop
- * @class DataType.Date.Locale
- */
-var YDateEn = {
-	a: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-	A: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-	b: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-	B: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-	c: "%a %d %b %Y %T %Z",
-	p: ["AM", "PM"],
-	P: ["am", "pm"],
-	r: "%I:%M:%S %p",
-	x: "%d/%m/%y",
-	X: "%T"
-};
-
-Y.namespace("DataType.Date.Locale");
-
-Y.DataType.Date.Locale["en"] = YDateEn;
-
-Y.DataType.Date.Locale["en-US"] = Y.merge(YDateEn, {
-	c: "%a %d %b %Y %I:%M:%S %p %Z",
-	x: "%m/%d/%Y",
-	X: "%I:%M:%S %p"
-});
-
-Y.DataType.Date.Locale["en-GB"] = Y.merge(YDateEn, {
-	r: "%l:%M:%S %P %Z"
-});
-Y.DataType.Date.Locale["en-AU"] = Y.merge(YDateEn);
-
-
-
-
-
-}, '@VERSION@' );
-
-
-
-YUI.add('datatype-date', function(Y){}, '@VERSION@' ,{use:['datatype-date-parse', 'datatype-date-format']});
-
