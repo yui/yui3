79dbd0920841d62824330d1c56bdb16abc9fff6d
Matt Sweeney <matt.sweeney@yahoo.com>
2009-06-11 11:10:26 -0700

    building delegate into node-base

--- build/node/node-base-debug.js
+++ build/node/node-base-debug.js
@@ -1268,6 +1268,28 @@ Y.NodeList.importMethod(Y.Node.prototype, ['getAttribute', 'setAttribute']);
     Y.Node.importMethod(Y.DOM, methods);
     Y.NodeList.importMethod(Y.Node.prototype, methods);
 })(Y);
+/*
+ * Functionality to make the node a delegated event container
+ * @module node
+ * @submodule node-event-delegate
+ */
+
+/**
+ * Functionality to make the node a delegated event container
+ * @method delegate
+ * @param fn {Function} the function to execute
+ * @param delegateType {String} the event type to delegate
+ * @param selector {String} a selector that must match the target of the event.
+ * @return {Event.Handle} the detach handle
+ * @for Node
+ */
+Y.Node.prototype.delegate = function(fn, delegateType, selector) {
+    var a = Y.Array(arguments, 0, true);
+    a.push('delegate');
+    a.splice(2, 0, Y.Node.getDOMNode(this));
+    return Y.on.apply(Y, a);
+};
+
 
 
 }, '@VERSION@' ,{requires:['dom-base', 'base', 'selector']});
--- src/node/js/node-event-delegate.js
+++ src/node/js/node-event-delegate.js
@@ -1,21 +1,21 @@
 /*
  * Functionality to make the node a delegated event container
  * @module node
- * @for Node
  * @submodule node-event-delegate
  */
 
 /**
  * Functionality to make the node a delegated event container
- * @event delegate
- * @param fn {string} the function to execute
- * @param delegateType {string} the event type to delegate
- * @param spec {string} a selector that must match the target of the event.
+ * @method delegate
+ * @param fn {Function} the function to execute
+ * @param delegateType {String} the event type to delegate
+ * @param selector {String} a selector that must match the target of the event.
  * @return {Event.Handle} the detach handle
- * @for YUI
+ * @for Node
  */
-Y.Node.prototype.delegate = function(fn, delegateType, spec) {
+Y.Node.prototype.delegate = function(fn, delegateType, selector) {
     var a = Y.Array(arguments, 0, true);
+    a.push('delegate');
     a.splice(2, 0, Y.Node.getDOMNode(this));
     return Y.on.apply(Y, a);
 };
