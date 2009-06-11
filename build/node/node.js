52dd28aec3a1b8ba18886fc747459a3355a85b51
Matt Sweeney <matt.sweeney@yahoo.com>
2009-06-11 13:45:57 -0700

    fixed node::delegate, context

--- build/node/node.js
+++ build/node/node.js
@@ -1284,16 +1284,17 @@ Y.NodeList.importMethod(Y.Node.prototype, ['getAttribute', 'setAttribute']);
 /**
  * Functionality to make the node a delegated event container
  * @method delegate
+ * @param type {String} the event type to delegate
  * @param fn {Function} the function to execute
- * @param delegateType {String} the event type to delegate
  * @param selector {String} a selector that must match the target of the event.
  * @return {Event.Handle} the detach handle
  * @for Node
  */
-Y.Node.prototype.delegate = function(fn, delegateType, selector) {
-    var a = Y.Array(arguments, 0, true);
-    a.push('delegate');
-    a.splice(2, 0, Y.Node.getDOMNode(this));
+Y.Node.prototype.delegate = function(type, fn, selector, context) {
+    context = context || this;
+    var args = Array.prototype.slice.call(arguments, 4),
+        a = ['delegate', fn, Y.Node.getDOMNode(this), type, selector, context];
+    a = a.concat(args);
     return Y.on.apply(Y, a);
 };
 
