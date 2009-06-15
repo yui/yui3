3c1868dc15a1014c0098d7768bd9fd70941bf9cb
Matt Sweeney <matt.sweeney@yahoo.com>
2009-06-15 10:37:23 -0700

    updated NodeList docs

--- build/node/node-base.js
+++ build/node/node-base.js
@@ -1353,6 +1353,26 @@ if (!document.documentElement.hasAttribute) { // IE < 8
     };
 }
 
+/**
+ * Allows setting attributes on DOM nodes, normalizing in some cases.
+ * This passes through to the DOM node, allowing for custom attributes.
+ * @method setAttribute
+ * @see Node
+ * @for NodeList
+ * @chainable
+ * @param {string} name The attribute name 
+ * @param {string} value The value to set
+ */
+
+/**
+ * Allows getting attributes on DOM nodes, normalizing in some cases.
+ * This passes through to the DOM node, allowing for custom attributes.
+ * @method getAttribute
+ * @see Node
+ * @for NodeList
+ * @param {string} name The attribute name 
+ * @return {string} The attribute value 
+ */
 Y.NodeList.importMethod(Y.Node.prototype, ['getAttribute', 'setAttribute']);
 
 (function() { // IE clones expandos; regenerate UID
@@ -1377,51 +1397,51 @@ Y.NodeList.importMethod(Y.Node.prototype, ['getAttribute', 'setAttribute']);
  */
 
     var methods = [
-        /**
-         * Determines whether the node has the given className.
-         * @method hasClass
-         * @for Node
-         * @param {String} className the class name to search for
-         * @return {Boolean} Whether or not the node has the given class. 
-         */
-        'hasClass',
-
-        /**
-         * Adds a class name to the node.
-         * @method addClass         
-         * @param {String} className the class name to add to the node's class attribute
-         * @chainable
-         */
-        'addClass',
-
-        /**
-         * Removes a class name from the node.
-         * @method removeClass         
-         * @param {String} className the class name to remove from the node's class attribute
-         * @chainable
-         */
-        'removeClass',
-
-        /**
-         * Replace a class with another class.
-         * If no oldClassName is present, the newClassName is simply added.
-         * @method replaceClass  
-         * @param {String} oldClassName the class name to be replaced
-         * @param {String} newClassName the class name that will be replacing the old class name
-         * @chainable
-         */
-        'replaceClass',
-
-        /**
-         * If the className exists on the node it is removed, if it doesn't exist it is added.
-         * @method toggleClass  
-         * @param {String} className the class name to be toggled
-         * @chainable
-         */
-        'toggleClass'
     ];
 
     Y.Node.importMethod(Y.DOM, methods);
+    /**
+     * Determines whether each node has the given className.
+     * @method hasClass
+     * @see Node
+     * @for NodeList
+     * @param {String} className the class name to search for
+     * @return {Array} An array of booleans for each node bound to the NodeList. 
+     */
+
+    /**
+     * Adds a class name to each node.
+     * @method addClass         
+     * @see Node
+     * @param {String} className the class name to add to the node's class attribute
+     * @chainable
+     */
+
+    /**
+     * Removes a class name from each node.
+     * @method removeClass         
+     * @see Node
+     * @param {String} className the class name to remove from the node's class attribute
+     * @chainable
+     */
+
+    /**
+     * Replace a class with another class for each node.
+     * If no oldClassName is present, the newClassName is simply added.
+     * @method replaceClass  
+     * @see Node
+     * @param {String} oldClassName the class name to be replaced
+     * @param {String} newClassName the class name that will be replacing the old class name
+     * @chainable
+     */
+
+    /**
+     * If the className exists on the node it is removed, if it doesn't exist it is added.
+     * @method toggleClass  
+     * @see Node
+     * @param {String} className the class name to be toggled
+     * @chainable
+     */
     Y.NodeList.importMethod(Y.Node.prototype, methods);
 })(Y);
 /*
