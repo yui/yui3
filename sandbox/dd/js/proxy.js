030d49a83d585fd2d07a9051a3b8575d7e706ae0
Dav Glass <davglass@gmail.com>
2009-07-31 12:31:42 -0700

    Another patch

--- sandbox/dd/js/proxy.js
+++ sandbox/dd/js/proxy.js
@@ -126,9 +126,9 @@ YUI.add('dd-proxy', function(Y) {
         },
         destructor: function() {
             var host = this.get(HOST);
-            for (var i in this._hands) {
-                this._hands[i].detach();
-            }
+            Y.each(this._hands, function(v) {
+                v.detach();
+            });
             host.set(DRAG_NODE, host.get(NODE));
         }
     };
