24290502361d9387f17d822ad640beabaf7d7837
Isaac Schlueter <isaacs@sistertrain-lm.(none)>
2009-09-16 12:25:35 -0700

    Simplify the setting of escape (no need to wrap)

--- src/querystring/js/querystring-stringify.js
+++ src/querystring/js/querystring-stringify.js
@@ -20,9 +20,7 @@ var QueryString = Y.namespace("QueryString");
  * @for QueryString
  * @static
  **/
-QueryString.escape = function (s) {
-    return encodeURIComponent(s);
-};
+QueryString.escape = encodeURIComponent;
 
 
 var stack = [];
