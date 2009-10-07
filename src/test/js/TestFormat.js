bb6e451d9b8cf6421394062b7e49a9674265058b
Nicholas <nzakas@yahoo-inc.com>
2009-10-07 10:52:42 -0700

    Removed duplicate XML format definition

--- src/test/js/TestFormat.js
+++ src/test/js/TestFormat.js
@@ -42,36 +42,6 @@
      * @static
      */
     Y.Test.Format.XML = function(results) {
-    
-        var l = Y.Lang;
-        var xml = "<" + results.type + " name=\"" + xmlEscape(results.name) + "\"";
-        
-        if (results.type == "test"){
-            xml += " result=\"" + xmlEscape(results.result) + "\" message=\"" + xmlEscape(results.message) + "\">";
-        } else {
-            xml += " passed=\"" + results.passed + "\" failed=\"" + results.failed + "\" ignored=\"" + results.ignored + "\" total=\"" + results.total + "\">";
-            Y.Object.each(results, function(value, prop){
-                if (l.isObject(value) && !l.isArray(value)){
-                    xml += arguments.callee(value);
-                }
-            });        
-        }
-    
-        xml += "</" + results.type + ">";
-        
-        return xml;
-    
-    };
-    
-    /**
-     * Returns test results formatted as an XML string.
-     * @param {Object} result The results object created by TestRunner.
-     * @return {String} An XML-formatted string of results.
-     * @namespace Test.Format
-     * @method XML
-     * @static
-     */
-    Y.Test.Format.XML = function(results) {
 
         function serializeToXML(results){
             var l   = Y.Lang,
