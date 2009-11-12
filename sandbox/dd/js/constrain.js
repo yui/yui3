1ddc64c706a1d38ab21574f07ec555d10fd52d35
Dav Glass <davglass@gmail.com>
2009-11-11 20:39:02 -0800

    Fixes #2528229 - Added a cacheRegion configuration option

--- sandbox/dd/js/constrain.js
+++ sandbox/dd/js/constrain.js
@@ -158,6 +158,14 @@ YUI.add('dd-constrain', function(Y) {
         */
         constrain2view: {
             value: false
+        },
+        /**
+        * @attribute cacheRegion
+        * @description Should the region be cached for performace. Default: true
+        * @type Boolean
+        */
+        cacheRegion: {
+            value: true
         }
     };
 
@@ -187,7 +195,9 @@ YUI.add('dd-constrain', function(Y) {
         * @description Get's the region and caches it, called from window.resize and when the cache is null
         */
         _cacheRegion: function() {
-            this._regionCache = this.get(CON_2_NODE).get('region');
+            if (this.get('cacheRegion')) {
+                this._regionCache = this.get(CON_2_NODE).get('region');
+            }
         },
         /**
         * @method resetCache
