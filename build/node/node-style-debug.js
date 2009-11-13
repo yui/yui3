9f92ae862da670f7ca0c79d42d378efeeea32ab6
Matt Sweeney <matt.sweeney@yahoo.com>
2009-11-12 17:45:18 -0800

    updated node-size and dom-size

--- build/node/node-style-debug.js
+++ build/node/node-style-debug.js
@@ -78,6 +78,44 @@ Y.Node.importMethod(Y.DOM, methods);
  */
 Y.NodeList.importMethod(Y.Node.prototype, methods);
 })(Y);
+Y.mix(Y.Node.ATTRS, {
+    height: {
+        setter: function(h) {
+            Y.DOM.setHeight(this._node, h);
+            return h;
+        },
+
+        getter: function() {
+            return this._node.offsetHeight;
+        }
+    },
+
+    width: {
+        setter: function(w) {
+            Y.DOM.setWidth(this._node, w);
+            return w;
+        },
+
+        getter: function() {
+            return this._node.offsetWidth;
+        }
+    }
+});
+
+Y.mix(Y.Node.prototype, {
+    sizeTo: function(size) {
+        var node;
+        if (!Y.Lang.isArray(size, true)) {
+            node = Y.one(size);
+            size = [node.get('width'), node.get('height')];
+        }
+
+        this.setAttrs({
+            width: size[0],
+            height: size[1]
+        });
+    }
+});
 
 
 }, '@VERSION@' ,{requires:['dom-style', 'node-base']});
