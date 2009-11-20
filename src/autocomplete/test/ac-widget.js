eb9076feda39105b9fe71b0380c75cd365b03b77
isaacs <i@foohack.com>
2009-11-20 13:13:46 -0800

    The widget

---
+++ src/autocomplete/test/ac-widget.js
@@ -0,0 +1,11 @@
+// just a default display widget for the autocomplete component
+// this is the one you expect when you say "autocomplete"
+YUI.add('ac-widget', function(Y) {
+
+function ACWidget () { ACWidget.superclass.constructor.apply(this, arguments) };
+
+
+
+}, '@VERSION', {
+    requires : ['widget','node']
+});
