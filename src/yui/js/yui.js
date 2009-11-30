6a8fcbfdb7d9d0b938fc350e3fc29eedb2048586
Adam Moore <adamoore@yahoo-inc.com>
2009-11-30 15:38:03 -0800

    Automatic base path detection can handle hybrid combo urls (e.g., http://yui.yahooapis.com/combo?2.8.0r4/build/yuiloader-dom-event/yuiloader-dom-event.js&3.0.0/build/yui/yui-min.js).  [Fixes #2528423].

--- src/yui/js/yui.js
+++ src/yui/js/yui.js
@@ -181,19 +181,40 @@ YUI.prototype = {
             bootstrap: true,
             fetchCSS: true,
         
-            base: function() {
-                var b, nodes, i, match;
+            base: (Y === YUI) ? Y.Env.cdn : function() {
+                var b, nodes, i, src, match;
 
                 // get from querystring
                 nodes = document.getElementsByTagName('script');
 
                 for (i=0; i<nodes.length; i=i+1) {
-                    match = nodes[i].src.match(/^(.*)yui\/yui[\.\-].*js(\?.*)?$/);
+                    src = nodes[i].src;
+
+                    // DEBUG
+                    //src = "http://yui.yahooapis.com/combo?2.8.0r4/build/yuiloader-dom-event/yuiloader-dom-event.js&3.0.0/build/yui/yui-min.js";
+                    //console.log('src) ' + src);
+                    // DEBUG
+
+                    match = src.match(/^(.*)yui\/yui[\.\-].*js(\?.*)?$/);
                     b = match && match[1];
+
+                    // First stage base extraction
+                    // console.log('1) ' + b);
+
                     if (b) {
+                        // extract correct path for mixed combo urls
+                        // http://yuilibrary.com/projects/yui3/ticket/2528423
+                        // http://yui.yahooapis.com/combo?2.8.0r4/build/yuiloader-dom-event/yuiloader-dom-event.js&3.0.0/build/yui/yui-min.js
+                        match = src.match(/^(.*\?)(.*\&)(.*)yui\/yui[\.\-].*js(\?.*)?$/);
+                        if (match && match[3]) {
+                            b = match[1] + match[3];
+                        }
+
                         break;
                     }
                 }
+
+                // console.log('2) ' + b);
 
                 // use CDN default
                 return b || Y.Env.cdn;
