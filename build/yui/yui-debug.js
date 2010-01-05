aeed535a9cfe23128557430909ef0f8f74570590
Adam Moore <adamoore@yahoo-inc.com>
2010-01-05 12:30:37 -0800

    The log event now isn't configured to emit a facade because that capability may not be there, and the facade only was used because broadcasting used to require it and no longer does.

--- build/yui/yui-debug.js
+++ build/yui/yui-debug.js
@@ -3017,8 +3017,7 @@ INSTANCE.log = function(msg, cat, src, silent) {
             if (Y.fire && !silent) {
                 if (!_published) {
                     Y.publish(LOGEVENT, {
-                        broadcast: 2,
-                        emitFacade: 1
+                        broadcast: 2
                     });
 
                     _published = 1;
