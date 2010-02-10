a18bc5aca2881194f7522ec4ab7249c82eae71a6
Matt Sweeney <matt.sweeney@yahoo.com>
2010-02-10 12:02:33 -0800

    added iphone touch events [fixes #2527902]

--- src/node/js/node.js
+++ src/node/js/node.js
@@ -77,6 +77,7 @@ Y_Node.DOM_EVENTS = {
     beforeunload: 1,
     blur: 1,
     change: 1,
+    changedTouches: 1, // iphone
     click: 1,
     close: 1,
     command: 1,
@@ -92,6 +93,7 @@ Y_Node.DOM_EVENTS = {
     drop: 1,
     error: 1,
     focus: 1,
+    identifier: 1, // iphone
     key: 1,
     keydown: 1,
     keypress: 1,
@@ -109,10 +111,14 @@ Y_Node.DOM_EVENTS = {
     mousewheel: 1,
     reset: 1,
     resize: 1,
+    rotation: 1, // iphone
+    scale: 1, // iphone
     select: 1,
     submit: 1,
     scroll: 1,
+    targetTouches: 1, // iphone
     textInput: 1,
+    touches: 1, // iphone
     unload: 1
 };
 
