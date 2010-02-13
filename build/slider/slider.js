44d3a76c8ad3c617d17b975fa5994ba0296bfad9
Luke Smith <lsmith@yahoo-inc.com>
2010-02-12 22:57:58 -0800

    Conflict resolution

--- build/slider/slider.js
+++ build/slider/slider.js
@@ -2,5 +2,4 @@ YUI.add('slider', function(Y) {
 
 Y.Slider = Y.Base.build( 'slider', Y.SliderBase, [ Y.IntValueRange, Y.ClickableRail ], { dynamic: true } );
 
-
 }, '@VERSION@' ,{requires:['slider-base', 'clickable-rail', 'int-value-range']});
