49da34d5fa322274d97228b967ca96ad37037fb5
Luke Smith <lsmith@yahoo-inc.com>
2010-02-13 00:38:00 -0800

    Changed the code layout a bit, and removed duplicate dependencies.

--- build/slider/slider.js
+++ build/slider/slider.js
@@ -1,5 +1,25 @@
 YUI.add('slider', function(Y) {
 
-Y.Slider = Y.Base.build( 'slider', Y.SliderBase, [ Y.IntValueRange, Y.ClickableRail ], { dynamic: true } );
+/**
+ * Create a sliding value range input visualized as a draggable thumb on a
+ * background rail element.
+ * 
+ * @module slider
+ */
+
+/**
+ * Create a slider to represent an integer value between a given minimum and
+ * maximum.  Sliders may be aligned vertically or horizontally, based on the
+ * <code>axis</code> configuration.
+ *
+ * @class Slider
+ * @extends Widget
+ * @param config {Object} Configuration object
+ * @constructor
+ */
+Y.Slider = Y.Base.build( 'slider', Y.SliderBase,
+    [ Y.IntValueRange, Y.ClickableRail ],
+    { dynamic: true } );
+
 
 }, '@VERSION@' ,{requires:['slider-base', 'clickable-rail', 'int-value-range']});
