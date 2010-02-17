5975621eab532c650550adb19dec27aecb643bff
Satyen Desai <sdesai@yahoo-inc.com>
2010-02-16 23:43:40 -0800

    Fixed build for lang rollups. Currently adding en, en-US, fr-FR and ko-KR for integration testing. Need to figure out if we want all bundles committed to the yui3 repo, or stored elsewhere. In the case of datatype, lang build results in 500+ lang files with rollups and min versions

--- build/datatype/datatype-date-format.js
+++ build/datatype/datatype-date-format.js
@@ -68,7 +68,7 @@ Y.config.dateFormat = Y.config.dateFormat || "%Y-%m-%d";
  * @property locale
  * @type String
  * @value "en"
- */
+*/
 Y.config.locale = Y.config.locale || "en";
 
 var Dt = {
@@ -443,4 +443,4 @@ Y.DataType.Date.Locale["en-AU"] = Y.merge(YDateEn);
 
 
 
-}, '@VERSION@' ,{lang:['ar','ar-JO','ca','ca-ES','da','da-DK','de','de-AT','de-DE','el','el-GR','en','en-AU','en-CA','en-GB','en-GY','en-IE','en-IN','en-JO','en-MY','en-NZ','en-PH','en-SG','en-US','es','es-AR','es-BO','es-CL','es-CO','es-EC','es-ES','es-MX','es-PE','es-PY','es-US','es-UY','es-VE','fi','fi-FI','fr','fr-BE','fr-CA','fr-FR','fr-GF','hi','hi-IN','id','id-ID','it','it-IT','ja','ja-JP','ko','ko-KR','ms','ms-MY','nb','nb-NO','nl','nl-BE','nl-NL','nl-SR','pl','pl-PL','pt','pt-BR','ro','ro-RO','ru','ru-RU','sv','sv-SE','th','th-TH','tr','tr-TR','vi','vi-VN','zh-Hans','zh-Hans-CN','zh-Hant','zh-Hant-HK','zh-Hant-TW']});
+}, '@VERSION@' ,{lang:['en', 'en-US', 'fr-FR', 'ko-KR']});
