65e7a38419598170b7db321f1e3cccc8275a8dcc
Adam Moore <adamoore@yahoo-inc.com>
2010-03-04 13:45:48 -0800

    More 2in3 tests

--- src/loader/template/meta.js
+++ src/loader/template/meta.js
@@ -2,15 +2,15 @@
 var VERSION         = Y.version,
     BUILD           = '/build/',
     ROOT            = VERSION + BUILD,
-
+    CDN             = 'http://yui.yahooapis.com/',
     GALLERY_VERSION = Y.config.gallery || 'gallery-2010.03.02-18',
     GALLERY_ROOT    = GALLERY_VERSION + BUILD,
     YUI2_VERSION    = Y.config.yui2 || '2.8.0_test4',
     YUI2_ROOT       = '2in3/' + YUI2_VERSION + BUILD,
-    COMBO_BASE      = 'http://yui.yahooapis.com/combo?',
+    COMBO_BASE      = CDN + 'combo?',
     META =          { version:   VERSION,
                       root:      ROOT,
-                      base:      'http://yui.yahooapis.com/' + ROOT,
+                      base:      CDN + ROOT,
                       comboBase: COMBO_BASE,
                       skin:      { defaultSkin: 'sam',
                                    base:        'assets/skins/',
@@ -21,12 +21,13 @@ var VERSION         = Y.version,
                                                   'cssfonts-context' ] },
                       groups:    {},
                       modules:   { /* METAGEN */ },
-                      patterns:  {}                                     };
+                      patterns:  {}                                     },
+    groups =          META.groups;
 
-META.groups[VERSION] = {};
+groups[VERSION] = {};
 
-META.groups.gallery = {
-    base:      'http://yui.yahooapis.com/' + GALLERY_ROOT,
+groups.gallery = {
+    base:      CDN + GALLERY_ROOT,
     ext:       false,
     combine:   true,
     root:      GALLERY_ROOT,
@@ -34,8 +35,8 @@ META.groups.gallery = {
     patterns:  { 'gallery-': {} }
 };
 
-META.groups.yui2 = {
-    base:      'http://yui.yahooapis.com/' + YUI2_ROOT,
+groups.yui2 = {
+    base:      CDN + YUI2_ROOT,
     combine:   true,
     // base:      '/2in3/',
     ext:       false,
@@ -44,20 +45,9 @@ META.groups.yui2 = {
     patterns:  { 
         'yui2-': {
             configFn: function(me) {
-                // var path = me.path;
-                // path = path.replace(/yui2-/g, '');
-                // path = path.replace(/core\//, '/');
-                // path = path.replace(/core-min/, '_core-min');
-                // path = path.replace(/^simple/, '');
-                // me.path = path;
                 if(/-skin|reset|fonts|grids|base/.test(me.name)) {
                     me.type = 'css';
                     me.path = me.path.replace(/\.js/, '.css');
-                    // var path = me.path;
-                    // path = path.replace(/\.js/, '.css');
-                    // path = path.replace(/\/yui2-skin/, '/assets/skins/sam/yui2-skin');
-                    // me.path = path;
-                    // return false;
                 }
             }
         } 
