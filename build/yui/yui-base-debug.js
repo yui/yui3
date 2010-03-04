2659f65a0425d98b674a33579167696396019630
Adam Moore <adamoore@yahoo-inc.com>
2010-03-04 08:59:58 -0800

    Testing intrinsic yui2 loading via the 2in3 project.

--- build/yui/yui-base-debug.js
+++ build/yui/yui-base-debug.js
@@ -145,7 +145,6 @@ YUI.prototype = {
             Env   = Y.Env;
 
         Y.version = VERSION;
-        Y.gallery = 'gallery-2010.02.22-22'; // @TODO build time
 
         if (!Env) {
             Y.Env = {
@@ -1080,8 +1079,14 @@ Y.log('This instance is not provisioned to fetch missing modules: ' + missing, '
  */
 
 /**
- * The default gallery version to create gallery module urls
+ * The default gallery version to build gallery module urls
  * @property gallery
+ * @type string
+ */
+
+/**
+ * The default yui2 version to build yui2 module urls
+ * @property yui2 
  * @type string
  */
 
--- src/loader/template/meta.js
+++ src/loader/template/meta.js
@@ -1,10 +1,13 @@
 (function() {
 var VERSION         = Y.version,
-    ROOT            = VERSION + '/build/',
-    GALLERY_VERSION = Y.config.gallery || Y.gallery,
-    GALLERY_ROOT    = GALLERY_VERSION + '/build/',
+    BUILD           = '/build/',
+    ROOT            = VERSION + BUILD,
+
+    GALLERY_VERSION = Y.config.gallery || 'gallery-2010.03.02-18',
+    GALLERY_ROOT    = GALLERY_VERSION + BUILD,
+    YUI2_VERSION    = Y.config.yui2 || '2.8.0_test4',
+    YUI2_ROOT       = '2in3/' + YUI2_VERSION + BUILD,
     COMBO_BASE      = 'http://yui.yahooapis.com/combo?',
-    GALLERY_BASE    = 'http://yui.yahooapis.com/' + GALLERY_ROOT,
     META =          { version:   VERSION,
                       root:      ROOT,
                       base:      'http://yui.yahooapis.com/' + ROOT,
@@ -23,12 +26,42 @@ var VERSION         = Y.version,
 META.groups[VERSION] = {};
 
 META.groups.gallery = {
-    base:      GALLERY_BASE,
+    base:      'http://yui.yahooapis.com/' + GALLERY_ROOT,
     ext:       false,
     combine:   true,
     root:      GALLERY_ROOT,
     comboBase: COMBO_BASE,
     patterns:  { 'gallery-': {} }
+};
+
+META.groups.yui2 = {
+    base:      'http://yui.yahooapis.com/' + YUI2_ROOT,
+    combine:   true,
+    // base:      '/2in3/',
+    ext:       false,
+    root:      YUI2_ROOT,
+    comboBase: COMBO_BASE,
+    patterns:  { 
+        'yui2-': {
+            configFn: function(me) {
+                // var path = me.path;
+                // path = path.replace(/yui2-/g, '');
+                // path = path.replace(/core\//, '/');
+                // path = path.replace(/core-min/, '_core-min');
+                // path = path.replace(/^simple/, '');
+                // me.path = path;
+                if(/-skin|reset|fonts|grids|base/.test(me.name)) {
+                    me.type = 'css';
+                    me.path = me.path.replace(/\.js/, '.css');
+                    // var path = me.path;
+                    // path = path.replace(/\.js/, '.css');
+                    // path = path.replace(/\/yui2-skin/, '/assets/skins/sam/yui2-skin');
+                    // me.path = path;
+                    // return false;
+                }
+            }
+        } 
+    }
 };
 
 YUI.Env[VERSION] = META;
