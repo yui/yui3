b40f5113b5f28daabc29b6ad49c905f290cf6cb7
Luke Smith <lsmith@yahoo-inc.com>
2010-03-24 21:47:26 -0700

    Moved test suites into modules for arraylist, event-synthetic, console, console-filters, async-queue, slider, stylesheet, and json.  Consolidated testing in sandbox/unittest/all.html.  Individual test pages still in src/(component)/tests/(component).html.  More work to be done on the testing infrastructure, including addressing requisite markup or styles, possibly incorporating performance testing, isolating test env for each test suite, and possibly abstracting out YUI Test API in favor of meta-only structures translated into Y.Test.Suite/Cases

---
+++ sandbox/collection/tests/arraylist-test.js
@@ -0,0 +1,88 @@
+var suite = new Y.Test.Suite("Y.ArrayList");
+
+suite.add( new Y.Test.Case({
+    name: "Lifecycle",
+
+    "construct with array should not error": function () {
+    },
+
+    "construct after augmented class instantiation should not clobber items": function () {
+    }
+}));
+
+suite.add( new Y.Test.Case({
+    name: "API",
+
+    setUp: function () {
+    },
+
+    "test item": function () {
+    },
+
+    "test each": function () {
+    },
+
+    "test some": function () {
+    },
+
+    "test indexOf": function () {
+    },
+
+    "test size": function () {
+    },
+
+    "test isEmpty": function () {
+    },
+
+    "test _item": function () {
+    }
+}));
+
+suite.add( new Y.Test.Case({
+    name: "Additional API",
+
+    setUp: function () {
+    },
+
+    "test add": function () {
+    },
+
+    "test remove": function () {
+    },
+
+    "test filter": function () {
+    }
+}));
+
+suite.add( new Y.Test.Case({
+    name: "addMethod",
+
+    "test addMethod": function () {
+    },
+
+    "methods should be chainable unless a value is returned": function () {
+    }
+}));
+
+suite.add( new Y.Test.Case({
+    name: "Array.invoke",
+
+    "test Y.Array.invoke": function () {
+    }
+}));
+
+suite.add( new Y.Test.Case({
+    name: "Runtime expectations",
+
+    "test ": function () {
+    }
+}));
+
+suite.add( new Y.Test.Case({
+    name: "Bugs",
+
+    "test ": function () {
+    }
+}));
+
+Y.Test.Runner.add( suite );
