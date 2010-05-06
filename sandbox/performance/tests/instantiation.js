cb3f11812d68ab239c2593d969c93e4b945bc57b
Ryan Grove <ryan@wonko.com>
2010-05-05 17:47:30 -0700

    Add YUI 2 comparison test and more comments.

--- sandbox/performance/tests/instantiation.js
+++ sandbox/performance/tests/instantiation.js
@@ -3,21 +3,38 @@ YUI.add('perf-instantiation', function (Y) {
 // Object hash mapping test names to test objects. A test object may have the
 // following properties, of which only the "test" property is required:
 //
+// iterations (Number):
+//   Number of iterations to run.
+//
+// noBootstrap (Boolean):
+//   By default, YUI3 core and Loader are bootstrapped into each test sandbox.
+//   Set this to true to start with a pristine sandbox.
+//
 // setup (Function):
-//   Setup function to execute before each iteration of the test.
+//   Setup function to execute before each iteration of the test. Runs in the
+//   same sandbox as the test.
 //
 // teardown (Function):
-//   Teardown function to execute after each iteration of the test.
+//   Teardown function to execute after each iteration of the test. Runs in the
+//   same sandbox as the test.
 //
 // test (Function):
 //   The test itself, each iteration of which will be timed. In order to make
-//   async execution testable, the test function needs to set this.done = true
+//   async execution testable, the test function needs to call profileStop()
 //   when it's completely finished; until this happens, the next test cannot
 //   run and the profile timer will keep ticking.
 //
+// useStrictSandbox (Boolean):
+//   By default, each test gets a single sandbox which is reused for all
+//   iterations of that test. Set this to true if you want a brand new sandbox
+//   to be created for each iteration of the test.
+//
+// warmup (Boolean):
+//   If this is true, the test will be run once (and timing data discarded) as a
+//   warmup before the main iterations begin.
+//
 Y.Performance.addTests({
-
-    "yui-min.js parse/execution time": {
+    "YUI3: yui-min.js + loader-min.js parsing/execution": {
         iterations: 10,
 
         // Don't bootstrap YUI by default.
@@ -27,7 +44,35 @@ Y.Performance.addTests({
         useStrictSandbox: true,
 
         setup: function () {
-            window.yuiScript = sandbox.xhrGet('../../build/yui/yui-min.js');
+            var yuiJS    = sandbox.xhrGet('../../build/yui/yui-min.js'),
+                loaderJS = sandbox.xhrGet('../../build/loader/loader-min.js');
+
+            if (yuiJS && loaderJS) {
+                window.yuiScript = yuiJS + "\n" + loaderJS;
+            } else {
+                throw new Error('Failed to load yui-min.js and/or loader-min.js.');
+            }
+        },
+
+        test: function () {
+            eval(yuiScript);
+            profileStop();
+        }
+    },
+
+    "YUI2: yuiloader-dom-event.js parsing/execution": {
+        iterations: 10,
+        noBootstrap: true,
+        useStrictSandbox: true,
+
+        setup: function () {
+            var yuiLoaderJS = sandbox.xhrGet('assets/yui2/yuiloader-dom-event.js');
+
+            if (yuiLoaderJS) {
+                window.yuiScript = yuiLoaderJS;
+            } else {
+                throw new Error('Failed to load yuiloder-dom-event.js.');
+            }
         },
 
         test: function () {
