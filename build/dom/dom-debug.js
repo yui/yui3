68655ace454e24733da45b5ec4caf68f9514eda9
Matt Sweeney <matt.sweeney@yahoo.com>
2010-05-12 13:22:16 -0700

    added comment

--- build/dom/dom-debug.js
+++ build/dom/dom-debug.js
@@ -297,7 +297,7 @@ Y.DOM = {
 
             if (nodes.length === 1) { // return single node, breaking parentNode ref from "fragment"
                 ret = nodes[0].parentNode.removeChild(nodes[0]);
-            } else if (nodes[0] && nodes[0].className === 'yui3-big-dummy') {
+            } else if (nodes[0] && nodes[0].className === 'yui3-big-dummy') { // using dummy node to preserve some attributes (e.g. OPTION not selected)
                 if (nodes.length === 2) {
                     ret = nodes[0].nextSibling;
                 } else {
