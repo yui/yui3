ed22be76b9f8fc34b800159011745aec06ca7690
Ryan Grove <ryan@wonko.com>
2010-06-11 12:37:17 -0700

    Add a /proxy route to serve as an XDR request proxy.

--- sandbox/performance/perf-server.js
+++ sandbox/performance/perf-server.js
@@ -5,11 +5,12 @@
  * See the README for details.
  */
 
-var fs     = require('fs'),
-    http   = require('http'),
-    mime   = require('./lib/mime'),
-    path   = require('path'),
-    sys    = require('sys'),
+var fs       = require('fs'),
+    http     = require('http'),
+    mime     = require('./lib/mime'),
+    parseURL = require('url').parse,
+    path     = require('path'),
+    sys      = require('sys'),
 
     config = readConfig('conf/config.json'),
     server = require('./lib/router').Server();
@@ -49,7 +50,63 @@ server.get('/combo/([^/]+)/?', function (root) {
     // Content-Type. Need to look into what the real ComboHandler does when
     // multiple file types are requested in a single request.
     this.res.headers['content-type'] = mimeType + ';charset=utf-8';
-    return response + "\n";
+    this.end(response + "\n");
+});
+
+// Cross-domain request proxy. Currently this doesn't handle redirects of any
+// kind. This is still a little rough, but it works.
+server.get('/proxy/?', function () {
+    var client,
+        request,
+        result = '',
+        that   = this,
+        url    = this.query.url;
+
+    if (!url) {
+        this.sendError(400, '400 Bad Request', 'Missing required "url" parameter.');
+    }
+
+    url = parseURL(url);
+
+    if (!url.hostname) {
+        url.port     = config.server.port;
+        url.hostname = config.server.host;
+        url.host     = url.hostname + ':' + url.port;
+    }
+
+    if (!url.pathname) {
+        url.pathname = '/';
+    }
+
+    if (url.pathname.charAt(0) !== '/') {
+        url.pathname = '/' + url.pathname;
+    }
+
+    client  = http.createClient(url.port || 80, url.hostname, url.protocol === 'https:');
+    request = client.request((url.pathname || '/') + (url.search || ''), {host: url.host});
+
+    request.addListener('response', function (response) {
+        var header;
+
+        // Copy status and headers from the remote response to our response.
+        that.res.status = response.statusCode;
+
+        for (header in response.headers) {
+            if (response.headers.hasOwnProperty(header)) {
+                that.res.headers[header.toLowerCase()] = response.headers[header];
+            }
+        }
+
+        response.addListener('data', function (chunk) {
+            result += chunk;
+        });
+
+        response.addListener('end', function () {
+            that.end(result);
+        });
+    });
+
+    request.end();
 });
 
 server.listen(config.server.port, config.server.host);
