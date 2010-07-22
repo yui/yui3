5730c580272fea68b11654e327c309e2df1b5ec5
Satyen Desai <sdesai@yahoo-inc.com>
2010-07-22 15:39:26 -0700

    APIDoc tweak

--- src/scrollview/js/paginator-plugin.js
+++ src/scrollview/js/paginator-plugin.js
@@ -11,6 +11,7 @@ var BOUNCE_DECELERATION_CONST = 0.5,
  * Scrollview plugin that adds support for paging
  *
  * @class ScrollViewPaginatorPlugin
+ * @namepace Plugin
  * @extends Plugin.Base 
  * @constructor
  */
