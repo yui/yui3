e6abb8a9590d9f4058669f9d3d90a3d22b5449e6
Dav Glass <davglass@gmail.com>
2010-09-01 14:02:26 -0500

    Editor API doc update

--- build/editor/editor.js
+++ build/editor/editor.js
@@ -1994,7 +1994,7 @@ YUI.add('editor-tab', function(Y) {
 YUI.add('createlink-base', function(Y) {
 
     /**
-     * Adds prompt style link creation. Adds an override for the <a href="Plugin.ExecCommand.html#method_COMMANDS.createlink">createlink execCommand</a>.
+     * Base class for Editor. Handles the business logic of Editor, no GUI involved only utility methods and events.
      * @module editor
      * @submodule createlink-base
      */     
