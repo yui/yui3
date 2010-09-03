14252554241c668e31a0036c8b84e5f031e466c0
Matt Sweeney <matt.sweeney@yahoo.com>
2010-09-03 11:15:41 -0700

    revert to anim-easing requiring anim-base [fixes 2529248]

--- build/loader/loader-debug.js
+++ build/loader/loader-debug.js
@@ -2201,7 +2201,9 @@ YUI.Env[Y.version].modules = YUI.Env[Y.version].modules || {
                 ]
             }, 
             "anim-easing": {
-                "requires": []
+                "requires": [
+                    "anim-base"
+                ]
             }, 
             "anim-node-plugin": {
                 "requires": [
@@ -3462,7 +3464,7 @@ YUI.Env[Y.version].modules = YUI.Env[Y.version].modules || {
         }
     }
 };
-YUI.Env[Y.version].md5 = 'eef930e983dcf433c78baf3bd96ef4e0';
+YUI.Env[Y.version].md5 = 'cb1db98536fb15d179c178aab4be9340';
 
 
 }, '@VERSION@' ,{requires:['loader-base']});
