7a2291f10e1d1fd34505c9b7c496572e24c30ca8
TSHA <tsha@yahoo-inc.com>
2010-09-07 20:15:32 -0700

    Fix 2529290 [rainhead pull request]

--- build/io/io-xdr-debug.js
+++ build/io/io-xdr-debug.js
@@ -37,12 +37,12 @@ YUI.add('io-xdr', function(Y) {
     */
     _rS = {},
 
-    ie = w && w.XDomainRequest,
-
     // Document reference
     d = Y.config.doc,
     // Window reference
-    w = Y.config.win;
+    w = Y.config.win,
+	// IE8 cross-origin request detection
+    ie = w && w.XDomainRequest;
 
    /**
     * @description Method that creates the Flash transport swf.
