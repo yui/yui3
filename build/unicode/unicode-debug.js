07b4ee66f1da22dbe6ff220c7e572bacd33c8214
Ryan Grove <ryan@wonko.com>
2010-09-09 17:47:39 -0700

    Build highlight-* and unicode-*.

--- build/unicode/unicode-debug.js
+++ build/unicode/unicode-debug.js
@@ -117,11 +117,11 @@ AccentFold = {
      *
      * @method fold
      * @param {String|Array} input String or array of strings to be folded.
-     * @return {String|Array} Folded string or array of string.
+     * @return {String|Array} Folded string or array of strings.
      * @static
      */
     fold: function (input) {
-        if (YArray.test(input)) {
+        if (Y.Lang.isArray(input)) {
             return YArray.map(input, AccentFold.fold);
         }
 
@@ -169,43 +169,42 @@ YUI.add('unicode-data-accentfold', function(Y) {
 // http://unicode.org/cldr/utility/list-unicodeset.jsp?a=[:toNFKD%3D/^a/:]&abb=on
 
 Y.namespace('Unicode.Data').AccentFold = {
-    0:  /[⁰₀⓪０]/gi,
-    1:  /[¹₁①１]/gi,
-    2:  /[²₂②２]/gi,
-    3:  /[³₃③３]/gi,
-    4:  /[⁴₄④４]/gi,
-    5:  /[⁵₅⑤５]/gi,
-    6:  /[⁶₆⑥６]/gi,
-    7:  /[⁷₇⑦７]/gi,
-    8:  /[⁸₈⑧８]/gi,
-    9:  /[⁹₉⑨９]/gi,
-    a:  /[ªà-åāăąǎǟǡǻȁȃȧᵃḁẚạảấầẩẫậắằẳẵặⓐａ]/gi,
-    ae: /[æǣǽӕ]/gi, // http://unicode.org/cldr/utility/list-unicodeset.jsp?a=[:name%3D/\b(AE|A+IE)\b/:]&abb=on
-    b:  /[ᵇḃḅḇⓑｂ]/gi,
-    c:  /[çćĉċčᶜḉⓒｃ]/gi,
-    d:  /[ďᵈḋḍḏḑḓⅾⓓｄ]/gi,
-    e:  /[è-ëēĕėęěȅȇȩᵉḕḗḙḛḝẹẻẽếềểễệₑℯⓔｅ]/gi,
-    f:  /[ᶠḟⓕｆ]/gi,
-    g:  /[ĝğġģǧǵᵍḡℊⓖｇ]/gi,
-    h:  /[ĥȟʰḣḥḧḩḫẖℎⓗｈ]/gi,
-    i:  /[ì-ïĩīĭįĳǐȉȋᵢḭḯỉịⁱℹⅰⓘｉ]/gi,
-    j:  /[ĵǰʲⓙⱼｊ]/gi,
-    k:  /[ķǩᵏḱḳḵⓚｋ]/gi,
-    l:  /[ĺļľŀǉˡḷḹḻḽℓⅼⓛｌ]/gi,
-    m:  /[ᵐḿṁṃⅿⓜｍ]/gi,
-    n:  /[ñńņňǹṅṇṉṋⁿⓝｎ]/gi,
-    o:  /[ºò-öōŏőơǒǫǭȍȏȫȭȯȱᵒṍṏṑṓọỏốồổỗộớờởỡợₒℴⓞｏ]/gi,
-    p:  /[ᵖṕṗⓟｐ]/gi,
-    q:  /[ʠⓠｑ]/gi,
-    r:  /[ŕŗřȑȓʳᵣṙṛṝṟⓡｒ]/gi,
-    s:  /[śŝşšſșˢṡṣṥṧṩẛⓢｓ]/gi,
-    t:  /[ţťțᵗṫṭṯṱẗⓣｔ]/gi,
-    u:  /[ù-üũūŭůűųưǔǖǘǚǜȕȗᵘᵤṳṵṷṹṻụủứừửữựⓤｕ]/gi,
-    v:  /[ᵛᵥṽṿⅴⓥｖ]/gi,
-    w:  /[ŵʷẁẃẅẇẉẘⓦｗ]/gi,
-    x:  /[ˣẋẍₓⅹⓧｘ]/gi,
-    y:  /[ýÿŷȳʸẏẙỳỵỷỹⓨｙ]/gi,
-    z:  /[źżžᶻẑẓẕⓩｚ]/gi
+    0: /[⁰₀⓪０]/gi,
+    1: /[¹₁①１]/gi,
+    2: /[²₂②２]/gi,
+    3: /[³₃③３]/gi,
+    4: /[⁴₄④４]/gi,
+    5: /[⁵₅⑤５]/gi,
+    6: /[⁶₆⑥６]/gi,
+    7: /[⁷₇⑦７]/gi,
+    8: /[⁸₈⑧８]/gi,
+    9: /[⁹₉⑨９]/gi,
+    a: /[ªà-åāăąǎǟǡǻȁȃȧᵃḁẚạảấầẩẫậắằẳẵặⓐａ]/gi,
+    b: /[ᵇḃḅḇⓑｂ]/gi,
+    c: /[çćĉċčᶜḉⓒｃ]/gi,
+    d: /[ďᵈḋḍḏḑḓⅾⓓｄ]/gi,
+    e: /[è-ëēĕėęěȅȇȩᵉḕḗḙḛḝẹẻẽếềểễệₑℯⓔｅ]/gi,
+    f: /[ᶠḟⓕｆ]/gi,
+    g: /[ĝğġģǧǵᵍḡℊⓖｇ]/gi,
+    h: /[ĥȟʰḣḥḧḩḫẖℎⓗｈ]/gi,
+    i: /[ì-ïĩīĭįĳǐȉȋᵢḭḯỉịⁱℹⅰⓘｉ]/gi,
+    j: /[ĵǰʲⓙⱼｊ]/gi,
+    k: /[ķǩᵏḱḳḵⓚｋ]/gi,
+    l: /[ĺļľŀǉˡḷḹḻḽℓⅼⓛｌ]/gi,
+    m: /[ᵐḿṁṃⅿⓜｍ]/gi,
+    n: /[ñńņňǹṅṇṉṋⁿⓝｎ]/gi,
+    o: /[ºò-öōŏőơǒǫǭȍȏȫȭȯȱᵒṍṏṑṓọỏốồổỗộớờởỡợₒℴⓞｏ]/gi,
+    p: /[ᵖṕṗⓟｐ]/gi,
+    q: /[ʠⓠｑ]/gi,
+    r: /[ŕŗřȑȓʳᵣṙṛṝṟⓡｒ]/gi,
+    s: /[śŝşšſșˢṡṣṥṧṩẛⓢｓ]/gi,
+    t: /[ţťțᵗṫṭṯṱẗⓣｔ]/gi,
+    u: /[ù-üũūŭůűųưǔǖǘǚǜȕȗᵘᵤṳṵṷṹṻụủứừửữựⓤｕ]/gi,
+    v: /[ᵛᵥṽṿⅴⓥｖ]/gi,
+    w: /[ŵʷẁẃẅẇẉẘⓦｗ]/gi,
+    x: /[ˣẋẍₓⅹⓧｘ]/gi,
+    y: /[ýÿŷȳʸẏẙỳỵỷỹⓨｙ]/gi,
+    z: /[źżžᶻẑẓẕⓩｚ]/gi
 };
 
 
