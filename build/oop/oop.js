d2db9b0a607539e9e9843a6de10ec6202b5b272f
Adam Moore <adamoore@yahoo-inc.com>
2010-09-22 14:52:42 -0700

    lint cleanup

--- build/oop/oop.js
+++ build/oop/oop.js
@@ -8,10 +8,15 @@ YUI.add('oop', function(Y) {
  * @module oop
  */
 
-    var L  = Y.Lang, 
-        A  = Y.Array,
+/**
+ * The following methods are added to the YUI instance
+ * @class YUI~oop
+ */
+
+    var L = Y.Lang,
+        A = Y.Array,
         OP = Object.prototype,
-        CLONE_MARKER = "_~yuim~_",
+        CLONE_MARKER = '_~yuim~_',
         EACH = 'each',
         SOME = 'some',
 
@@ -30,54 +35,51 @@ YUI.add('oop', function(Y) {
             }
         };
 
-    /**
-     * The following methods are added to the YUI instance
-     * @class YUI~oop
-     */
 
     /**
      * Applies prototype properties from the supplier to the receiver.
      * The receiver can be a constructor or an instance.
      * @method augment
-     * @param {Function} r  the object to receive the augmentation
-     * @param {Function} s  the object that supplies the properties to augment
-     * @param ov {boolean} if true, properties already on the receiver
+     * @param {function} r  the object to receive the augmentation.
+     * @param {function} s  the object that supplies the properties to augment.
+     * @param {boolean} ov if true, properties already on the receiver
      * will be overwritten if found on the supplier.
-     * @param wl {string[]} a whitelist.  If supplied, only properties in 
+     * @param {string[]} wl  a whitelist.  If supplied, only properties in
      * this list will be applied to the receiver.
-     * @param args {Array | Any} arg or arguments to apply to the supplier
+     * @param {Array | Any} args arg or arguments to apply to the supplier
      * constructor when initializing.
-     * @return {object} the augmented object
+     * @return {object} the augmented object.
      *
      * @todo constructor optional?
      * @todo understanding what an instance is augmented with
-     * @TODO best practices for overriding sequestered methods.
+     * @todo best practices for overriding sequestered methods.
      */
     Y.augment = function(r, s, ov, wl, args) {
-        var sProto           = s.prototype, 
-            newProto         = null, 
-            construct        = s, 
-            a                = (args) ? Y.Array(args) : [], 
-            rProto           = r.prototype, 
-            target           = rProto || r, 
+        var sProto = s.prototype,
+            newProto = null,
+            construct = s,
+            a = (args) ? Y.Array(args) : [],
+            rProto = r.prototype,
+            target = rProto || r,
             applyConstructor = false,
             sequestered, replacements;
 
         // working on a class, so apply constructor infrastructure
         if (rProto && construct) {
-            sequestered  = {};
+            sequestered = {};
             replacements = {};
-            newProto     = {};
+            newProto = {};
 
             // sequester all of the functions in the supplier and replace with
             // one that will restore all of them.
             Y.Object.each(sProto, function(v, k) {
                 replacements[k] = function() {
 
-// overwrite the prototype with all of the sequestered functions,
-// but only if it hasn't been overridden
-                    for (var i in sequestered) {
-                        if (sequestered.hasOwnProperty(i) && (this[i] === replacements[i])) {
+            // overwrite the prototype with all of the sequestered functions,
+            // but only if it hasn't been overridden
+                        for (var i in sequestered) {
+                        if (sequestered.hasOwnProperty(i) &&
+                                (this[i] === replacements[i])) {
                             this[i] = sequestered[i];
                         }
                     }
@@ -124,13 +126,13 @@ YUI.add('oop', function(Y) {
      * object will be augmented with the supplier's value.  If the property
      * is an array, the suppliers value will be appended to the target.
      * @method aggregate
-     * @param {Function} r  the object to receive the augmentation
-     * @param {Function} s  the object that supplies the properties to augment
-     * @param ov {boolean} if true, properties already on the receiver
+     * @param {function} r  the object to receive the augmentation.
+     * @param {function} s  the object that supplies the properties to augment.
+     * @param {boolean} ov if true, properties already on the receiver
      * will be overwritten if found on the supplier.
-     * @param wl {string[]} a whitelist.  If supplied, only properties in 
+     * @param {string[]} wl a whitelist.  If supplied, only properties in
      * this list will be applied to the receiver.
-     * @return the extended object
+     * @return {object} the extended object.
      */
     Y.aggregate = function(r, s, ov, wl) {
         return Y.mix(r, s, ov, wl, 0, true);
@@ -142,29 +144,29 @@ YUI.add('oop', function(Y) {
      * Static members will not be inherited.
      *
      * @method extend
-     * @param {Function} r   the object to modify
-     * @param {Function} s the object to inherit
-     * @param {Object} px prototype properties to add/override
-     * @param {Object} sx static properties to add/override
-     * @return the extended object
+     * @param {function} r   the object to modify.
+     * @param {function} s the object to inherit.
+     * @param {object} px prototype properties to add/override.
+     * @param {object} sx static properties to add/override.
+     * @return {object} the extended object.
      */
     Y.extend = function(r, s, px, sx) {
-        if (!s||!r) {
+        if (!s || !r) {
             // @TODO error symbols
-            Y.error("extend failed, verify dependencies");
+            Y.error('extend failed, verify dependencies');
         }
 
-        var sp = s.prototype, rp=Y.Object(sp);
-        r.prototype=rp;
+        var sp = s.prototype, rp = Y.Object(sp);
+        r.prototype = rp;
 
-        rp.constructor=r;
-        r.superclass=sp;
+        rp.constructor = r;
+        r.superclass = sp;
 
         // assign constructor property
         if (s != Object && sp.constructor == OP.constructor) {
-            sp.constructor=s;
+            sp.constructor = s;
         }
-    
+
         // add prototype overrides
         if (px) {
             Y.mix(rp, px, true);
@@ -183,29 +185,32 @@ YUI.add('oop', function(Y) {
      * a collection.  Supports arrays, objects, and
      * Y.NodeLists
      * @method each
-     * @param o the object to iterate
-     * @param f the function to execute.  This function
-     * receives the value, key, and object as parameters
-     * @param proto if true, prototype properties are
-     * iterated on objects
-     * @return {YUI} the YUI instance
+     * @param {object} o the object to iterate.
+     * @param {function} f the function to execute.  This function
+     * receives the value, key, and object as parameters.
+     * @param {object} c the execution context for the function.
+     * @param {boolean} proto if true, prototype properties are
+     * iterated on objects.
+     * @return {YUI} the YUI instance.
      */
     Y.each = function(o, f, c, proto) {
         return dispatch(o, f, c, proto, EACH);
     };
 
-    /*
+    /**
      * Executes the supplied function for each item in
      * a collection.  The operation stops if the function
      * returns true. Supports arrays, objects, and
      * Y.NodeLists.
      * @method some
-     * @param o the object to iterate
-     * @param f the function to execute.  This function
-     * receives the value, key, and object as parameters
-     * @param proto if true, prototype properties are
-     * iterated on objects
-     * @return {boolean} true if the function ever returns true, false otherwise
+     * @param {object} o the object to iterate.
+     * @param {function} f the function to execute.  This function
+     * receives the value, key, and object as parameters.
+     * @param {object} c the execution context for the function.
+     * @param {boolean} proto if true, prototype properties are
+     * iterated on objects.
+     * @return {boolean} true if the function ever returns true,
+     * false otherwise.
      */
     Y.some = function(o, f, c, proto) {
         return dispatch(o, f, c, proto, SOME);
@@ -220,21 +225,23 @@ YUI.add('oop', function(Y) {
      * filter keys, validate values, etc.
      *
      * @method clone
-     * @param o what to clone
-     * @param safe {boolean} if true, objects will not have prototype
+     * @param {object} o what to clone.
+     * @param {boolean} safe if true, objects will not have prototype
      * items from the source.  If false, they will.  In this case, the
-     * original is initially protected, but the clone is not completely immune
-     * from changes to the source object prototype.  Also, cloned prototype
-     * items that are deleted from the clone will result in the value
-     * of the source prototype being exposed.  If operating on a non-safe
-     * clone, items should be nulled out rather than deleted.
-     * @param f optional function to apply to each item in a collection;
-     *          it will be executed prior to applying the value to
-     *          the new object.  Return false to prevent the copy.
-     * @param c optional execution context for f
-     * @param owner Owner object passed when clone is iterating an
-     * object.  Used to set up context for cloned functions.
-     * @return {Array|Object} the cloned object
+     * original is initially protected, but the clone is not completely
+     * immune from changes to the source object prototype.  Also, cloned
+     * prototype items that are deleted from the clone will result
+     * in the value of the source prototype being exposed.  If operating
+     * on a non-safe clone, items should be nulled out rather than deleted.
+     * @param {function} f optional function to apply to each item in a
+     * collection; it will be executed prior to applying the value to
+     * the new object.  Return false to prevent the copy.
+     * @param {object} c optional execution context for f.
+     * @param {object} owner Owner object passed when clone is iterating
+     * an object.  Used to set up context for cloned functions.
+     * @param {object} cloned hash of previously cloned objects to avoid
+     * multiple clones.
+     * @return {Array|Object} the cloned object.
      */
     Y.clone = function(o, safe, f, c, owner, cloned) {
 
@@ -242,7 +249,7 @@ YUI.add('oop', function(Y) {
             return o;
         }
 
-        // @TODO cloning YUI instances doesn't currently work
+        // @todo cloning YUI instances doesn't currently work
         if (o instanceof YUI) {
             return o;
         }
@@ -254,7 +261,8 @@ YUI.add('oop', function(Y) {
             case 'date':
                 return new Date(o);
             case 'regexp':
-                // return new RegExp(o.source); // if we do this we need to set the flags too
+                // if we do this we need to set the flags too
+                // return new RegExp(o.source);
                 return o;
             case 'function':
                 // o2 = Y.bind(o, owner);
@@ -288,7 +296,8 @@ YUI.add('oop', function(Y) {
                         // } else if (o[k] === o) {
                         //     this[k] = this;
                         } else {
-                            this[k] = Y.clone(v, safe, f, c, owner || o, marked);
+                            this[k] =
+                                Y.clone(v, safe, f, c, owner || o, marked);
                         }
                     }
                 }
@@ -309,26 +318,28 @@ YUI.add('oop', function(Y) {
     /**
      * Returns a function that will execute the supplied function in the
      * supplied object's context, optionally adding any additional
-     * supplied parameters to the beginning of the arguments collection the 
+     * supplied parameters to the beginning of the arguments collection the
      * supplied to the function.
      *
      * @method bind
-     * @param f {Function|String} the function to bind, or a function name
-     * to execute on the context object
-     * @param c the execution context
-     * @param args* 0..n arguments to include before the arguments the 
+     * @param {Function|String} f the function to bind, or a function name
+     * to execute on the context object.
+     * @param {object} c the execution context.
+     * @param {any} args* 0..n arguments to include before the arguments the
      * function is executed with.
-     * @return {function} the wrapped function
+     * @return {function} the wrapped function.
      */
     Y.bind = function(f, c) {
-        var xargs = arguments.length > 2 ? Y.Array(arguments, 2, true) : null;
-        return function () {
-            var fn = L.isString(f) ? c[f] : f, 
-                args = (xargs) ? xargs.concat(Y.Array(arguments, 0, true)) : arguments;
+        var xargs = arguments.length > 2 ?
+                Y.Array(arguments, 2, true) : null;
+        return function() {
+            var fn = L.isString(f) ? c[f] : f,
+                args = (xargs) ?
+                    xargs.concat(Y.Array(arguments, 0, true)) : arguments;
             return fn.apply(c || fn, args);
         };
     };
-    
+
     /**
      * Returns a function that will execute the supplied function in the
      * supplied object's context, optionally adding any additional
@@ -336,18 +347,19 @@ YUI.add('oop', function(Y) {
      * is executed with.
      *
      * @method rbind
-     * @param f {Function|String} the function to bind, or a function name
-     * to execute on the context object
-     * @param c the execution context
-     * @param args* 0..n arguments to append to the end of arguments collection
-     * supplied to the function
-     * @return {function} the wrapped function
+     * @param {Function|String} f the function to bind, or a function name
+     * to execute on the context object.
+     * @param {object} c the execution context.
+     * @param {any} args* 0..n arguments to append to the end of
+     * arguments collection supplied to the function.
+     * @return {function} the wrapped function.
      */
     Y.rbind = function(f, c) {
         var xargs = arguments.length > 2 ? Y.Array(arguments, 2, true) : null;
-        return function () {
-            var fn = L.isString(f) ? c[f] : f, 
-                args = (xargs) ? Y.Array(arguments, 0, true).concat(xargs) : arguments;
+        return function() {
+            var fn = L.isString(f) ? c[f] : f,
+                args = (xargs) ?
+                    Y.Array(arguments, 0, true).concat(xargs) : arguments;
             return fn.apply(c || fn, args);
         };
     };
