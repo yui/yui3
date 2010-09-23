d0f5103c2e830cd35cb3d326fab217eddfc24828
Dav Glass <davglass@gmail.com>
2010-09-23 08:58:33 -0500

    Fixed nodeChange event for missing color/font info

--- sandbox/editor/js/editor-base.js
+++ sandbox/editor/js/editor-base.js
@@ -201,10 +201,12 @@ YUI.add('editor-base', function(Y) {
                     }
                 });
 
-                fColor = EditorBase.FILTER_RGB(s.color);
+                fColor = EditorBase.FILTER_RGB(n.getStyle('color'));
                 var bColor2 = EditorBase.FILTER_RGB(s.backgroundColor);
                 if (bColor2 !== 'transparent') {
-                    bColor = bColor2;
+                    if (bColor2 !== '') {
+                        bColor = bColor2;
+                    }
                 }
                 
             });
