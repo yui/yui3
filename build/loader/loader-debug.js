54119bda17c0eb663f4d2147df7363de8019647f
Adam Moore <adamoore@yahoo-inc.com>
2010-10-04 15:47:04 -0700

    loader correctly processes rollups when use is called multiple times [fixes #2529389].

--- build/loader/loader-debug.js
+++ build/loader/loader-debug.js
@@ -2141,22 +2141,22 @@ YUI.add('loader-rollup', function(Y) {
  * @private
  */
 Y.Loader.prototype._rollup = function() {
-    var i, j, m, s, rollups = {}, r = this.required, roll,
+    var i, j, m, s, r = this.required, roll,
         info = this.moduleInfo, rolled, c, smod;
 
     // find and cache rollup modules
     if (this.dirty || !this.rollups) {
+        this.rollups = {};
         for (i in info) {
             if (info.hasOwnProperty(i)) {
                 m = this.getModule(i);
                 // if (m && m.rollup && m.supersedes) {
                 if (m && m.rollup) {
-                    rollups[i] = m;
+                    this.rollups[i] = m;
                 }
             }
         }
 
-        this.rollups = rollups;
         this.forceMap = (this.force) ? Y.Array.hash(this.force) : {};
     }
 
@@ -2165,8 +2165,8 @@ Y.Loader.prototype._rollup = function() {
         rolled = false;
 
         // go through the rollup candidates
-        for (i in rollups) {
-            if (rollups.hasOwnProperty(i)) {
+        for (i in this.rollups) {
+            if (this.rollups.hasOwnProperty(i)) {
                 // there can be only one, unless forced
                 if (!r[i] && ((!this.loaded[i]) || this.forceMap[i])) {
                     m = this.getModule(i);
