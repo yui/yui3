d6ed13752962e9de0d6dc030db3f1bcf54e6e540
Eduardo Lundgren <eduardolundgren@gmail.com>
2010-10-13 16:04:47 -0300

    #2529437 Fires tickAlignX/tickAlignY after drag:drag

--- build/dd/dd-debug.js
+++ build/dd/dd-debug.js
@@ -348,7 +348,7 @@ YUI.add('dd-ddm-base', function(Y) {
 
 
 
-}, '@VERSION@' ,{requires:['node', 'base', 'yui-throttle', 'classnamemanager'], skinnable:false});
+}, '@VERSION@' ,{skinnable:false, requires:['node', 'base', 'yui-throttle', 'classnamemanager']});
 YUI.add('dd-ddm', function(Y) {
 
 
@@ -469,7 +469,7 @@ YUI.add('dd-ddm', function(Y) {
 
 
 
-}, '@VERSION@' ,{requires:['dd-ddm-base', 'event-resize'], skinnable:false});
+}, '@VERSION@' ,{skinnable:false, requires:['dd-ddm-base', 'event-resize']});
 YUI.add('dd-ddm-drop', function(Y) {
 
 
@@ -880,7 +880,7 @@ YUI.add('dd-ddm-drop', function(Y) {
 
 
 
-}, '@VERSION@' ,{requires:['dd-ddm'], skinnable:false});
+}, '@VERSION@' ,{skinnable:false, requires:['dd-ddm']});
 YUI.add('dd-drag', function(Y) {
 
 
@@ -2109,7 +2109,7 @@ YUI.add('dd-drag', function(Y) {
 
 
 
-}, '@VERSION@' ,{requires:['dd-ddm-base'], skinnable:false});
+}, '@VERSION@' ,{skinnable:false, requires:['dd-ddm-base']});
 YUI.add('dd-proxy', function(Y) {
 
 
@@ -2357,7 +2357,7 @@ YUI.add('dd-proxy', function(Y) {
 
 
 
-}, '@VERSION@' ,{requires:['dd-ddm', 'dd-drag'], skinnable:false});
+}, '@VERSION@' ,{skinnable:false, requires:['dd-ddm', 'dd-drag']});
 YUI.add('dd-constrain', function(Y) {
 
 
@@ -2553,6 +2553,7 @@ YUI.add('dd-constrain', function(Y) {
 
 	        this.get(HOST).on('drag:start', Y.bind(this._handleStart, this));
 	        this.get(HOST).after('drag:align', Y.bind(this.align, this));
+	        this.get(HOST).after('drag:drag', Y.bind(this.drag, this));
 	    },
 	    /**
 	    * @private
@@ -2737,9 +2738,16 @@ YUI.add('dd-constrain', function(Y) {
 	        _xy = this._checkTicks(_xy, r);
 
 	        host.actXY = _xy;
-
-			var xt = this.get('tickX'),
-				yt = this.get('tickY');
+	    },
+	    /**
+	    * @method drag
+	    * @description Fires after drag:drag. Handle the tickX and tickX align events.
+	    */
+		drag: function(event) {
+			var host = this.get(HOST),
+				xt = this.get('tickX'),
+				yt = this.get('tickY'),
+				_xy = [host.actXY[0], host.actXY[1]];
 
 			if ((Y.Lang.isNumber(xt) || this.get(TICK_X_ARRAY)) && (this._lastTickXFired !== _xy[0])) {
 				this._tickAlignX();
@@ -2750,7 +2758,7 @@ YUI.add('dd-constrain', function(Y) {
 				this._tickAlignY();
 				this._lastTickYFired = _xy[1];
 			}
-	    },
+		},
 	    /**
 	    * @private
 	    * @method _checkTicks
@@ -2881,8 +2889,7 @@ YUI.add('dd-constrain', function(Y) {
 	});
 
 
-
-}, '@VERSION@' ,{requires:['dd-drag'], skinnable:false});
+}, '@VERSION@' ,{skinnable:false, requires:['dd-drag']});
 YUI.add('dd-scroll', function(Y) {
 
 
@@ -3309,7 +3316,7 @@ YUI.add('dd-scroll', function(Y) {
 
 
 
-}, '@VERSION@' ,{optional:['dd-proxy'], requires:['dd-drag'], skinnable:false});
+}, '@VERSION@' ,{requires:['dd-drag'], skinnable:false, optional:['dd-proxy']});
 YUI.add('dd-drop', function(Y) {
 
 
@@ -3858,7 +3865,7 @@ YUI.add('dd-drop', function(Y) {
 
 
 
-}, '@VERSION@' ,{requires:['dd-ddm-drop', 'dd-drag'], skinnable:false});
+}, '@VERSION@' ,{skinnable:false, requires:['dd-ddm-drop', 'dd-drag']});
 YUI.add('dd-delegate', function(Y) {
 
 
@@ -4188,7 +4195,7 @@ YUI.add('dd-delegate', function(Y) {
 
 
 
-}, '@VERSION@' ,{optional:['dd-drop-plugin'], requires:['dd-drag', 'event-mouseenter'], skinnable:false});
+}, '@VERSION@' ,{requires:['dd-drag', 'event-mouseenter'], skinnable:false, optional:['dd-drop-plugin']});
 
 
 YUI.add('dd', function(Y){}, '@VERSION@' ,{use:['dd-ddm-base', 'dd-ddm', 'dd-ddm-drop', 'dd-drag', 'dd-proxy', 'dd-constrain', 'dd-drop', 'dd-scroll', 'dd-delegate'], skinnable:false});
--- build/dd/dd-drag-debug.js
+++ build/dd/dd-drag-debug.js
@@ -1226,4 +1226,4 @@ YUI.add('dd-drag', function(Y) {
 
 
 
-}, '@VERSION@' ,{requires:['dd-ddm-base'], skinnable:false});
+}, '@VERSION@' ,{skinnable:false, requires:['dd-ddm-base']});
