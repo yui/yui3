0d1f96c53d6d6cb7db657f4450212c20cc776a91
Dav Glass <davglass@gmail.com>
2010-10-20 08:18:31 -0500

    Ammended fix for IE and the cursor flash on TabKey

--- sandbox/editor/js/editor-base.js
+++ sandbox/editor/js/editor-base.js
@@ -174,9 +174,7 @@ YUI.add('editor-base', function(Y) {
                             cur = sel.getCursor();
                             cur.insert(EditorBase.TABKEY, 'before');
                             sel.focusCursor();
-                            if (Y.UA.gecko) {
-                                inst.Selection.cleanCursor();
-                            }
+                            inst.Selection.cleanCursor();
                         }
                     }
                     break;
