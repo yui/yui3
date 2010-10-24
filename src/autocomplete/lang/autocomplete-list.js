01c4c7ac9d947771b365cb71b62865d888f2dc92
Ryan Grove <ryan@wonko.com>
2010-10-24 12:37:37 -0700

    Internationalize autocomplete-list strings.
    Only an English version at the moment. Please translate and send pull
    requests!

---
+++ src/autocomplete/lang/autocomplete-list.js
@@ -0,0 +1,5 @@
+{
+    // ARIA live region notifications.
+    item_selected  : "{item} selected.",
+    items_available: "Suggestions are available. Use the up and down arrow keys to select suggestions."
+}
