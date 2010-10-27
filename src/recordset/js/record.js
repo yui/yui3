e81af05b77b6bb6ebfc812494de2d3a621272d90
Tilo Mitra <tilo@yahoo-inc.com>
2010-10-27 15:04:09 -0700

    add comments to all recordset submodules

--- src/recordset/js/record.js
+++ src/recordset/js/record.js
@@ -24,9 +24,6 @@ var Record = Y.Base.create('record', Y.Base, [], {
 		if (field === undefined) {
         	return this.get("data");
 		}
-		else if (field === 'id') {
-			return this.get('id');
-		}
 		else {
 			return this.get("data")[field];
 		}
