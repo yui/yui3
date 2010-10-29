c1b13ae78a90b9e3702718eff5181f4ebe21214e
Satyen Desai <sdesai@yahoo-inc.com>
2010-10-29 10:23:26 -0700

    We now consciously stamp Base-based objects in Base's constructor, instead of the inadvertent stamping we did in toString (which was removed for 3.3.0).
    DD was relying on the implicit toString stamping, when using Drop
    objects as "hashkeys".

--- build/base/base-debug.js
+++ build/base/base-debug.js
@@ -74,6 +74,9 @@ YUI.add('base-base', function(Y) {
     function Base() {
         Y.log('constructor called', 'life', 'base');
 
+        // So the object can be used as a hash key (as DD does)
+        Y.stamp(this);
+
         Attribute.call(this);
 
         // If Plugin.Host has been augmented [ through base-pluginhost ], setup it's
