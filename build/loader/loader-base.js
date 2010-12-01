9ad2c0a37183c979f01d692e09b9404ea735b234
Matt Sweeney <matt.sweeney@yahoo.com>
2010-12-01 12:51:47 -0800

    declare feature tests in dom-style-ie.js and ie-style-test.js

--- build/loader/loader-base.js
+++ build/loader/loader-base.js
@@ -87,7 +87,6 @@ if (!YUI.Env[Y.version]) {
 }
 
 
-
 /**
  * Loader dynamically loads script and css files.  It includes the dependency
  * info for the version of the library in use, and will automatically pull in
@@ -2233,7 +2232,6 @@ Y.Loader.prototype = {
         return this._filter((base || this.base || '') + path, name);
     }
 };
-
 
 
 
--- src/yui/js/load-tests.js
+++ src/yui/js/load-tests.js
@@ -36,7 +36,25 @@ add('load', '2', {
 // ie-style-test.js
 add('load', '3', {
     "test": function (Y) {
-    var testFeature = Y.Features.test;
+
+    var testFeature = Y.Features.test,
+        addFeature = Y.Features.add,
+        WINDOW = Y.config.win,
+        DOCUMENT = Y.config.doc,
+        DOCUMENT_ELEMENT = 'documentElement',
+        ret = false;
+
+    addFeature('style', 'computedStyle', {
+        test: function() {
+            return WINDOW && 'getComputedStyle' in WINDOW;
+        }
+    });
+
+    addFeature('style', 'opacity', {
+        test: function() {
+            return DOCUMENT && 'opacity' in DOCUMENT[DOCUMENT_ELEMENT].style;
+        }
+    });
 
     ret =  (!testFeature('style', 'opacity') &&
             !testFeature('style', 'computedStyle'));
