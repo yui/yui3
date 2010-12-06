a90f7fefd9901a8da896d4bfe602441e0b7078d3
Jeff Conniff <jconniff@yahoo-inc.com>
2010-12-06 11:20:22 -0800

    ticket 4186561 externalized strs from Intl module

--- src/dial/js/Dial.js
+++ src/dial/js/Dial.js
@@ -166,15 +166,8 @@
          * @default {label: 'My label', resetStr: 'Reset', tooltipHandle: 'Drag to set value'}
          */
         strings: {
-
-//            valueFn: function () {
-//                return Y.Intl.get('autocomplete-list');
-//            }
-//            value: Y.Intl.get('dial')
-
-			value: {label: 'My label',
-				resetStr: 'Reset',
-				tooltipHandle: 'Drag to set value'
+			valueFn: function () {
+				return Y.Intl.get('dial');
 			}
         },
 
