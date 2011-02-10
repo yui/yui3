6838b0516c21526ce027ace6d33efb0ad3bd06f4
Dav Glass <davglass@gmail.com>
2011-02-10 14:23:53 -0600

    Editor Build

--- build/editor/editor-debug.js
+++ build/editor/editor-debug.js
@@ -3752,7 +3752,7 @@ YUI.add('editor-bidi', function(Y) {
     var EditorBidi = function() {
         EditorBidi.superclass.constructor.apply(this, arguments);
     }, HOST = 'host', DIR = 'dir', BODY = 'BODY', NODE_CHANGE = 'nodeChange',
-    B_C_CHANGE = 'bidiContextChange', FIRST_P = BODY + ' > p';
+    B_C_CHANGE = 'bidiContextChange', FIRST_P = BODY + ' > p', STYLE = 'style';
 
     Y.extend(EditorBidi, Y.Base, {
         /**
@@ -3962,6 +3962,13 @@ YUI.add('editor-bidi', function(Y) {
             host: {
                 value: false
             }
+        },
+        RE_TEXT_ALIGN: /text-align:\s*\w*\s*;/,
+        removeTextAlign: function(n) {
+            if (n.getAttribute(STYLE).match(EditorBidi.RE_TEXT_ALIGN)) {
+     	 		n.setAttribute(STYLE, n.getAttribute(STYLE).replace(EditorBidi.RE_TEXT_ALIGN, ''));
+     	 	}
+            return n;
         }
     });
     
@@ -3990,6 +3997,8 @@ YUI.add('editor-bidi', function(Y) {
         inst.Selection.filterBlocks();
         if (sel.isCollapsed) { // No selection
             block = EditorBidi.blockParent(sel.anchorNode);
+            //Remove text-align attribute if it exists
+            block = EditorBidi.removeTextAlign(block);
             if (!direction) {
                 //If no direction is set, auto-detect the proper setting to make it "toggle"
                 dir = block.getAttribute(DIR);
@@ -4016,6 +4025,8 @@ YUI.add('editor-bidi', function(Y) {
             selectedBlocks = inst.all(EditorBidi.addParents(selectedBlocks));
             selectedBlocks.each(function(n) {
                 var d = direction;
+                //Remove text-align attribute if it exists
+                n = EditorBidi.removeTextAlign(n);
                 if (!d) {
                     dir = n.getAttribute(DIR);
                     if (!dir || dir == 'ltr') {
