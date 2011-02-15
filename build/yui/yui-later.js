da9c5db470b1794cbe2976bb24b1a0e68dcabfb4
Adam Moore <adamoore@yahoo-inc.com>
2011-02-15 11:11:19 -0800

    Y.later works on native IE functions that do not have an apply() method [fixes #2529845].

--- build/yui/yui-later.js
+++ build/yui/yui-later.js
@@ -6,6 +6,8 @@ YUI.add('yui-later', function(Y) {
  * @submodule yui-later
  */
 
+var NO_ARGS = [];
+
 /**
  * Executes the supplied function in the context of the supplied
  * object 'when' milliseconds later.  Executes the function a
@@ -22,6 +24,10 @@ YUI.add('yui-later', function(Y) {
  * the function is executed with one parameter for each array item.
  * If you need to pass a single array parameter, it needs to be wrapped
  * in an array [myarray].
+ *
+ * Note: native methods in IE may not have the call and apply methods.
+ * In this case, it will work, but you are limited to four arguments.
+ *
  * @param periodic {boolean} if true, executes continuously at supplied
  * interval until canceled.
  * @return {object} a timer object. Call the cancel() method on this
@@ -29,20 +35,17 @@ YUI.add('yui-later', function(Y) {
  */
 Y.later = function(when, o, fn, data, periodic) {
     when = when || 0;
+    data = (!Y.Lang.isUndefined(data)) ? Y.Array(data) : data;
 
-    var m = fn, f, id;
-
-    if (o && Y.Lang.isString(fn)) {
-        m = o[fn];
-    }
-
-    f = !Y.Lang.isUndefined(data) ? function() {
-        m.apply(o, Y.Array(data));
-    } : function() {
-        m.call(o);
-    };
-
-    id = (periodic) ? setInterval(f, when) : setTimeout(f, when);
+    var method = (o && Y.Lang.isString(fn)) ? o[fn] : fn,
+        wrapper = function() {
+            if (!method.apply) {
+                method(data[0], data[1], data[2], data[3]);
+            } else {
+                method.apply(o, data || NO_ARGS);
+            }
+        },
+        id = (periodic) ? setInterval(wrapper, when) : setTimeout(wrapper, when);
 
     return {
         id: id,
