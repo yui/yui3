641a842efb6ed2370c500deb7a5c9b0d1be46201
Dav Glass <davglass@gmail.com>
2011-02-15 19:55:50 -0600

    Editor dev

--- sandbox/editor/edit.php
+++ sandbox/editor/edit.php
@@ -510,7 +510,7 @@ YUI(yConfig).use('node', 'selector-css3', 'base', 'editor-base', 'editor-para', 
         Y.log('frame:ready, set content', 'info', 'editor');
         var inst = this.getInstance();
         //this.set('content', '<p>' + inst.Selection.CURSOR + '</p>');
-        this.set('content', inst.Selection.CURSOR + '<hr><p>This is some content below the HR</p>');
+        this.set('content', '<p>' + inst.Selection.CURSOR + '</p><br><p>This is some content below the HR</p>');
         //this.set('content', ' ');
 
         //This stops image resizes, but for all images!!
