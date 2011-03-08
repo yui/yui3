970d3605073835415b62245302838340763d086f
Luke Smith <lsmith@yahoo-inc.com>
2011-03-08 00:12:29 -0800

    Remove hard coded 'yui3-' strings. Replace tabs with spaces

--- build/console/lang/console.js
+++ build/console/lang/console.js
@@ -1,1 +1,1 @@
-YUI.add("lang/console",function(A){A.Intl.add("console","",{title:"Log Console",pause:"Pause",clear:"Clear",collapse:"Collapse",expand:"Expand"});},"@VERSION@");
+YUI.add("lang/console",function(a){a.Intl.add("console","",{title:"Log Console",pause:"Pause",clear:"Clear",collapse:"Collapse",expand:"Expand"});},"@VERSION@");
