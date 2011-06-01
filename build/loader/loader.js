7b2e028945b46b29ea51a759b3e0f0483f9a16f4
Ryan Grove <ryan@wonko.com>
2011-05-31 22:13:42 -0700

    Rebuild loader metadata for autocomplete and event.

--- build/loader/loader.js
+++ build/loader/loader.js
@@ -2432,7 +2432,9 @@ YUI.Env[Y.version].modules = YUI.Env[Y.version].modules || {
                 ]
             }, 
             "autocomplete-list": {
-                "after": "autocomplete-sources", 
+                "after": [
+                    "autocomplete-sources"
+                ], 
                 "lang": [
                     "en"
                 ], 
@@ -3277,7 +3279,9 @@ YUI.Env[Y.version].modules = YUI.Env[Y.version].modules || {
     }, 
     "escape": {}, 
     "event": {
-        "after": "node-base", 
+        "after": [
+            "node-base"
+        ], 
         "plugins": {
             "event-base-ie": {
                 "after": [
@@ -3303,7 +3307,9 @@ YUI.Env[Y.version].modules = YUI.Env[Y.version].modules || {
         }, 
         "submodules": {
             "event-base": {
-                "after": "node-base", 
+                "after": [
+                    "node-base"
+                ], 
                 "requires": [
                     "event-custom-base"
                 ]
@@ -4207,7 +4213,7 @@ YUI.Env[Y.version].modules = YUI.Env[Y.version].modules || {
         }
     }
 };
-YUI.Env[Y.version].md5 = 'dfa5ccb16a08c372d665628c75414f43';
+YUI.Env[Y.version].md5 = '392afeb55591a072ab7f71844038c610';
 
 
 }, '@VERSION@' ,{requires:['loader-base']});
