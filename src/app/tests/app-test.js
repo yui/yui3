41f764074322a901460ae58669db5d3c976cf326
Ryan Grove <ryan@wonko.com>
2011-06-01 17:19:57 -0700

    Finish unit tests for View.

--- src/app/tests/app-test.js
+++ src/app/tests/app-test.js
@@ -1203,8 +1203,130 @@ modelListSuite.add(new Y.Test.Case({
     }
 }));
 
+// -- View Suite ---------------------------------------------------------------
+viewSuite = new Y.Test.Suite('View');
+
+// -- View: Lifecycle ----------------------------------------------------------
+viewSuite.add(new Y.Test.Case({
+    name: 'Lifecycle',
+
+    'container should be a <div> node by default': function () {
+        var view = new Y.View();
+
+        Assert.isInstanceOf(Y.Node, view.container);
+        Assert.areSame('div', view.container.get('tagName').toLowerCase());
+    },
+
+    'events property should be an empty object by default': function () {
+        var view = new Y.View();
+
+        Assert.isObject(view.events);
+        Assert.isTrue(Y.Object.isEmpty(view.events));
+    },
+
+    'model property should be undefined by default': function () {
+        Assert.isUndefined(new Y.View().model);
+    },
+
+    'initializer should allow setting a model reference at init': function () {
+        var model = new Y.Model(),
+            view  = new Y.View({model: model});
+
+        Assert.areSame(model, view.model);
+    },
+
+    'initializer should allow setting a template at init': function () {
+        var template = {},
+            view     = new Y.View({template: template});
+
+        Assert.areSame(template, view.template);
+    },
+
+    'initializer should call create() to create the container node': function () {
+        var calls = 0,
+
+            TestView = Y.Base.create('testView', Y.View, [], {
+                create: function (container) {
+                    calls += 1;
+                    Assert.areSame('<b/>', container);
+                }
+            });
+
+        new TestView({container: '<b/>'});
+
+        Assert.areSame(1, calls);
+    },
+
+    'initializer should call attachEvents()': function () {
+        var calls  = 0,
+            events = {'#foo': {click: 'handler'}},
+
+            TestView = Y.Base.create('testView', Y.View, [], {
+                events: {'#bar': {click: 'handler'}},
+
+                attachEvents: function (events) {
+                    calls += 1;
+
+                    Assert.areSame(this.events, events);
+                    Assert.areSame('handler', events['#foo'].click);
+                    Assert.areSame('handler', events['#bar'].click);
+                }
+            });
+
+        new TestView({events: events});
+
+        Assert.areSame(1, calls);
+    },
+
+    'destructor should remove the container from the DOM': function () {
+        var view = new Y.View();
+
+        Y.one('body').append(view.container);
+        Assert.isTrue(view.container.inDoc());
+
+        view.destroy();
+        Assert.isNull(view.container._node);
+    }
+}));
+
+viewSuite.add(new Y.Test.Case({
+    name: 'Methods',
+
+    'create() should create and return a container node': function () {
+        var view = new Y.View(),
+            node = Y.Node.create('<div/>');
+
+        Assert.areSame(node, view.create(node), "should return the same node if it's already a node");
+
+        node = view.create('<div class="foo"/>');
+        Assert.isInstanceOf(Y.Node, node);
+        Assert.areSame('div', node.get('tagName').toLowerCase());
+        Assert.isTrue(node.hasClass('foo'));
+
+        node = view.create(Y.config.doc.createElement('div'));
+        Assert.isInstanceOf(Y.Node, node);
+        Assert.areSame('div', node.get('tagName').toLowerCase());
+    },
+
+    'remove() should remove the container node from the DOM': function () {
+        var view = new Y.View();
+
+        Y.one('body').append(view.container);
+        Assert.isTrue(view.container.inDoc());
+
+        view.remove();
+        Assert.isFalse(view.container.inDoc());
+    },
+
+    'render() should be a chainable noop': function () {
+        var view = new Y.View();
+        Assert.areSame(view, view.render());
+    }
+}));
+
 suite.add(modelSuite);
 suite.add(modelListSuite);
+suite.add(viewSuite);
 
 Y.Test.Runner.add(suite);
 
