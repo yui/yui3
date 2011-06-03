e61b2929889a9e43c60c9e546c962de723a617fa
Dav Glass <davglass@gmail.com>
2011-06-03 12:19:58 -0500

    Resize Build

--- build/resize/resize-proxy-debug.js
+++ build/resize/resize-proxy-debug.js
@@ -143,4 +143,4 @@ Y.namespace('Plugin');
 Y.Plugin.ResizeProxy = ResizeProxy;
 
 
-}, '@VERSION@' ,{skinnable:false, requires:['resize-base', 'plugin']});
+}, '@VERSION@' ,{requires:['resize-base', 'plugin'], skinnable:false});
