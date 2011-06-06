2001471a4b213ca9a07b45f166f7d8cd95fe0ff2
Dav Glass <davglass@gmail.com>
2011-06-06 09:31:08 -0500

    YUI Build

--- build/yui/yui.js
+++ build/yui/yui.js
@@ -372,8 +372,7 @@ proto = {
                                         'intl-base',
                                         'loader',
                                         'yui-log',
-                                        'yui-later',
-                                        'yui-throttle'];
+                                        'yui-later'];
 
         for (i = 0; i < extras.length; i++) {
             if (mods[extras[i]]) {
@@ -4660,49 +4659,7 @@ Y.Lang.later = Y.later;
 
 
 }, '@VERSION@' ,{requires:['yui-base']});
-YUI.add('yui-throttle', function(Y) {
-
-/**
- * Provides a throttle/limiter for function calls
- * @module yui
- * @submodule yui-throttle
- */
-
-/*! Based on work by Simon Willison: http://gist.github.com/292562 */
-/**
- * Throttles a call to a method based on the time between calls.
- * @method throttle
- * @for YUI
- * @param fn {function} The function call to throttle.
- * @param ms {int} The number of milliseconds to throttle the method call.
- * Can set globally with Y.config.throttleTime or by call. Passing a -1 will
- * disable the throttle. Defaults to 150.
- * @return {function} Returns a wrapped function that calls fn throttled.
- * @since 3.1.0
- */
-Y.throttle = function(fn, ms) {
-    ms = (ms) ? ms : (Y.config.throttleTime || 150);
-
-    if (ms === -1) {
-        return (function() {
-            fn.apply(null, arguments);
-        });
-    }
-
-    var last = Y.Lang.now();
-
-    return (function() {
-        var now = Y.Lang.now();
-        if (now - last > ms) {
-            last = now;
-            fn.apply(null, arguments);
-        }
-    });
-};
 
 
-}, '@VERSION@' ,{requires:['yui-base']});
-
-
-YUI.add('yui', function(Y){}, '@VERSION@' ,{use:['yui-base','get','features','rls','intl-base','yui-log','yui-later','yui-throttle']});
+YUI.add('yui', function(Y){}, '@VERSION@' ,{use:['yui-base','get','features','rls','intl-base','yui-log','yui-later']});
 
