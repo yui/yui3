d44f7a03d9ef42b8088b57899edd083a6e8fc817
Dav Glass <davglass@gmail.com>
2011-06-10 13:23:48 -0500

    Added simple script to turn off rollup builds

---
+++ src/loader/scripts/flatten.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+var fs = require('fs'),
+    path = require('path');
+
+var root = path.join(__dirname, '../../');
+
+var dirs = fs.readdirSync(root);
+
+dirs.forEach(function(d) {
+    if (d === 'yui') { //Skip YUI, we want that as a rollup, Maybe
+        return;
+    }
+    var stat = fs.statSync(path.join(root, d));
+    if (stat.isDirectory()) {
+        var files = fs.readdirSync(path.join(root, d));
+        files.forEach(function(f) {
+            if (f.indexOf('.properties') > 0) {
+                console.log(d + '/' + f);
+                var write = false;
+                var str = fs.readFileSync(path.join(root, d, f), 'utf8').split('\n');
+                str.forEach(function(line, num) {
+                    if (line.indexOf('global.build.component') === 0) {
+                        write = true;
+                        str[num] = line.replace('global.build.component', '#global.build.component');
+                    }
+                });
+                if (write) {
+                    //console.error(str.join('\n'));
+                    console.log('Writing: ', path.join(root, d, f));
+                    fs.writeFileSync(path.join(root, d, f), str.join('\n'), 'utf8');
+                }
+
+            }
+        });
+    }
+});
