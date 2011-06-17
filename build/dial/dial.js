743810bb39026f0d1454018c2eccae9ac47a0710
Jeff Conniff <jconniff@yahoo-inc.com>
2011-06-17 11:18:19 -0700

    Dial: build files for reduce code for click on max

--- build/dial/dial.js
+++ build/dial/dial.js
@@ -575,7 +575,6 @@ YUI.add('dial', function(Y) {
 						newValue = ((minAng > ang) && (ang > oppositeMidRangeAngle)) ? this.get('min') : this.get('max');
 					}
 					this._prevAng = this._getAngleFromValue(newValue);
-					this.set('value', newValue); // Setting the value here because it won't pass the conditions below that lead to setting of the value at min or max.
 					return newValue;
 				}
 			}
