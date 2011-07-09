8ebea46a72db798f14907a09fa8ce99422f01809
Luke Smith <lsmith@yahoo-inc.com>
2011-07-08 17:18:39 -0700

    Purge rollup files and update build.xml
    Core should not contain rollup files any more.
    All my modules that live in shared src directories
    have a new build.xml file that subants the
    build.*.xml files in that dir and passes along the
    requested target.

--- build/datatable/lang/datatable_en.js
+++
@@ -1,1 +0,0 @@
-YUI.add("lang/datatable-sort_en",function(a){a.Intl.add("datatable-sort","en",{asc:"Ascending",desc:"Descending"});},"@VERSION@");YUI.add("lang/datatable_en",function(a){},"@VERSION@",{use:["lang/datatable-sort_en"]});
