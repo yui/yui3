9956685c7edee4c39f6ed17bba4aa428e79c54f2
Ryan Grove <ryan@wonko.com>
2011-07-21 10:21:51 -0700

    Allow ModelLists to have their own attributes. [Fixes #2530560]
    The get(), getAsHTML(), and getAsURL() methods on a ModelList will now
    check first for an attribute on the list itself, and return that value
    if found. Otherwise, it will return an array of attribute values from
    the models in the list.
    
    This allows lists to host their own attributes.

--- src/app/js/model-list.js
+++ src/app/js/model-list.js
@@ -25,8 +25,9 @@ defined, models are sorted in insertion order).
 @since 3.4.0
 **/
 
-var Lang   = Y.Lang,
-    YArray = Y.Array,
+var AttrProto = Y.Attribute.prototype,
+    Lang      = Y.Lang,
+    YArray    = Y.Array,
 
     /**
     Fired when a model is added to the list.
@@ -229,6 +230,68 @@ Y.ModelList = Y.extend(ModelList, Y.Base, {
     },
 
     /**
+    If _name_ refers to an attribute on this ModelList instance, returns the
+    value of that attribute. Otherwise, returns an array containing the values
+    of the specified attribute from each model in this list.
+
+    @method get
+    @param {String} name Attribute name or object property path.
+    @return {Any|Array} Attribute value or array of attribute values.
+    @see Model.get()
+    **/
+    get: function (name) {
+        if (this.attrAdded(name)) {
+            return AttrProto.get.apply(this, arguments);
+        }
+
+        return this.invoke('get', name);
+    },
+
+    /**
+    If _name_ refers to an attribute on this ModelList instance, returns the
+    HTML-escaped value of that attribute. Otherwise, returns an array containing
+    the HTML-escaped values of the specified attribute from each model in this
+    list.
+
+    The values are escaped using `Escape.html()`.
+
+    @method getAsHTML
+    @param {String} name Attribute name or object property path.
+    @return {String|String[]} HTML-escaped value or array of HTML-escaped
+      values.
+    @see Model.getAsHTML()
+    **/
+    getAsHTML: function (name) {
+        if (this.attrAdded(name)) {
+            return Y.Escape.html(AttrProto.get.apply(this, arguments));
+        }
+
+        return this.invoke('getAsHTML', name);
+    },
+
+
+    /**
+    If _name_ refers to an attribute on this ModelList instance, returns the
+    URL-encoded value of that attribute. Otherwise, returns an array containing
+    the URL-encoded values of the specified attribute from each model in this
+    list.
+
+    The values are encoded using the native `encodeURIComponent()` function.
+
+    @method getAsURL
+    @param {String} name Attribute name or object property path.
+    @return {String|String[]} URL-encoded value or array of URL-encoded values.
+    @see Model.getAsURL()
+    **/
+    getAsURL: function (name) {
+        if (this.attrAdded(name)) {
+            return encodeURIComponent(AttrProto.get.apply(this, arguments));
+        }
+
+        return this.invoke('getAsURL', name);
+    },
+
+    /**
     Returns the model with the specified _clientId_, or `null` if not found.
 
     @method getByClientId
@@ -260,7 +323,7 @@ Y.ModelList = Y.extend(ModelList, Y.Base, {
 
     @method invoke
     @param {String} name Name of the method to call on each model.
-    @param {any} *args Zero or more arguments to pass to the invoked method.
+    @param {Any} *args Zero or more arguments to pass to the invoked method.
     @return {Array} Array of return values, indexed according to the index of
       the model on which the method was called.
     **/
@@ -782,39 +845,3 @@ Y.ModelList = Y.extend(ModelList, Y.Base, {
 });
 
 Y.augment(ModelList, Y.ArrayList);
-
-/**
-Returns an array containing the values of the specified attribute from each
-model in this list.
-
-@method get
-@param {String} name Attribute name or object property path.
-@return {Array} Array of attribute values.
-@see Model.get()
-**/
-
-/**
-Returns an array containing the HTML-escaped versions of the values of the
-specified string attributes from each model in this list. The values are escaped
-using `Y.Escape.html()`.
-
-@method getAsHTML
-@param {String} name Attribute name or object property path.
-@return {String[]} Array of HTML-escaped attribute values.
-@see Model.getAsHTML()
-**/
-
-/**
-Returns an array containing the URL-encoded versions of the values of the
-specified string attributes from each model in this list. The values are encoded
-using the native `encodeURIComponent()` function.
-
-@method getAsURL
-@param {String} name Attribute name or object property path.
-@return {String[]} Array of URL-encoded attribute values.
-@see Model.getAsURL()
-**/
-
-Y.ArrayList.addMethod(ModelList.prototype, [
-    'get', 'getAsHTML', 'getAsURL'
-]);
