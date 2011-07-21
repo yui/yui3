dd1438ea909a39d6bb203a40df018e2da8028239
Ryan Grove <ryan@wonko.com>
2011-07-21 09:51:31 -0700

    API doc improvements.

--- src/app/js/model-list.js
+++ src/app/js/model-list.js
@@ -1,6 +1,13 @@
 /**
 Provides an API for managing an ordered list of Model instances.
 
+@submodule model-list
+@since 3.4.0
+**/
+
+/**
+Provides an API for managing an ordered list of Model instances.
+
 In addition to providing convenient `add`, `create`, `refresh`, and `remove`
 methods for managing the models in the list, ModelLists are also bubble targets
 for events on the model instances they contain. This means, for example, that
@@ -11,11 +18,11 @@ ModelLists also maintain sort order efficiently as models are added and removed,
 based on a custom `comparator` function you may define (if no comparator is
 defined, models are sorted in insertion order).
 
-@submodule model-list
 @class ModelList
-@constructor
+@extends Base
 @uses ArrayList
-@uses Base
+@constructor
+@since 3.4.0
 **/
 
 var Lang   = Y.Lang,
