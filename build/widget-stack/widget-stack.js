1d0fabe5ba8f80f039cd5f8935fafdd8a6718103
Eric Ferraiuolo <eferraiuolo@gmail.com>
2011-09-09 16:12:28 -0400

    Built WidgetStack.

--- build/widget-stack/widget-stack.js
+++ build/widget-stack/widget-stack.js
@@ -256,6 +256,9 @@ YUI.add('widget-stack', function(Y) {
                 }
 
                 // Eagerly attach resize handlers
+                //
+                // Required because of Event stack behavior, commit ref: cd8dddc
+                // Should be revisted after Ticket #2531067 is resolved.
                 if (UA.ie == 6) {
                     this._addShimResizeHandlers();
                 }
@@ -283,6 +286,11 @@ YUI.add('widget-stack', function(Y) {
                 };
 
             handles.push(this.on(VisibleChange, createBeforeVisible));
+            // Depending how how Ticket #2531067 is resolved, a reversal of
+            // commit ref: cd8dddc could lead to a more elagent solution, with
+            // the addition of this line here:
+            //
+            // handles.push(this.after(VisibleChange, this.sizeShim));
         },
 
         /**
