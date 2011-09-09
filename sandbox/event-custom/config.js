441680c8e0551cf747b499e4d6ef0f2299a55df0
Luke Smith <lsmith@yahoo-inc.com>
2011-09-09 09:37:37 -0700

    Starting shell for perf comparison tests
    Not much here, and it may well be entirely replaced. I honestly
    don't remember much about this work any more; will have to read
    up on it again.  But since I don't want to lose the work if it
    turns out to be valuable and I don't want the untracked files
    cluttering my git status, in they go!

---
+++ sandbox/event-custom/config.js
@@ -0,0 +1,29 @@
+var YUI_config = {
+    bootstrap: false,
+    combine:false,
+    filter:"raw",
+    allowRollup:false,
+    base:"/before/"/*, 
+    core:[
+        "yui-base",
+        "features",
+        "yui-log",
+        "yui-later",
+        "get",
+        "oop",
+        "dom-base",
+        "selector-native", 
+        "selector-css2", 
+        "event-custom-base",
+        "event-base",
+        "node-base", 
+        "dom-style",
+        "node-style",
+        "event-delegate",
+        "node-event-delegate",
+        "dom-screen",
+        "node-screen"
+    ],
+    loaderPath:"loader/loader.js"
+    */
+};
