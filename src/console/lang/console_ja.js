564fbd1ead44e385dc5335164d87c7d41fa96b15
Ryuichi Okumura <okuryu@okuryu.com>
2011-10-23 00:39:32 +0900

    Change string representation in language files.

--- src/console/lang/console_ja.js
+++ src/console/lang/console_ja.js
@@ -1,7 +1,7 @@
 {
-    title: "ログコンソール",
-    pause: "一時停止",
-    clear: "クリア",
-    collapse: "閉じる",
-    expand: "開く"
+    title: "\u30ED\u30B0\u30B3\u30F3\u30BD\u30FC\u30EB",
+    pause: "\u4E00\u6642\u505C\u6B62",
+    clear: "\u30AF\u30EA\u30A2",
+    collapse: "\u9589\u3058\u308B",
+    expand: "\u958B\u304F"
 }
