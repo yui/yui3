91d2e8efeec13bafe9e74c03a0febc34604ce0f6
Dav Glass <davglass@gmail.com>
2011-11-09 08:24:43 -0600

    Fixes #2530257 - Scoped detachAll to DDs events

--- sandbox/dd/js/drag.js
+++ sandbox/dd/js/drag.js
@@ -968,6 +968,7 @@ YUI.add('dd-drag', function(Y) {
         * @description Internal init handler
         */
         initializer: function(cfg) {
+
             this.get(NODE).dd = this;
 
             if (!this.get(NODE).get('id')) {
@@ -1013,7 +1014,9 @@ YUI.add('dd-drag', function(Y) {
         _unprep: function() {
             var node = this.get(NODE);
             node.removeClass(DDM.CSS_PREFIX + '-draggable');
-            node.detachAll();
+            node.detachAll('mouseup');
+            node.detachAll('dragstart');
+            node.detachAll(Drag.START_EVENT);
         },
         /**
         * @method start
@@ -1237,7 +1240,6 @@ YUI.add('dd-drag', function(Y) {
         */
         destructor: function() {
             this._unprep();
-            this.detachAll();
             if (this.target) {
                 this.target.destroy();
             }
