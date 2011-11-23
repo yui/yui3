2cd07aea8b9b5f1c6b950771eb22a47844ad2efa
Derek Gathright <drgath@gmail.com>
2011-11-23 11:48:31 -0800

    new Button build

--- build/button-group/button-group.js
+++ build/button-group/button-group.js
@@ -1,7 +1,8 @@
 YUI.add('button-group', function(Y) {
 
+/*global Y */
 
-var ButtonGroup = function(config) {
+var ButtonGroup = function (config) {
     
     this.addAttrs({
         
@@ -26,27 +27,31 @@ var ButtonGroup = function(config) {
     Y.Array.each(buttons, function(button){
         this.addButton(button);
     }, this);
-}
+};
 
 ButtonGroup.prototype.addButton = function(/* Button */ button){
-    //console.log('Adding button');
-    var buttons = this.get('buttons');
+    var buttons;
+    
+    buttons = this.get('buttons');
+    
     if (this.get('type') === 'radio') {
         button.set('type', 'toggle');
     }
+    
     button.before('selectedChange', this._beforeButtonSelectedChange, this);
     button.after('selectedChange', this._afterButtonSelectedChange, this);
     buttons.push(button);
+    
     this.set('buttons', buttons);
-}
+};
 
 ButtonGroup.prototype._beforeButtonSelectedChange = function(e) {
-    //console.log('Before selection change');
+    var button, selection;
     
-    var button = e.target;
+    button = e.target;
     
     if (this.get('type') === 'radio') {
-        var selection = this.get('selection');
+        selection = this.get('selection');
         if (selection.length) {
             if (button.get('srcNode')._yuid === selection[0].get('srcNode')._yuid) {
                 e.preventDefault();
@@ -56,31 +61,34 @@ ButtonGroup.prototype._beforeButtonSelectedChange = function(e) {
             }
         }
     }
-}
+};
 
-ButtonGroup.prototype._afterButtonSelectedChange = function(e) {
+ButtonGroup.prototype._afterButtonSelectedChange = function() {
     this._syncSelection();
-}
+};
 
 ButtonGroup.prototype._syncSelection = function() {
-    var buttons = this.get('buttons');
+    var buttons, selections;
+    
+    buttons = this.get('buttons');
     
     // Split apart the selected from the non-selected buttons
-    var selections = Y.Array.partition(buttons, function(/* Button */ button){
+    selections = Y.Array.partition(buttons, function(/* Button */ button){
         return button.get('selected');
     });
-    this.set('selection', selections['matches']);
+    
+    this.set('selection', selections.matches);
     // TODO: do something with selections['rejects']?
-}
+};
 
 ButtonGroup.prototype._removeButtonFromSelectionExcept = function(/* Button */ buttonToKeep) {
     var selection = this.get('selection');
     Y.Array.each(selection, function(button){
         if (button.get('srcNode')._yuid !== buttonToKeep.get('srcNode')._yuid) {
-            button.set('selected', false, {propogate:false});
+            button.set('selected', false, {propagate:false});
         }
     }, this);                
-}
+};
 
 Y.augment(ButtonGroup, Y.Attribute);
 
