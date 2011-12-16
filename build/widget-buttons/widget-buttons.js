0eae691d93468d00d205010e40475ecce06776ed
Eric Ferraiuolo <eferraiuolo@gmail.com>
2011-12-16 11:30:25 -0500

    Built Panel and WidgetButtons.

--- build/widget-buttons/widget-buttons.js
+++ build/widget-buttons/widget-buttons.js
@@ -337,4 +337,4 @@ WidgetButtons.prototype = {
 Y.WidgetButtons = WidgetButtons;
 
 
-}, '@VERSION@' ,{requires:['base-build', 'widget', 'widget-stdmod']});
+}, '@VERSION@' ,{requires:['cssbuttons', 'base-build', 'widget', 'widget-stdmod']});
