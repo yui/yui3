cfb2dc43f7aba18c4c6b0db07ca060089bd2e670
Luke Smith <lsmith@yahoo-inc.com>
2011-12-20 01:11:23 -0800

    Add API docs and other minor code updates.

--- src/datatable/js/head.js
+++ src/datatable/js/head.js
@@ -1,3 +1,66 @@
+/**
+View class responsible for rendering the `<thead>` section of a table. Used as
+the default `headerView` for `Y.DataTable.Base` and `Y.DataTable` classes.
+
+Translates the provided array of column configuration objects into a rendered
+`<thead>` based on the data in those objects.
+    
+
+The structure of the column data is expected to be a single array of objects,
+where each object corresponds to a `<th>`.  Those objects may contain a
+`children` property containing a similarly structured array to indicate the
+nested cells should be grouped under the parent column's colspan in a separate
+row of header cells. E.g.
+
+```
+new Y.DataTable.HeaderView({
+  container: tableNode,
+  columns: [
+    { key: 'id' }, // no nesting
+    { key: 'name', children: [
+      { key: 'firstName', label: 'First' },
+      { key: 'lastName',  label: 'Last' } ] }
+  ]
+}).render();
+```
+
+This would translate to the following visualization:
+
+```
+---------------------
+|    |     name     |
+|    |---------------
+| id | First | Last |
+---------------------
+```
+
+Supported properties of the column objects include:
+
+  * `label`    - The HTML content of the header cell.
+  * `key`      - If `label` is not specified, the `key` is used for content.
+  * `children` - Array of columns to appear below this column in the next
+                 row.
+  * `abbr`     - The content of the 'abbr' attribute of the `<th>`
+
+Through the life of instantiation and rendering, the column objects will have
+the following properties added to them:
+
+  * `colspan` - To supply the `<th>` attribute
+  * `rowspan` - To supply the `<th>` attribute
+  * `parent`  - If the column is a child of another column, this points to
+    its parent column
+  * `_yuid`   - A unique YUI generated id used as the `<th>`'s 'id' for
+    reference in the data `<td>`'s 'headers' attribute.
+
+The column object is also used to provide values for {placeholder} tokens in the
+instance's `CELL_TEMPLATE`, so you can modify the template and include other
+column object properties to populate them.
+
+@module datatable-head
+@class HeaderView
+@namespace DataTable
+@extends View
+**/
 var fromTemplate = Y.Lang.sub,
     Lang = Y.Lang,
     isArray = Lang.isArray,
@@ -8,6 +71,20 @@ var fromTemplate = Y.Lang.sub,
 
 Y.namespace('DataTable').HeaderView = Y.Base.create('tableHeader', Y.View, [], {
     // -- Instance properties -------------------------------------------------
+
+    /**
+    Template used to create the table's header cell markup.  Override this to
+    customize how these cells' markup is created.
+
+    @property CELL_TEMPLATE
+    @type {HTML}
+    @default '<th id="{_yuid}" abbr="{abbr}
+                colspan="{colspan}" rowspan="{rowspan}">
+            <div class="{linerClass}">
+                {content}
+            </div>
+        </th>'
+    **/
     CELL_TEMPLATE :
         '<th id="{_yuid}" abbr="{abbr}" ' +
                 'colspan="{colspan}" rowspan="{rowspan}">' +
@@ -16,23 +93,74 @@ Y.namespace('DataTable').HeaderView = Y.Base.create('tableHeader', Y.View, [], {
             '</div>' +
         '</th>',
 
+    /**
+    The data representation of the header rows to render.  This is assigned by
+    parsing the `columns` configuration array, and is used by the render()
+    method.
+
+    @property columns
+    @type {Array[]}
+    @default (initially unset)
+    **/
+    //TODO: should this be protected?
+    //columns: null,
+
+    /**
+    The object that serves as the source of truth for column and row data.
+    This property is assigned at instantiation from the `source` property of
+    the configuration object passed to the constructor.
+
+    @property source
+    @type {Object}
+    @default (initially unset)
+    **/
+    //TODO: should this be protected?
+    //source: null,
+
+    /**
+    Template used to create the table's header row markup.  Override this to
+    customize the row markup.
+
+    @property ROW_TEMPLATE
+    @type {HTML}
+    @default '<tr>{content}</tr>'
+    **/
     ROW_TEMPLATE:
         '<tr>{content}</tr>',
 
+    /**
+    Template used to create the table's thead markup.
+
+    @property THEAD_TEMPLATE
+    @type {HTML}
+    @default '<thead class="{classes}">{content}</thead>'
+    **/
     THEAD_TEMPLATE:
         '<thead class="{classes}">{content}</thead>',
 
-    // -- Public methods ------------------------------------------------------
-    bindUI: function () {
-        // TODO: How best to decouple this?
-        this._eventHandles.push(
-            this.host.after('columnsChange', this._afterColumnChange));
-    },
 
+    // -- Public methods ------------------------------------------------------
+
+    /**
+    Destroys the instance.
+
+    @method destructor
+    **/
     destructor: function () {
         (new Y.EventHandle(this._eventHandles)).detach();
     },
 
+    /**
+    Builds a CSS class name from the provided tokens.  If the instance is
+    created with `cssPrefix` or `source` in the configuration, it will use this
+    prefix (the `_cssPrefix` of the `source` object) as the base token.  This
+    allows class instances to generate markup with class names that correspond
+    to the parent class that is consuming them.
+
+    @method getClassName
+    @param {String} token* Any number of tokens to include in the class name
+    @return {String} The generated class name
+    **/
     getClassName: function () {
         var args = toArray(arguments);
         args.unshift(this._cssPrefix);
@@ -41,28 +169,24 @@ Y.namespace('DataTable').HeaderView = Y.Base.create('tableHeader', Y.View, [], {
         return _getClassName.apply(ClassNameManager, args);
     },
 
-    initializer: function (config) {
-        var cssPrefix = config.cssPrefix || (config.host || {}).cssPrefix;
-
-        this.host    = config.source;
-        this.columns = this._parseColumns(config.columns);
-
-        this._eventHandles = [];
-
-        if (cssPrefix) {
-            this._cssPrefix = cssPrefix;
-        }
-    },
-
+    /**
+    Creates the `<thead>` Node by assembling markup generated by populating the
+    `THEAD_TEMPLATE`, `ROW_TEMPLATE`, and `CELL_TEMPLATE` templates with
+    content from the `columns` property.
+    
+    @method render
+    @return {HeaderView} The instance
+    @chainable
+    **/
     render: function () {
         var table    = this.get('container'),
             columns  = this.columns,
-            thead    = this.host._theadNode,
+            thead    = this.source._theadNode,
             defaults = {
                             abbr: '',
                             colspan: 1,
                             rowspan: 1,
-                            // TODO: remove dependence on this.host
+                            // TODO: remove dependence on this.source
                             linerClass: this.getClassName('liner')
                        },
             existing, replace, i, len, j, jlen, col, html;
@@ -95,7 +219,7 @@ Y.namespace('DataTable').HeaderView = Y.Base.create('tableHeader', Y.View, [], {
                                 col, {
                                     content: col.label ||
                                              col.key   ||
-                                             ("Column " + j)
+                                             ("Column " + (j + 1))
                                 }
                             ));
                     }
@@ -125,13 +249,125 @@ Y.namespace('DataTable').HeaderView = Y.Base.create('tableHeader', Y.View, [], {
         return this;
     },
 
-    // -- Protected and private methods ---------------------------------------
+    // -- Protected and private properties and methods ------------------------
+    /**
+    The base token for classes created with the `getClassName` method.
+
+    @property _cssPrefix
+    @type {String}
+    @default 'yui3-table'
+    @protected
+    **/
+    _cssPrefix: ClassNameManager.getClassName('table'),
+
+    /**
+    Handles changes in the source's columns attribute.  Redraws the headers.
+
+    @method _afterColumnChange
+    @param {EventFacade} e The `columnsChange` event object
+    @protected
+    **/
     _afterColumnChange: function (e) {
         // TODO
     },
 
-    _cssPrefix: 'table',
+    /**
+    Binds event subscriptions from the UI and the source (if assigned).
 
+    @method bindUI
+    @protected
+    **/
+    bindUI: function () {
+        if (this.source) {
+            // TODO: How best to decouple this?
+            this._eventHandles.push(
+                this.source.after('columnsChange', this._afterColumnsChange));
+        }
+    },
+
+    /**
+    Initializes the instance. Reads the following configuration properties:
+
+      * `columns` - (REQUIRED) The initial column information
+      * `cssPrefix` - The base string for classes generated by `getClassName`
+      * `source` - The object to serve as source of truth for column info
+
+    @method initializer
+    @param {Object} config Configuration data
+    **/
+    initializer: function (config) {
+        config || (config = {});
+
+        var cssPrefix = config.cssPrefix || (config.source || {}).cssPrefix;
+
+        this.source  = config.source;
+        this.columns = this._parseColumns(config.columns);
+
+        this._eventHandles = [];
+
+        if (cssPrefix) {
+            this._cssPrefix = cssPrefix;
+        }
+    },
+
+    /**
+    Translate the input column format into a structure useful for rendering a
+    `<thead>`, rows, and cells.  The structure of the input is expected to be a
+    single array of objects, where each object corresponds to a `<th>`.  Those
+    objects may contain a `children` property containing a similarly structured
+    array to indicate the nested cells should be grouped under the parent
+    column's colspan in a separate row of header cells. E.g.
+
+    ```
+    [
+      { key: 'id' }, // no nesting
+      { key: 'name', children: [
+        { key: 'firstName', label: 'First' },
+        { key: 'lastName',  label: 'Last' } ] }
+    ]
+    ```
+
+    would indicate two header rows with the first column 'id' being assigned a
+    `rowspan` of `2`, the 'name' column appearing in the first row with a
+    `colspan` of `2`, and the 'firstName' and 'lastName' columns appearing in
+    the second row, below the 'name' column.
+
+    ```
+    ---------------------
+    |    |     name     |
+    |    |---------------
+    | id | First | Last |
+    ---------------------
+    ```
+
+    Supported properties of the column objects include:
+
+      * `label`    - The HTML content of the header cell.
+      * `key`      - If `label` is not specified, the `key` is used for content.
+      * `children` - Array of columns to appear below this column in the next
+                     row.
+      * `abbr`     - The content of the 'abbr' attribute of the `<th>`
+
+    The output structure is basically a simulation of the `<thead>` structure
+    with arrays for rows and objects for cells.  Column objects have the
+    following properties added to them:
+    
+      * `colspan` - Per the `<th>` attribute
+      * `rowspan` - Per the `<th>` attribute
+      * `parent`  - If the column is a child of another column, this points to
+        its parent column
+      * `_yuid`   - A unique YUI generated id used as the `<th>`'s 'id' for
+        reference in the data `<td>`'s 'headers' attribute.
+
+    The column object is also used to provide values for {placeholder}
+    replacement in the `CELL_TEMPLATE`, so you can modify the template and
+    include other column object properties to populate them.
+
+    @method _parseColumns
+    @param {Object[]} data Array of column object data
+    @return {Array[]} An array of arrays corresponding to the header row
+            structure to render
+    **/
     _parseColumns: function (data) {
         var columns = [],
             stack = [],
@@ -151,10 +387,6 @@ Y.namespace('DataTable').HeaderView = Y.Base.create('tableHeader', Y.View, [], {
                 for (len = row.length; i < len; ++i) {
                     col = row[i];
                     children = col.children;
-
-                    if (typeof col === 'string') {
-                        row[i] = col = { key: col };
-                    }
 
                     Y.stamp(col);
 
