28594974a8c497e3f10e4e9dad3892323740fc5f
Eric Ferraiuolo <eferraiuolo@gmail.com>
2012-01-05 21:45:00 -0500

    Built WidgetButtons which now uses "cssbutton".

--- build/widget-buttons/widget-buttons-debug.js
+++ build/widget-buttons/widget-buttons-debug.js
@@ -338,4 +338,4 @@ WidgetButtons.prototype = {
 Y.WidgetButtons = WidgetButtons;
 
 
-}, '@VERSION@' ,{requires:['cssbuttons', 'base-build', 'widget', 'widget-stdmod']});
+}, '@VERSION@' ,{requires:['cssbutton', 'base-build', 'widget', 'widget-stdmod']});
