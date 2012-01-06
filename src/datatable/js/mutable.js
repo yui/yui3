af5c9cbb47b12856671c1ac4900f8ea8b46eac88
Luke Smith <lsmith@yahoo-inc.com>
2012-01-05 16:44:25 -0800

    +API docs, -reset() method
    `table.reset(blah)` was removed in favor of
    `table.set('data', blah);`

--- src/datatable/js/mutable.js
+++ src/datatable/js/mutable.js
@@ -10,8 +10,18 @@ var toArray = Y.Array,
 /**
 Adds mutation convenience methods to `Y.DataTable` (or other built class).
 
-@module datatable-mutation
-@class DataTable.Mutation
+Column mutation methods are paired with new custom events:
+
+ * addColumn
+ * removeColumn
+ * modifyColumn
+ * moveColumn
+
+Row mutation events are bubbled from the DataTable's `data` ModelList through
+the DataTable instance.
+
+@module datatable-mutable
+@class DataTable.Mutable
 @for DataTable
 **/
 
@@ -44,6 +54,12 @@ Y.mix(Mutable.prototype, {
     //   1, --  in the second child's children
     //   3 ] -- as the fourth child column
     table.addColumn({ key: 'age', sortable: true }, [ 2, 1, 3 ]);
+
+    @method addColumn
+    @param {Object|String} config The new column configuration object
+    @param {Number|Number[]} [index] the insertion index
+    @return {DataTable}
+    @chainable
     **/
     addColumn: function (config, index) {
         if (isString(config)) {
@@ -63,6 +79,27 @@ Y.mix(Mutable.prototype, {
         return this;
     },
 
+    /**
+    Updates an existing column definition. Fires the `modifyColumn` event.
+
+    @example
+    // Add a formatter to the existing 'price' column definition
+    table.modifyColumn('price', { formatter: currencyFormatter });
+
+    // Change the label on a header cell in a set of nested headers three rows
+    // deep.  The index array translates to
+    // [ 2,  -- in the third column's children
+    //   1,  -- the second child
+    //   3 ] -- the fourth child column
+    table.modifyColumn([2, 1, 3], { label: 'Experience' });
+
+    @method modifyColumn
+    @param {String|Number|Number[]|Object} name The column key, name, index, or
+                current configuration object
+    @param {Object} config The new column configuration properties
+    @return {DataTable}
+    @chainable
+    **/
     modifyColumn: function (name, config) {
         if (isString(config)) {
             config = { key: config };
@@ -78,6 +115,19 @@ Y.mix(Mutable.prototype, {
         return this;
     },
 
+    /**
+    Moves an existing column to a new location. Fires the `moveColumn` event.
+
+    The destination index can be a number or array of numbers to place a column
+    header in a nested header row.
+
+    @method moveColumn
+    @param {String|Number|Number[]|Object} name The column key, name, index, or
+                current configuration object
+    @param {Number|Number[]} index The destination index of the column
+    @return {DataTable}
+    @chainable
+    **/
     moveColumn: function (name, index) {
         if (name && (isNumber(index) || isArray(index))) {
             this.fire('moveColumn', {
@@ -89,6 +139,15 @@ Y.mix(Mutable.prototype, {
         return this;
     },
 
+    /**
+    Removes an existing column. Fires the `removeColumn` event.
+
+    @method removeColumn
+    @param {String|Number|Number[]|Object} name The column key, name, index, or
+                current configuration object
+    @return {DataTable}
+    @chainable
+    **/
     removeColumn: function (name) {
         if (name) {
             this.fire('removeColumn', {
@@ -99,6 +158,18 @@ Y.mix(Mutable.prototype, {
         return this;
     },
 
+    /**
+    Adds a new record to the DataTable's `data` ModelList.  Record data can be
+    an object of field values or an instance of the DataTable's configured
+    `recordType` class.
+
+    This relays all parameters to the `data` ModelList's `add` method.
+
+    @method addRow
+    @param {Object} data The data or Model instance for the new record
+    @return {DataTable}
+    @chainable
+    **/
     addRow: function () {
         if (this.data) {
             this.data.add.apply(this.data, arguments);
@@ -107,6 +178,19 @@ Y.mix(Mutable.prototype, {
         return this;
     },
 
+    /**
+    Removes a record from the DataTable's `data` ModelList.  The record can be
+    provided explicitly or targeted by it's `id` (see ModelList's `getById`
+    method), `clientId`, or index in the ModelList.
+
+    After locating the target Model, this relays the Model and all other passed
+    arguments to the `data` ModelList's `remove` method.
+
+    @method removeRow
+    @param {Object|String|Number} id The Model instance or identifier 
+    @return {DataTable}
+    @chainable
+    **/
     removeRow: function (id) {
         var modelList = this.data,
             model;
@@ -128,6 +212,19 @@ Y.mix(Mutable.prototype, {
         return this;
     },
 
+    /**
+    Updates an existing record in the DataTable's `data` ModelList.  The record
+    can be provided explicitly or targeted by it's `id` (see ModelList's
+    `getById` method), `clientId`, or index in the ModelList.
+
+    After locating the target Model, this relays the all other passed
+    arguments to the Model's `setAttrs` method.
+
+    @method modifyRow
+    @param {Object|String|Number} id The Model instance or identifier 
+    @return {DataTable}
+    @chainable
+    **/
     modifyRow: function (id, data) {
         var modelList = this.data,
             model;
@@ -147,18 +244,21 @@ Y.mix(Mutable.prototype, {
         return this;
     },
 
-    reset: function (data) {
-        if (this.data) {
-            this.data.reset.apply(this.data, arguments);
-        }
-
-        return this;
-    },
-
     // --------------------------------------------------------------------------
     // Protected properties and methods
     // --------------------------------------------------------------------------
 
+    /**
+    Default function for the `addColumn` event.
+
+    Inserts the specified column at the provided index.
+
+    @method _defAddColumnFn
+    @param {EventFacade} e The `addColumn` event
+        @param {Object} e.column The new column definition object
+        @param {Number|Number[]} e.index The array index to insert the new column
+    @protected
+    **/
     _defAddColumnFn: function (e) {
         var index   = toArray(e.index),
             columns = this.get('columns'),
@@ -177,6 +277,17 @@ Y.mix(Mutable.prototype, {
         }
     },
 
+    /**
+    Default function for the `modifyColumn` event.
+
+    Mixes the new column properties into the specified column definition.
+
+    @method _defModifyColumnFn
+    @param {EventFacade} e The `modifyColumn` event
+        @param {Object|String|Number|Number[]} e.column The column definition object or identifier
+        @param {Object} e.newColumnDef The properties to assign to the column
+    @protected
+    **/
     _defModifyColumnFn: function (e) {
         var columns = this.get('columns'),
             column  = this.getColumn(e.column);
@@ -189,6 +300,18 @@ Y.mix(Mutable.prototype, {
         }
     },
 
+    /**
+    Default function for the `moveColumn` event.
+
+    Removes the specified column from its current location and inserts it at the
+    specified array index (may be an array of indexes for nested headers).
+
+    @method _defMoveColumnFn
+    @param {EventFacade} e The `moveColumn` event
+        @param {Object|String|Number|Number[]} e.column The column definition object or identifier
+        @param {Object} e.index The destination index to move to
+    @protected
+    **/
     _defMoveColumnFn: function (e) {
         var columns = this.get('columns'),
             column  = this.getColumn(e.column),
@@ -218,6 +341,16 @@ Y.mix(Mutable.prototype, {
         }
     },
 
+    /**
+    Default function for the `removeColumn` event.
+
+    Splices the specified column from its containing columns array.
+
+    @method _defRemoveColumnFn
+    @param {EventFacade} e The `removeColumn` event
+        @param {Object|String|Number|Number[]} e.column The column definition object or identifier
+    @protected
+    **/
     _defRemoveColumnFn: function (e) {
         var columns = this.get('columns'),
             column  = this.getColumn(e.column),
@@ -236,6 +369,17 @@ Y.mix(Mutable.prototype, {
         }
     },
 
+    /**
+    Publishes the events used by the mutation methods:
+
+     * addColumn
+     * removeColumn
+     * modifyColumn
+     * moveColumn
+
+    @method initializer
+    @protected
+    **/
     initializer: function () {
         this.publish({
             addColumn:    { defaultFn: this._defAddColumnFn },
@@ -246,7 +390,58 @@ Y.mix(Mutable.prototype, {
     }
 });
 
+/**
+Adds an array of new records to the DataTable's `data` ModelList.  Record data
+can be an array of objects containing field values or an array of instance of
+the DataTable's configured `recordType` class.
+
+This relays all parameters to the `data` ModelList's `add` method.
+
+Technically, this is an alias to `addRow`, but please use the appropriately
+named method for readability.
+
+@method addRows
+@param {Object[]} data The data or Model instances to add
+@return {DataTable}
+@chainable
+**/
 Mutable.prototype.addRows = Mutable.prototype.addRow;
 
 // Add feature APIs to public Y.DataTable class
 Y.Base.mix(Y.DataTable, [Mutable]);
+
+/**
+Fired by the `addColumn` method.
+
+@event addColumn
+@preventable _defAddColumnFn
+@param {Object} column The new column definition object
+@param {Number|Number[]} index The array index to insert the new column
+**/
+
+/**
+Fired by the `removeColumn` method.
+
+@event removeColumn
+@preventable _defRemoveColumnFn
+@param {Object|String|Number|Number[]} column The column definition object or identifier
+**/
+
+/**
+Fired by the `modifyColumn` method.
+
+@event modifyColumn
+@preventable _defModifyColumnFn
+@param {Object|String|Number|Number[]} column The column definition object or identifier
+@param {Object} newColumnDef The properties to assign to the column
+**/
+
+/**
+Fired by the `moveColumn` method.
+
+@event moveColumn
+@preventable _defMoveColumnFn
+@param {Object|String|Number|Number[]} column The column definition object or identifier
+@param {Object} index The destination index to move to
+**/
+
