yui/yui3

commit 096a37701018dda1fa4147b86f2f19bf8a2bdac8

Add a `load` event to ModelList. [Ticket #2531399]

src/app/js/model-list.js
@@ -80,6 +80,35 @@ var AttrProto = Y.Attribute.prototype,
     EVT_ERROR = 'error',
 
     /**
+    Fired after models are loaded from a sync layer.
+
+    @event load
+    @param {Object} parsed The parsed version of the sync layer's response to
+        the load request.
+    @param {Mixed} response The sync layer's raw, unparsed response to the load
+        request.
+    @since 3.5.0
+    **/
+    EVT_LOAD = 'load',
+
+    /**
+    Fired when a model is removed from the list.
+
+    Listen to the `on` phase of this event to be notified before a model is
+    removed from the list. Calling `e.preventDefault()` during the `on` phase
+    will prevent the model from being removed.
+
+    Listen to the `after` phase of this event to be notified after a model has
+    been removed from the list.
+
+    @event remove
+    @param {Model} model The model being removed.
+    @param {Number} index The index of the model being removed.
+    @preventable _defRemoveFn
+    **/
+    EVT_REMOVE = 'remove',
+
+    /**
     Fired when the list is completely reset via the `reset()` method or sorted
     via the `sort()` method.
 
@@ -96,24 +125,7 @@ var AttrProto = Y.Attribute.prototype,
       `'sort'`.
     @preventable _defResetFn
     **/
-    EVT_RESET = 'reset',
-
-    /**
-    Fired when a model is removed from the list.
-
-    Listen to the `on` phase of this event to be notified before a model is
-    removed from the list. Calling `e.preventDefault()` during the `on` phase
-    will prevent the model from being removed.
-
-    Listen to the `after` phase of this event to be notified after a model has
-    been removed from the list.
-
-    @event remove
-    @param {Model} model The model being removed.
-    @param {int} index The index of the model being removed.
-    @preventable _defRemoveFn
-    **/
-    EVT_REMOVE = 'remove';
+    EVT_RESET = 'reset';
 
 function ModelList() {
     ModelList.superclass.constructor.apply(this, arguments);
@@ -453,9 +465,31 @@ Y.ModelList = Y.extend(ModelList, Y.Base, {
             options  = {};
         }
 
+        options || (options = {});
+
         this.sync('read', options, function (err, response) {
-            if (!err) {
-                self.reset(self.parse(response), options);
+            var facade = {
+                    options : options,
+                    response: response
+                },
+
+                parsed;
+
+            if (err) {
+                facade.error = err;
+                facade.src   = 'load';
+
+                self.fire(EVT_ERROR, facade);
+            } else {
+                // Lazy publish.
+                if (!self._loadEvent) {
+                    self._loadEvent = self.publish(EVT_LOAD, {
+                        preventable: false
+                    });
+                }
+
+                parsed = facade.parsed = self.parse(response);
+                self.fire(EVT_LOAD, facade);
             }
 
             callback && callback.apply(null, arguments);

src/app/tests/model-list-test.js
@@ -740,6 +740,30 @@ modelListSuite.add(new Y.Test.Case({
         Assert.areSame(1, calls);
     },
 
+    '`load` event should fire after a successful load operation': function () {
+        var calls = 0,
+            list  = this.createList();
+
+        list.on('load', function (e) {
+            calls += 1;
+
+            Assert.areSame('[{"foo": "bar"}]', e.response);
+            Assert.isObject(e.options);
+            Assert.isObject(e.parsed);
+            Assert.areSame('bar', e.parsed[0].foo);
+        });
+
+        list.sync = function (action, options, callback) {
+            callback(null, '[{"foo": "bar"}]');
+        };
+
+        list.load(function () {
+            Assert.areSame(1, calls, 'load event should fire before the callback runs');
+        });
+
+        Assert.areSame(1, calls, 'load event never fired');
+    },
+
     '`reset` event should fire when the list is reset or sorted': function () {
         var calls  = 0,
             list   = this.createList(),

src/app/docs/model-list/index.mustache
@@ -239,6 +239,7 @@ Model List instances provide the following events:
     <tr>
       <th>Event</th>
       <th>When</th>
+      <th>Preventable?</th>
       <th>Payload</th>
     </tr>
   </thead>
@@ -246,6 +247,7 @@ Model List instances provide the following events:
     <tr>
       <td>`add`</td>
       <td>A model is added to the list.</td>
+      <td style="text-align:center;">Y</td>
       <td>
         <dl>
           <dt>`model` (<em>Y.Model</em>)</dt>
@@ -262,11 +264,26 @@ Model List instances provide the following events:
     </tr>
 
     <tr>
-      <td>`error`</td>
-      <td>An error occurs, such as when an attempt is made to add a duplicate model to the list, or when a sync layer response can't be parsed.</td>
+      <td>`create`</td>
+      <td>A model is created or updated via the `create()` method, but before that model is saved or added to the list, and before the `add` event fires.</td>
+      <td style="text-align:center;">&nbsp;</td>
       <td>
         <dl>
-          <dt>`error` (<em>Any</em>)</dt>
+          <dt>`model` (<em>Y.Model</em>)</dt>
+          <dd>
+            The model instance being created or updated.
+          </dd>
+        </dl>
+      </td>
+    </tr>
+
+    <tr>
+      <td>`error`</td>
+      <td>An error occurs, such as when an attempt is made to add a duplicate model to the list, or when a sync layer response can't be parsed.</td>
+      <td style="text-align:center;">&nbsp;</td>
+      <td>
+        <dl>
+          <dt>`error` (<em>Mixed</em>)</dt>
           <dd>
             Error message, object, or exception generated by the error. Calling `toString()` on this should result in a meaningful error message.
           </dd>
@@ -298,8 +315,28 @@ Model List instances provide the following events:
     </tr>
 
     <tr>
+      <td>`load`</td>
+      <td>Models are loaded into the list from a sync layer.</td>
+      <td style="text-align:center;">&nbsp;</td>
+      <td>
+        <dl>
+          <dt>`parsed` (<em>Object</em>)</dt>
+          <dd>
+            The parsed version of the sync layer's response to the load request.
+          </dd>
+
+          <dt>`response` (<em>Mixed</em>)</dt>
+          <dd>
+            The sync layer's raw, unparsed response to the load request.
+          </dd>
+        </dl>
+      </td>
+    </tr>
+
+    <tr>
       <td>`remove`</td>
       <td>A model is removed from the list.</td>
+      <td style="text-align:center;">Y</td>
       <td>
         <dl>
           <dt>`model` (<em>Y.Model</em>)</dt>
@@ -318,6 +355,7 @@ Model List instances provide the following events:
     <tr>
       <td>`reset`</td>
       <td>The list is completely reset or sorted.</td>
+      <td style="text-align:center;">Y</td>
       <td>
         <dl>
           <dt>`models` (<em>Array</em>)</dt>
@@ -336,7 +374,7 @@ Model List instances provide the following events:
 </table>
 
 <p>
-Each of these events is preventable, which means you can subscribe to the "on" phase of an event and call `e.preventDefault()` in your subscriber function to prevent the event from actually occurring. This works because "on" subscribers actually run before an event causes any default logic to run.
+Some of these events are preventable, which means you can subscribe to the "on" phase of an event and call `e.preventDefault()` in your subscriber function to prevent the event from actually occurring. This works because "on" subscribers actually run before an event causes any default logic to run.
 </p>
 
 <p>

commit 74a43501aa9dc13f9aeb4f129cdb31f3baff2ed2

Fix broken load() method (broken in commit 096a3770).

src/app/js/model-list.js
@@ -516,6 +516,8 @@ Y.ModelList = Y.extend(ModelList, Y.Base, {
                 }
 
                 parsed = facade.parsed = self.parse(response);
+
+                self.reset(parsed, options);
                 self.fire(EVT_LOAD, facade);
             }
 

src/app/tests/model-list-test.js
@@ -393,6 +393,46 @@ modelListSuite.add(new Y.Test.Case({
         Assert.areSame(2, calls);
     },
 
+    'load() should reset the list with the loaded items': function () {
+        var calls = 0,
+            list  = this.createList();
+
+        list.sync = function (action, options, callback) {
+            if (action === 'read') {
+                callback(null, '[{"foo":"modelOne"}, {"foo":"modelTwo"}]');
+            }
+        };
+
+        list.load(function (err) {
+            calls += 1;
+
+            Assert.isNull(err, 'load error should be null');
+            Assert.areSame(2, list.size(), 'list should contain two models');
+            Assert.areSame('modelOne', list.item(0).get('foo'), 'modelOne should be loaded correctly');
+            Assert.areSame('modelTwo', list.item(1).get('foo'), 'modelTwo should be loaded correctly');
+        });
+
+        Assert.areSame(1, calls);
+    },
+
+    'load() callback should receive an error when a sync error occurs': function () {
+        var calls = 0,
+            list  = this.createList();
+
+        list.sync = function (action, options, callback) {
+            callback(new Error('OMG!'));
+        };
+
+        list.load(function (err) {
+            calls += 1;
+
+            Assert.isInstanceOf(Error, err);
+            Assert.areSame('OMG!', err.message);
+        });
+
+        Assert.areSame(1, calls);
+    },
+
     'map() should execute a function on every model in the list and return an array of return values': function () {
         var list = this.createList(),
             obj  = {},