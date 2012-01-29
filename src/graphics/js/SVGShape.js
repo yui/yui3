753e68bea51094492944cd3cd92191c91d1e7c50
Tripp <tripp.bridges@yahoo.com>
2012-01-28 20:23:57 -0800

    Allow for standalone shapes.

--- src/graphics/js/SVGShape.js
+++ src/graphics/js/SVGShape.js
@@ -40,12 +40,42 @@ Y.extend(SVGShape, Y.BaseGraphic, Y.mix({
 	 */
 	initializer: function(cfg)
 	{
-		var host = this;
+		var host = this,
+            graphic = cfg.graphic;
 		host.createNode(); 
-		host._graphic = cfg.graphic;
-		host._updateHandler();
+		if(graphic)
+        {
+            host._setGraphic(graphic);
+        }
+        host._updateHandler();
 	},
-   
+ 
+    /**
+     * Set the Graphic instance for the shape.
+     *
+     * @method _setGraphic
+     * @param {Graphic | Node | HTMLElement | String} render This param is used to determine the graphic instance. If it is a `Graphic` instance, it will be assigned
+     * to the `graphic` attribute. Otherwise, a new Graphic instance will be created and rendered into the dom element that the render represents.
+     * @private
+     */
+    _setGraphic: function(render)
+    {
+        var graphic;
+        if(render instanceof Y.SVGGraphic)
+        {
+		    this._graphic = render;
+        }
+        else
+        {
+            render = Y.one(render);
+            graphic = new Y.SVGGraphic({
+                render: render
+            });
+            graphic._appendShape(this);
+            this._graphic = graphic;
+        }
+    },
+
 	/**
 	 * Add a class name to each node.
 	 *
