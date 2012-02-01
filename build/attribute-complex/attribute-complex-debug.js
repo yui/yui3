cbbb7a9af1f572d0562409e9e2dabe206de93524
Satyen Desai <sdesai@yahoo-inc.com>
2012-01-31 16:52:08 -0800

    Pass attribute name to valueFn. Forgot attribute-complex impl. TODO Need to extract common code. See #2530503

--- build/attribute-complex/attribute-complex-debug.js
+++ build/attribute-complex/attribute-complex-debug.js
@@ -89,7 +89,7 @@ YUI.add('attribute-complex', function(Y) {
                     valFn = this[valFn];
                 }
                 if (valFn) {
-                    val = valFn.call(this);
+                    val = valFn.call(this, attr);
                 }
             }
 
