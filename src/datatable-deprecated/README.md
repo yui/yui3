a3b15d60042c81a524cebb94370e5a234a19d04b
Luke Smith <lsmith@yahoo-inc.com>
2012-02-02 21:03:50 -0800

    Move old DataTable code into datatable-deprecated

---
+++ src/datatable-deprecated/README.md
@@ -0,0 +1,7 @@
+DataTable
+=========
+
+Provides a Widget that is responsible for rendering columnar data into a highly
+customizable and fully accessible HTML table. The core functionality of
+DataTable is to visualize structured data as a table. A variety of Plugins can
+then be used to add features to the table such as sorting and scrolling.
---
+++ src/datatable-deprecated/tests/src/datatable-sort.js
@@ -0,0 +1,65 @@
+var suite = new Y.Test.Suite("Y.Plugin.DataTableSort");
+
+suite.add(new Y.Test.Case({
+    name: "DataTableSort tests",
+
+    "datatable-sort should not rely on a link in the template": function () {
+        var table, link, th;
+        
+        table = new Y.DataTable.Base({
+            columnset: [{ key: 'a', sortable: true }],
+            recordset: [{ a: "a1" }, { a: "a2" }, { a: "a3" }]
+        }).plug(Y.Plugin.DataTableSort);
+
+        Y.one('#testbed').empty();
+
+        table.render('#testbed');
+
+        th = table._theadNode.one('th');
+
+        Y.Assert.isInstanceOf(Y.Node, th);
+
+        link = th.one('a');
+        Y.Assert.isInstanceOf(Y.Node, link);
+
+        // Should not error
+        link.simulate('click');
+
+        table.destroy();
+
+        Y.one('#testbed').empty();
+
+        table = new Y.DataTable.Base({
+            columnset: [{ key: 'a', sortable: true }],
+            recordset: [{ a: "a1" }, { a: "a2" }, { a: "a3" }]
+        }).plug(Y.Plugin.DataTableSort, {
+            trigger: {
+                selector: 'th',
+                event: 'click'
+            },
+            template: '{value}' // override the template with link
+        });
+
+        table.render('#testbed');
+
+        th = table._theadNode.one('th');
+
+        Y.Assert.isInstanceOf(Y.Node, th);
+
+        link = th.one('a');
+        Y.Assert.isNull(link);
+
+        // Should not error
+        th.simulate('click');
+
+        table.destroy();
+
+        Y.one('#testbed').empty();
+    }
+
+    // test direction classes
+    // test trigger event
+    // test unplug
+}));
+
+Y.Test.Runner.add(suite);
