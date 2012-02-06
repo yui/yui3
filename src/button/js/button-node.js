5c3eadab8ffa6d8be5384cf5634e850af9123fc0
Matt Sweeney <matt.sweeney@yahoo.com>
2012-02-06 11:07:28 -0800

    call with constructor ATTRS

--- src/button/js/button-node.js
+++ src/button/js/button-node.js
@@ -1,15 +1,26 @@
 function ButtonNode(config) {
-    ButtonNode.superclass.constructor.call(this, config.srcNode || '<button/>');
+    ButtonNode.superclass.constructor.call(
+        this, config.srcNode || Y.DOM.create(this.TEMPLATE)
+    );
     this.initializer(config);
+}
 
+Y.extend(ButtonNode, Y.Node);
+
+// add ButtonBase API without clobbering Node/Attribute API
+Y.mix(ButtonNode.prototype, Y.ButtonBase.prototype);
+
+ButtonNode.prototype.getNode = function() {
+    return this;
 };
 
-Y.extend(ButtonNode, Y.Node, {
-    initNode: function() {
-        this._host = this;
-    }
-});
+// so can call with ButtonATTRS
+ButtonNode.prototype.renderAttrs = function(config) {
+    Y.AttributeCore.call(this, ButtonNode.ATTRS, config);
+    Y.AttributeEvents.apply(this, arguments);
+    Y.AttributeExtras.apply(this, arguments);
+};
 
-Y.mix(ButtonNode.prototype, Y.ButtonBase.prototype, true);
-ButtonNode.ATTRS = Y.merge(Y.ButtonBase.ATTRS, Y.Node.ATTRS);
-Y.Button = ButtonNode;
+ButtonNode.prototype.select = Y.ButtonBase.prototype.select;
+ButtonNode.ATTRS = Y.merge(Y.Node.ATTRS, Y.ButtonBase.ATTRS);
+Y.ButtonNode = ButtonNode;
