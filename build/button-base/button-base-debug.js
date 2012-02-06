4bba287ffd8fe4c41706bc8beb9e1cf33b1f5a13
Matt Sweeney <matt.sweeney@yahoo.com>
2012-02-06 12:27:06 -0800

    build button

--- build/button-base/button-base-debug.js
+++ build/button-base/button-base-debug.js
@@ -332,20 +332,27 @@ Y.mix(Button.prototype, Y.Attribute.prototype);
 // Export Button
 Y.ButtonBase = Button;
 function ButtonNode(config) {
-    ButtonNode.superclass.constructor.call(
-        this, config.srcNode || Y.DOM.create(this.TEMPLATE)
-    );
+    var node;
+
+    if (config && config.srcNode) { // Y.Node, selector string or DOM node
+        node = config.srcNode._node || config.srcNode; // TODO: allow new Y.Node(nodeInstance)
+    } else {
+        node = Y.DOM.create(this.TEMPLATE);
+    }
+    ButtonNode.superclass.constructor.call(this, node);
     this.initializer(config);
 }
 
 Y.extend(ButtonNode, Y.Node);
 
+// add ButtonBase API without clobbering Node/Attribute API
 Y.mix(ButtonNode.prototype, Y.ButtonBase.prototype);
 
 ButtonNode.prototype.getNode = function() {
     return this;
 };
 
+// so can call with ButtonATTRS
 ButtonNode.prototype.renderAttrs = function(config) {
     Y.AttributeCore.call(this, ButtonNode.ATTRS, config);
     Y.AttributeEvents.apply(this, arguments);
@@ -354,7 +361,9 @@ ButtonNode.prototype.renderAttrs = function(config) {
 
 ButtonNode.prototype.select = Y.ButtonBase.prototype.select;
 ButtonNode.ATTRS = Y.merge(Y.Node.ATTRS, Y.ButtonBase.ATTRS);
+
 Y.ButtonNode = ButtonNode;
+Y.Button = ButtonNode;
 function ButtonPlugin(config) {
     this._host = config.host;
     ButtonPlugin.superclass.constructor.apply(this, arguments);
