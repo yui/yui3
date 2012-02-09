4bc090583f2a5d4fee80ca7a79a993d4e5ac14ec
Ryan Grove <ryan@wonko.com>
2012-02-08 18:30:18 -0800

    Build autocomplete.

--- build/autocomplete-plugin/autocomplete-plugin-debug.js
+++ build/autocomplete-plugin/autocomplete-plugin-debug.js
@@ -1,32 +1,26 @@
 YUI.add('autocomplete-plugin', function(Y) {
 
 /**
- * Binds an AutoCompleteList instance to a Node instance.
- *
- * @module autocomplete
- * @submodule autocomplete-plugin
- */
+Binds an AutoCompleteList instance to a Node instance.
+
+@module autocomplete
+@submodule autocomplete-plugin
+**/
 
 /**
- * <p>
- * Binds an AutoCompleteList instance to a Node instance.
- * </p>
- *
- * <p>
- * Example:
- * </p>
- *
- * <pre>
- * Y.one('#my-input').plug(Y.Plugin.AutoComplete, {
- * &nbsp;&nbsp;source: 'select * from search.suggest where query="{query}"'
- * });
- * &nbsp;
- * // You can now access the AutoCompleteList instance at Y.one('#my-input').ac
- * </pre>
- *
- * @class Plugin.AutoComplete
- * @extends AutoCompleteList
- */
+Binds an AutoCompleteList instance to a Node instance.
+
+@example
+
+    Y.one('#my-input').plug(Y.Plugin.AutoComplete, {
+        source: 'select * from search.suggest where query="{query}"'
+    });
+
+    // You can now access the AutoCompleteList instance at Y.one('#my-input').ac
+
+@class Plugin.AutoComplete
+@extends AutoCompleteList
+**/
 
 var Plugin = Y.Plugin;
 
