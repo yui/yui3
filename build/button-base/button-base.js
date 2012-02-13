1c782a4bb13712fd7699d6ef042138abaae65ac5
Matt Sweeney <matt.sweeney@yahoo.com>
2012-02-13 12:01:49 -0800

    build button

--- build/button-base/button-base.js
+++ build/button-base/button-base.js
@@ -161,33 +161,6 @@ Button.CLASS_NAMES = {
 Y.mix(Button.prototype, Y.AttributeCore.prototype);
 // Export Button
 Y.ButtonBase = Button;
-function ButtonNode(config) {
-    ButtonNode.superclass.constructor.call(this, config.srcNode);
-    this.initializer(config);
-}
-
-Y.extend(ButtonNode, Y.Node, {
-    // call with ButtonNode.ATTRS
-    _initAttributes: function(config) {
-        Y.AttributeCore.call(this, ButtonNode.ATTRS, config);
-    },
-
-    _initNode: function(config) {
-        // enable Y.one() to return ButtonNode (for eventTarget, etc)
-//        Y.Node._instances[this._yuid] = this;
-        this._host = this;
-    }
-});
-
-// add ButtonBase API without clobbering Node/Attribute API
-Y.mix(ButtonNode.prototype, Y.ButtonBase.prototype);
-    
-// merge Node and Button ATTRS
-// TODO: protect existing? (what if Y.Node.ATTRS.disabled.getter)
-ButtonNode.ATTRS = Y.merge(Y.Node.ATTRS, Y.ButtonBase.ATTRS);
-ButtonNode.ATTRS.label._bypassProxy = true;
-
-Y.ButtonNode = ButtonNode;
 /**
     var node = Y.one('#my-button').plug(Y.Plugin.Button, {
         label: 'my button'
---
+++ src/button/js/button.js
@@ -0,0 +1,109 @@
+function ButtonWidget(config) {
+    ButtonWidget.superclass.constructor.apply(this, arguments);
+}
+
+Y.extend(ButtonWidget, Y.Widget,  {
+    initializer: function(config) {
+        this._host = this.get('boundingBox');
+    },
+
+    BOUNDING_TEMPLATE: Y.ButtonBase.prototype.TEMPLATE,
+    CONTENT_TEMPLATE: null,
+
+    bindUI: function() {
+        var button = this;
+        this.after('labelChange', this._afterLabelChange);
+        this.after('disabledChange', this._afterDisabledChange);
+        this.after('selectedChange', this._afterSelectedChange);
+    },
+
+    _uiSetSelected: function(value) {
+        this.get('contentBox').toggleClass('yui3-button-selected', value); 
+        // aria
+    },
+    _afterLabelChange: function(e) {
+        this._uiSetLabel(e.newVal);
+    },
+
+    _afterDisabledChange: function(e) {
+        this._uiSetDisabled(e.newVal);
+    },
+
+    _afterSelectedChange: function(e) {
+        this._uiSetSelected(e.newVal);
+    },
+
+    syncUI: function() {
+        this._uiSetLabel(this.get('label'));
+        this._uiSetDisabled(this.get('disabled'));
+        this._uiSetSelected(this.get('selected'));
+    },
+
+}, {
+    NAME: 'button',
+});
+
+ButtonWidget.ATTRS = {
+    label: {
+        value: Y.ButtonBase.ATTRS.label.value
+    },
+
+    disabled: {
+        value: false
+    },
+
+    selected: {
+        value: false
+    }
+};
+
+ButtonWidget.HTML_PARSER = {
+    label: function(node) {
+        this._host = node; // TODO: remove
+        return this._uiGetLabel();
+    },
+
+    disabled: function(node) {
+        return node.getDOMNode().disabled;
+    },
+
+    selected: function(node) {
+        return node.hasClass('yui3-button-selected');
+    }
+};
+
+Y.mix(ButtonWidget.prototype, Y.ButtonBase.prototype);
+
+Y.Button = ButtonWidget;
+
+function ToggleButton(config) {
+    ButtonWidget.superclass.constructor.apply(this, arguments);
+}
+
+// TODO: move to ButtonBase subclass to enable toggle plugin, widget, etc.
+Y.extend(ToggleButton, Y.Button,  {
+    trigger: 'click',
+
+    select: function() {
+        this.set('selected', true);
+    },
+
+    unselect: function() {
+        this.set('selected', false);
+    },
+
+    toggle: function() {
+        var button = this;
+        button.set('selected', !button.get('selected'));
+    },
+
+    bindUI: function() {
+        var button = this;
+        ToggleButton.superclass.bindUI.call(button);
+        button.get('contentBox').on(button.trigger, button.toggle, button);
+    }
+}, {
+    NAME: 'toggleButton'
+});
+
+Y.ToggleButton = ToggleButton;
