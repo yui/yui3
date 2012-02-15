d6322294cc8480048c5b018858fc1680a027f6de
Ryan Grove <ryan@wonko.com>
2012-02-15 13:59:52 -0800

    Improve event-valuechange API docs.

--- src/event-valuechange/js/event-valuechange.js
+++ src/event-valuechange/js/event-valuechange.js
@@ -1,17 +1,36 @@
 /**
- * Adds a synthetic <code>valueChange</code> event that fires when the
- * <code>value</code> property of an input field or textarea changes as a result
- * of a keystroke, mouse operation, or input method editor (IME) input event.
- *
- * @module event-valuechange
- */
+Adds a synthetic `valueChange` event that fires when the `value` property of an
+`<input>` or `<textarea>` node changes as a result of a keystroke, mouse
+operation, or input method editor (IME) input event.
+
+Usage:
+
+    YUI().use('event-valuechange', function (Y) {
+        Y.one('#my-input').on('valueChange', function (e) {
+            Y.log('previous value: ' + e.prevVal);
+            Y.log('new value: ' + e.newVal);
+        });
+    });
+
+@module event-valuechange
+**/
 
 /**
- * Provides the implementation for the synthetic <code>valueChange</code> event.
- *
- * @class ValueChange
- * @static
- */
+Provides the implementation for the synthetic `valueChange` event. This class
+isn't meant to be used directly, but is public to make monkeypatching possible.
+
+Usage:
+
+    YUI().use('event-valuechange', function (Y) {
+        Y.one('#my-input').on('valueChange', function (e) {
+            Y.log('previous value: ' + e.prevVal);
+            Y.log('new value: ' + e.newVal);
+        });
+    });
+
+@class ValueChange
+@static
+*/
 
 var YArray = Y.Array,
 
@@ -22,26 +41,26 @@ VC = {
     // -- Static Constants -----------------------------------------------------
 
     /**
-     * Interval (in milliseconds) at which to poll for changes to the value of
-     * an element with one or more <code>valueChange</code> subscribers when the
-     * user is likely to be interacting with it.
-     *
-     * @property POLL_INTERVAL
-     * @type Number
-     * @default 50
-     * @static
-     */
+    Interval (in milliseconds) at which to poll for changes to the value of an
+    element with one or more `valueChange` subscribers when the user is likely
+    to be interacting with it.
+
+    @property POLL_INTERVAL
+    @type Number
+    @default 50
+    @static
+    **/
     POLL_INTERVAL: 50,
 
     /**
-     * Timeout (in milliseconds) after which to stop polling when there hasn't
-     * been any new activity (keypresses, mouse clicks, etc.) on an element.
-     *
-     * @property TIMEOUT
-     * @type Number
-     * @default 10000
-     * @static
-     */
+    Timeout (in milliseconds) after which to stop polling when there hasn't been
+    any new activity (keypresses, mouse clicks, etc.) on an element.
+
+    @property TIMEOUT
+    @type Number
+    @default 10000
+    @static
+    **/
     TIMEOUT: 10000,
 
     // -- Protected Static Properties ------------------------------------------
@@ -53,16 +72,16 @@ VC = {
     // -- Protected Static Methods ---------------------------------------------
 
     /**
-     * Called at an interval to poll for changes to the value of the specified
-     * node.
-     *
-     * @method _poll
-     * @param {Node} node
-     * @param {String} stamp
-     * @param {EventFacade} e
-     * @protected
-     * @static
-     */
+    Called at an interval to poll for changes to the value of the specified
+    node.
+
+    @method _poll
+    @param {Node} node
+    @param {String} stamp
+    @param {EventFacade} e
+    @protected
+    @static
+    **/
     _poll: function (node, stamp, e) {
         var domNode = node._node, // performance cheat; getValue() is a big hit when polling
             newVal  = domNode && domNode.value,
@@ -93,14 +112,14 @@ VC = {
     },
 
     /**
-     * Restarts the inactivity timeout for the specified node.
-     *
-     * @method _refreshTimeout
-     * @param {Node} node
-     * @param {String} stamp
-     * @protected
-     * @static
-     */
+    Restarts the inactivity timeout for the specified node.
+
+    @method _refreshTimeout
+    @param {Node} node
+    @param {String} stamp
+    @protected
+    @static
+    **/
     _refreshTimeout: function (node, stamp) {
         VC._stopTimeout(node, stamp); // avoid dupes
 
@@ -114,22 +133,21 @@ VC = {
     },
 
     /**
-     * Begins polling for changes to the <code>value</code> property of the
-     * specified node. If polling is already underway for the specified node,
-     * it will not be restarted unless the <i>force</i> parameter is
-     * <code>true</code>
-     *
-     * @method _startPolling
-     * @param {Node} node Node to watch.
-     * @param {String} stamp (optional) Object stamp for the node. Will be
-     *   generated if not provided (provide it to improve performance).
-     * @param {EventFacade} e (optional) Event facade of the event that
-     *   initiated the polling (if any).
-     * @param {Boolean} force (optional) If <code>true</code>, polling will be
-     *   restarted even if we're already polling this node.
-     * @protected
-     * @static
-     */
+    Begins polling for changes to the `value` property of the specified node. If
+    polling is already underway for the specified node, it will not be restarted
+    unless the _force_ parameter is `true`
+
+    @method _startPolling
+    @param {Node} node Node to watch.
+    @param {String} [stamp] Object stamp for the node. Will be generated if not
+        provided (provide it to improve performance).
+    @param {EventFacade} [e] Event facade of the event that initiated the
+        polling (if any).
+    @param {Boolean} [force=false] If `true`, polling will be restarted even if
+        we're already polling this node.
+    @protected
+    @static
+    **/
     _startPolling: function (node, stamp, e, force) {
         if (!stamp) {
             stamp = Y.stamp(node);
@@ -156,15 +174,14 @@ VC = {
     },
 
     /**
-     * Stops polling for changes to the specified node's <code>value</code>
-     * attribute.
-     *
-     * @method _stopPolling
-     * @param {Node} node
-     * @param {String} stamp (optional)
-     * @protected
-     * @static
-     */
+    Stops polling for changes to the specified node's `value` attribute.
+
+    @method _stopPolling
+    @param {Node} node
+    @param {String} [stamp]
+    @protected
+    @static
+    **/
     _stopPolling: function (node, stamp) {
         if (!stamp) {
             stamp = Y.stamp(node);
@@ -177,14 +194,14 @@ VC = {
     },
 
     /**
-     * Clears the inactivity timeout for the specified node, if any.
-     *
-     * @method _stopTimeout
-     * @param {Node} node
-     * @param {String} stamp (optional)
-     * @protected
-     * @static
-     */
+    Clears the inactivity timeout for the specified node, if any.
+
+    @method _stopTimeout
+    @param {Node} node
+    @param {String} [stamp]
+    @protected
+    @static
+    **/
     _stopTimeout: function (node, stamp) {
         if (!stamp) {
             stamp = Y.stamp(node);
@@ -196,25 +213,25 @@ VC = {
     // -- Protected Static Event Handlers --------------------------------------
 
     /**
-     * Stops polling when a node's blur event fires.
-     *
-     * @method _onBlur
-     * @param {EventFacade} e
-     * @protected
-     * @static
-     */
+    Stops polling when a node's blur event fires.
+
+    @method _onBlur
+    @param {EventFacade} e
+    @protected
+    @static
+    **/
     _onBlur: function (e) {
         VC._stopPolling(e.currentTarget);
     },
 
     /**
-     * Resets a node's history and starts polling when a focus event occurs.
-     *
-     * @method _onFocus
-     * @param {EventFacade} e
-     * @protected
-     * @static
-     */
+    Resets a node's history and starts polling when a focus event occurs.
+
+    @method _onFocus
+    @param {EventFacade} e
+    @protected
+    @static
+    **/
     _onFocus: function (e) {
         var node = e.currentTarget;
 
@@ -223,25 +240,25 @@ VC = {
     },
 
     /**
-     * Starts polling when a node receives a keyDown event.
-     *
-     * @method _onKeyDown
-     * @param {EventFacade} e
-     * @protected
-     * @static
-     */
+    Starts polling when a node receives a keyDown event.
+
+    @method _onKeyDown
+    @param {EventFacade} e
+    @protected
+    @static
+    **/
     _onKeyDown: function (e) {
         VC._startPolling(e.currentTarget, null, e);
     },
 
     /**
-     * Starts polling when an IME-related keyUp event occurs on a node.
-     *
-     * @method _onKeyUp
-     * @param {EventFacade} e
-     * @protected
-     * @static
-     */
+    Starts polling when an IME-related keyUp event occurs on a node.
+
+    @method _onKeyUp
+    @param {EventFacade} e
+    @protected
+    @static
+    **/
     _onKeyUp: function (e) {
         // These charCodes indicate that an IME has started. We'll restart
         // polling and give the IME up to 10 seconds (by default) to finish.
@@ -251,27 +268,27 @@ VC = {
     },
 
     /**
-     * Starts polling when a node receives a mouseDown event.
-     *
-     * @method _onMouseDown
-     * @param {EventFacade} e
-     * @protected
-     * @static
-     */
+    Starts polling when a node receives a mouseDown event.
+
+    @method _onMouseDown
+    @param {EventFacade} e
+    @protected
+    @static
+    **/
     _onMouseDown: function (e) {
         VC._startPolling(e.currentTarget, null, e);
     },
 
     /**
-     * Called when event-valuechange receives a new subscriber.
-     *
-     * @method _onSubscribe
-     * @param {Node} node
-     * @param {Subscription} subscription
-     * @param {SyntheticEvent.Notifier} notifier
-     * @protected
-     * @static
-     */
+    Called when the `valueChange` event receives a new subscriber.
+
+    @method _onSubscribe
+    @param {Node} node
+    @param {Subscription} subscription
+    @param {SyntheticEvent.Notifier} notifier
+    @protected
+    @static
+    **/
     _onSubscribe: function (node, subscription, notifier) {
         var stamp     = Y.stamp(node),
             notifiers = VC._notifiers[stamp];
@@ -294,15 +311,15 @@ VC = {
     },
 
     /**
-     * Called when event-valuechange loses a subscriber.
-     *
-     * @method _onUnsubscribe
-     * @param {Node} node
-     * @param {Subscription} subscription
-     * @param {SyntheticEvent.Notifier} notifier
-     * @protected
-     * @static
-     */
+    Called when event-valuechange loses a subscriber.
+
+    @method _onUnsubscribe
+    @param {Node} node
+    @param {Subscription} subscription
+    @param {SyntheticEvent.Notifier} notifier
+    @protected
+    @static
+    **/
     _onUnsubscribe: function (node, subscription, notifier) {
         var stamp     = Y.stamp(node),
             notifiers = VC._notifiers[stamp],
@@ -324,45 +341,32 @@ VC = {
 };
 
 /**
- * <p>
- * Synthetic event that fires when the <code>value</code> property of an input
- * field or textarea changes as a result of a keystroke, mouse operation, or
- * input method editor (IME) input event.
- * </p>
- *
- * <p>
- * Unlike the <code>onchange</code> event, this event fires when the value
- * actually changes and not when the element loses focus. This event also
- * reports IME and multi-stroke input more reliably than <code>oninput</code> or
- * the various key events across browsers.
- * </p>
- *
- * @example
- *
- *     YUI().use('event-valuechange', function (Y) {
- *       Y.one('input').on('valueChange', function (e) {
- *         // Handle valueChange events on the first input element on the page.
- *       });
- *     });
- *
- * @event valueChange
- * @param {EventFacade} e Event facade with the following additional
- *   properties:
- *
- * <dl>
- *   <dt>prevVal (String)</dt>
- *   <dd>
- *     Previous value before the latest change.
- *   </dd>
- *
- *   <dt>newVal (String)</dt>
- *   <dd>
- *     New value after the latest change.
- *   </dd>
- * </dl>
- *
- * @for YUI
- */
+Synthetic event that fires when the `value` property of an `<input>` or
+`<textarea>` node changes as a result of a user-initiated keystroke, mouse
+operation, or input method editor (IME) input event.
+
+Unlike the `onchange` event, this event fires when the value actually changes
+and not when the element loses focus. This event also reports IME and multi-
+stroke input more reliably than `oninput` or the various key events across
+browsers.
+
+For performance reasons, only focused nodes are monitored for changes, so
+programmatic value changes on nodes that don't have focus won't be detected.
+
+@example
+
+    YUI().use('event-valuechange', function (Y) {
+        Y.one('#my-input').on('valueChange', function (e) {
+            Y.log('previous value: ' + e.prevVal);
+            Y.log('new value: ' + e.newVal);
+        });
+    });
+
+@event valueChange
+@param {String} prevVal Previous value prior to the latest change.
+@param {String} newVal New value after the latest change.
+@for YUI
+**/
 
 Y.Event.define('valueChange', {
     detach: VC._onUnsubscribe,
