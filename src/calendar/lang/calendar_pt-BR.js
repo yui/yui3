b2ee3f86c678a01682f5932d0dd9adaca64bc1c7
Leonardo Martins <leonardo@venidera.com>
2012-02-15 17:01:43 -0200

    Added pt-BR language files to calendar

---
+++ src/calendar/lang/calendar_pt-BR.js
@@ -0,0 +1,5 @@
+{
+    weekdays : ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+    short_weekdays : ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+    very_short_weekdays : ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"]
+}
