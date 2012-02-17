de6a2676e7f98228c9836c6f15af85d577ec4775
Luke Smith <lsmith@yahoo-inc.com>
2012-02-16 18:16:48 -0800

    Add stub for datatable-message tests

---
+++ src/datatable/tests/src/datatable-message.js
@@ -0,0 +1,12 @@
+var suite = new Y.Test.Suite("datatable-message");
+
+suite.add(new Y.Test.Case({
+    name: "datatable-message",
+
+    "": function () {
+    }
+
+
+}));
+
+Y.Test.Runner.add(suite);
