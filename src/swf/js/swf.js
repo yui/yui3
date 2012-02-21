199dc81906fc78f117294681cf521344e1dc0855
Ryan Grove <ryan@wonko.com>
2012-02-21 10:39:01 -0800

    Fix JSLint warnings and dangerous brace style.

--- src/swf/js/swf.js
+++ src/swf/js/swf.js
@@ -55,11 +55,11 @@ function SWF (p_oElement /*:String*/, swfURL /*:String*/, p_oAttributes /*:Objec
     var _id = this._id;
     var oElement = Node.one(p_oElement);
 
-    var flashVersion = p_oAttributes["version"] || FLASH_VER;
+    var flashVersion = p_oAttributes.version || FLASH_VER;
     var flashVersionSplit = (flashVersion + '').split(".");
-    var isFlashVersionRight = SWFDetect.isFlashVersionAtLeast(parseInt(flashVersionSplit[0]), parseInt(flashVersionSplit[1]), parseInt(flashVersionSplit[2]));
+    var isFlashVersionRight = SWFDetect.isFlashVersionAtLeast(parseInt(flashVersionSplit[0], 10), parseInt(flashVersionSplit[1], 10), parseInt(flashVersionSplit[2], 10));
     var canExpressInstall = (SWFDetect.isFlashVersionAtLeast(8,0,0));
-    var shouldExpressInstall = canExpressInstall && !isFlashVersionRight && p_oAttributes["useExpressInstall"];
+    var shouldExpressInstall = canExpressInstall && !isFlashVersionRight && p_oAttributes.useExpressInstall;
     var flashURL = (shouldExpressInstall)?EXPRESS_INSTALL_URL:swfURL;
     var objstring = '<object ';
     var w, h;
@@ -67,57 +67,56 @@ function SWF (p_oElement /*:String*/, swfURL /*:String*/, p_oAttributes /*:Objec
 
     Y.SWF._instances[_id] = this;
     if (oElement && (isFlashVersionRight || shouldExpressInstall) && flashURL) {
-                objstring += 'id="' + _id + '" ';
-                if (uA.ie) {
-                    objstring += 'classid="' + FLASH_CID + '" ';
-                }
-                else {
-                    objstring += 'type="' + FLASH_TYPE + '" data="' + Escape.html(flashURL) + '" ';
-                }
-
-                w = "100%";
-                h = "100%";
-
-                objstring += 'width="' + w + '" height="' + h + '">';
-
-                if (uA.ie) {
-                    objstring += '<param name="movie" value="' + Escape.html(flashURL) + '"/>';
-                }
-
-                for (var attribute in p_oAttributes.fixedAttributes) {
-                    if (possibleAttributes.hasOwnProperty(attribute)) {
-                        objstring += '<param name="' + Escape.html(attribute) + '" value="' + Escape.html(p_oAttributes.fixedAttributes[attribute]) + '"/>';
-                    }
-                }
-
-                for (var flashvar in p_oAttributes.flashVars) {
-                    var fvar = p_oAttributes.flashVars[flashvar];
-                    if (Lang.isString(fvar)) {
-                        flashvarstring += "&" + Escape.html(flashvar) + "=" + Escape.html(encodeURIComponent(fvar));
-                    }
-                }
-
-                if (flashvarstring) {
-                    objstring += '<param name="flashVars" value="' + flashvarstring + '"/>';
-                }
-
-                objstring += "</object>";
-                oElement.setContent(objstring);
-
-                this._swf = Node.one("#" + _id);
-            }
-    /**
-    * Fired when the Flash player version on the user's machine is below the required value.
-    *
-    * @event wrongflashversion
-    */
-    else {
-            var event = {};
-            event.type = "wrongflashversion";
-            this.publish("wrongflashversion", {fireOnce:true});
-            this.fire("wrongflashversion", event);
+        objstring += 'id="' + _id + '" ';
+        if (uA.ie) {
+            objstring += 'classid="' + FLASH_CID + '" ';
+        } else {
+            objstring += 'type="' + FLASH_TYPE + '" data="' + Escape.html(flashURL) + '" ';
         }
-};
+
+        w = "100%";
+        h = "100%";
+
+        objstring += 'width="' + w + '" height="' + h + '">';
+
+        if (uA.ie) {
+            objstring += '<param name="movie" value="' + Escape.html(flashURL) + '"/>';
+        }
+
+        for (var attribute in p_oAttributes.fixedAttributes) {
+            if (possibleAttributes.hasOwnProperty(attribute)) {
+                objstring += '<param name="' + Escape.html(attribute) + '" value="' + Escape.html(p_oAttributes.fixedAttributes[attribute]) + '"/>';
+            }
+        }
+
+        for (var flashvar in p_oAttributes.flashVars) {
+            var fvar = p_oAttributes.flashVars[flashvar];
+            if (Lang.isString(fvar)) {
+                flashvarstring += "&" + Escape.html(flashvar) + "=" + Escape.html(encodeURIComponent(fvar));
+            }
+        }
+
+        if (flashvarstring) {
+            objstring += '<param name="flashVars" value="' + flashvarstring + '"/>';
+        }
+
+        objstring += "</object>";
+        oElement.setContent(objstring);
+
+        this._swf = Node.one("#" + _id);
+    } else {
+        /**
+         * Fired when the Flash player version on the user's machine is
+         * below the required value.
+         *
+         * @event wrongflashversion
+         */
+        var event = {};
+        event.type = "wrongflashversion";
+        this.publish("wrongflashversion", {fireOnce:true});
+        this.fire("wrongflashversion", event);
+    }
+}
 
 /**
  * @private
@@ -140,28 +139,20 @@ SWF.eventHandler = function (swfid, event) {
     SWF._instances[swfid]._eventHandler(event);
 };
 
-SWF.prototype =
-{
+SWF.prototype = {
     /**
      * @private
      * Propagates a specific event from Flash to JS.
      * @method _eventHandler
      * @param event {Object} The event to be propagated from Flash.
      */
-
-    _eventHandler: function(event)
-    {
-        if (event.type === "swfReady")
-        {
+    _eventHandler: function(event) {
+        if (event.type === "swfReady") {
             this.publish("swfReady", {fireOnce:true});
             this.fire("swfReady", event);
-        }
-        else if(event.type === "log")
-        {
+        } else if(event.type === "log") {
             Y.log(event.message, event.category, this.toString());
-        }
-        else
-        {
+        } else {
             this.fire(event.type, event);
         }
     },
@@ -173,16 +164,15 @@ SWF.prototype =
      * @param func {String} the name of the function to call
      * @param args {Array} the set of arguments to pass to the function.
      */
-
-    callSWF: function (func, args)
-    {
+    callSWF: function (func, args) {
         if (!args) {
-              args= [];
-        };
+            args= [];
+        }
+
         if (this._swf._node[func]) {
-        return(this._swf._node[func].apply(this._swf._node, args));
+            return(this._swf._node[func].apply(this._swf._node, args));
         } else {
-        return null;
+            return null;
         }
     },
 
@@ -192,8 +182,7 @@ SWF.prototype =
      * @method toString
      * @return {String} Unique name of the SWF instance.
      */
-    toString: function()
-    {
+    toString: function() {
         return "SWF " + this._id;
     }
 };
