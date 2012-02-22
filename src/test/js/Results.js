33d85edf47749fa345d7b636b9b4b9d0d0386f44
Dav Glass <davglass@gmail.com>
2012-02-22 12:11:57 -0600

    Migrated the current YUITest project into our source tree
        Created a Makefile and wrapper to copy the source
        into the proper directory. Then setup the build
        to wrap the module, add logging support with
        `Y.log` and added all the aliases back into `Y`
        and `Y.Test`

---
+++ src/test/js/Results.js
@@ -0,0 +1,74 @@
+/**
+ * Convenience type for storing and aggregating
+ * test result information.
+ * @private
+ * @namespace YUITest
+ * @class Results
+ * @constructor
+ * @param {String} name The name of the test.
+ */
+YUITest.Results = function(name){
+
+    /**
+     * Name of the test, test case, or test suite.
+     * @type String
+     * @property name
+     */
+    this.name = name;
+    
+    /**
+     * Number of passed tests.
+     * @type int
+     * @property passed
+     */
+    this.passed = 0;
+    
+    /**
+     * Number of failed tests.
+     * @type int
+     * @property failed
+     */
+    this.failed = 0;
+    
+    /**
+     * Number of errors that occur in non-test methods.
+     * @type int
+     * @property errors
+     */
+    this.errors = 0;
+    
+    /**
+     * Number of ignored tests.
+     * @type int
+     * @property ignored
+     */
+    this.ignored = 0;
+    
+    /**
+     * Number of total tests.
+     * @type int
+     * @property total
+     */
+    this.total = 0;
+    
+    /**
+     * Amount of time (ms) it took to complete testing.
+     * @type int
+     * @property duration
+     */
+    this.duration = 0;
+}
+
+/**
+ * Includes results from another results object into this one.
+ * @param {YUITest.Results} result The results object to include.
+ * @method include
+ * @return {void}
+ */
+YUITest.Results.prototype.include = function(results){
+    this.passed += results.passed;
+    this.failed += results.failed;
+    this.ignored += results.ignored;
+    this.total += results.total;
+    this.errors += results.errors;
+};
---
+++ src/test/js/YTestWrap.js
@@ -0,0 +1,11 @@
+
+Y.namespace('Test');
+
+YUITest.Object = Y.Object;
+
+YUITest.Array = Y.Array;
+
+YUITest.Util = {
+    mix: Y.mix,
+    JSON: Y.JSON
+}
