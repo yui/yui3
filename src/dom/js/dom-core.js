6e32c30fa308d7f01a91d97f9ec19d0a57901ebe
Matt Sweeney <matt.sweeney@yahoo.com>
2012-02-28 19:07:48 -0800

    break out inline id accessor into getId/setId

--- src/dom/js/dom-core.js
+++ src/dom/js/dom-core.js
@@ -41,6 +41,28 @@ Y_DOM = {
         return Y_DOM.allById(id, doc)[0] || null;
     },
 
+    getId: function(node) {
+        var id;
+        // IE < 8 returns HTMLElement from FORM when INPUT name === "id"
+        // and HTMLCollection when INPUT id === "id"
+        // via both getAttribute and form.id 
+        if (node.id && !node.id.tagName && !node.id.item) {
+            id = node.id;
+        } else if (node.attributes && node.attributes.id) {
+            id = node.attributes.id.value;
+        }
+
+        return id;
+    },
+
+    setId: function(node, id) {
+        if (node.setAttribute) {
+            node.setAttribute('id', id);
+        } else {
+            node.id = id;
+        }
+    },
+
     /*
      * Finds the ancestor of the element.
      * @method ancestor
