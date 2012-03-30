b70337fbc5f4c0234682d342ef9bce1bb751fac4
Derek Gathright <drgath@gmail.com>
2012-03-29 17:29:14 -0700

    fixes 2531959 - Added event-mousewheel to scrollview dependencies

--- src/loader/js/yui3.js
+++ src/loader/js/yui3.js
@@ -2002,6 +2002,7 @@ YUI.Env[Y.version].modules = YUI.Env[Y.version].modules || {
         "requires": [
             "widget", 
             "event-gestures", 
+            "event-mousewheel", 
             "transition"
         ], 
         "skinnable": true
@@ -2427,4 +2428,4 @@ YUI.Env[Y.version].modules = YUI.Env[Y.version].modules || {
         ]
     }
 };
-YUI.Env[Y.version].md5 = 'e10cbe2ecc9d0ecc296ae706d7e27262';
+YUI.Env[Y.version].md5 = 'f5a3bc9bda2441a3b15fb52c567fc1f7';
