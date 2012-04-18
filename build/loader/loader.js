a25cda551beed9d6c87463a240e86fd578eee325
Dav Glass <davglass@gmail.com>
2012-04-18 14:40:26 -0500

    Seed Build

--- build/loader/loader.js
+++ build/loader/loader.js
@@ -98,6 +98,8 @@ if (!YUI.Env[Y.version]) {
 }
 
 
+/*jslint forin: true */
+
 /**
  * Loader dynamically loads script and css files.  It includes the dependency
  * information for the version of the library in use, and will automatically pull in
@@ -537,6 +539,7 @@ Y.Loader = function(o) {
     * Should Loader fetch scripts in `async`, defaults to `true`
     * @property async
     */
+
     self.async = true;
 
     self._inspectPage();
@@ -552,6 +555,12 @@ Y.Loader = function(o) {
     if (Y.config.tests) {
         self.testresults = Y.config.tests;
     }
+    
+    /*
+    if (self.ignoreRegistered) {
+        self.loaded = {};
+    }
+    */
 
     /**
      * List of rollup files found in the library metadata
@@ -618,9 +627,36 @@ Y.Loader = function(o) {
      */
     //self.results = {};
 
+    if (self.ignoreRegistered) {
+        //Clear inpage already processed modules.
+        self.resetModules();
+    }
+
 };
 
 Y.Loader.prototype = {
+    resetModules: function() {
+        var self = this;
+        oeach(self.moduleInfo, function(mod) {
+            var name = mod.name,
+                details  = (YUI.Env.mods[name] ? YUI.Env.mods[name].details : null);
+
+            if (details) {
+                self.moduleInfo[name]._reset = true;
+                self.moduleInfo[name].requires = details.requires || [];
+                self.moduleInfo[name].optional = details.optional || [];
+                self.moduleInfo[name].supersedes = details.supercedes || [];
+            }
+
+            if (mod.defaults) {
+                oeach(mod.defaults, function(val, key) {
+                    if (mod[key]) {
+                        mod[key] = mod.defaults[key];
+                    }
+                });
+            }
+        });
+    },
     /**
     Regex that matches a CSS URL. Used to guess the file type when it's not
     specified.
@@ -1035,10 +1071,25 @@ Y.Loader.prototype = {
         this.moduleInfo[name] = o;
 
         o.requires = o.requires || [];
+        if (!o.defaults) {
+            o.defaults = {
+                requires: o.requires ? [].concat(o.requires) : null,
+                supersedes: o.supersedes ? [].concat(o.supersedes) : null,
+                optional: o.optional ? [].concat(o.optional) : null
+            };
+        }
 
         if (o.skinnable) {
-            skinname = this._addSkin(this.skin.defaultSkin, name);
-            o.requires.unshift(skinname);
+            overrides = this.skin.overrides;
+            if (overrides && overrides[name]) {
+                for (j = 0; j < overrides[name].length; j++) {
+                    skinname = this._addSkin(overrides[name][j], name);
+                    o.requires.unshift(skinname);
+                }
+            } else {
+                skinname = this._addSkin(this.skin.defaultSkin, name);
+                o.requires.unshift(skinname);
+            }
         }
 
         o.requires = this.filterRequires(o.requires) || [];
@@ -1357,7 +1408,6 @@ Y.Loader.prototype = {
 
         // console.log('cache: ' + mod.langCache + ' == ' + this.lang);
 
-        // if (mod.expanded && (!mod.langCache || mod.langCache == this.lang)) {
         if (mod.expanded && (!this.lang || mod.langCache === this.lang)) {
             return mod.expanded;
         }
