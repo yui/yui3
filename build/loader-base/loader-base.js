716d243ad8d824531fd9d97c07541b40f89be8a1
Dav Glass <davglass@gmail.com>
2012-04-18 16:15:25 -0500

    Seed and Loader build after gallery version bump

--- build/loader-base/loader-base.js
+++ build/loader-base/loader-base.js
@@ -13,7 +13,7 @@ if (!YUI.Env[Y.version]) {
             BUILD = '/build/',
             ROOT = VERSION + BUILD,
             CDN_BASE = Y.Env.base,
-            GALLERY_VERSION = 'gallery-2012.04.12-13-50',
+            GALLERY_VERSION = 'gallery-2012.04.18-20-14',
             TNT = '2in3',
             TNT_VERSION = '4',
             YUI2_VERSION = '2.9.0',
