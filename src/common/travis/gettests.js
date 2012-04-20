e648139b0f2a1214d1b33575e653a8f06c12781d
Dav Glass <davglass@gmail.com>
2012-04-20 10:20:42 -0500

    Fixing JSON require in test script for node 0.4.x

--- src/common/travis/gettests.js
+++ src/common/travis/gettests.js
@@ -2,10 +2,12 @@
 
 process.chdir(__dirname);
 
+var fs = require('fs');
+
 var root = process.argv[2],
     path = require('path');
 
-var json = require('./tests.json');
+var json = JSON.parse(fs.readFileSync('./tests.json', 'utf8'));
 
 json.tests.forEach(function(v, k) {
     json.tests[k] = path.join(root, v);
