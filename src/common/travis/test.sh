b77d7289c34b349821a4df5a67ea3664d9fe2e67
Dav Glass <davglass@gmail.com>
2012-04-20 09:19:30 -0500

    Initial Test with IO and Loader tests on Node

--- src/common/travis/test.sh
+++ src/common/travis/test.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-echo "Test: `pwd`"
+root=`pwd`
 
-cd "$(dirname "$0")"
+cd ./build-npm/
 
-echo "cd `pwd`"
+npm_base=`pwd`
+yuitest="${npm_base}/node_modules/.bin/yuitest"
+
+echo "Build Root: ${root}"
+echo "NPM Base: ${npm_base}"
+echo "YUITest: ${yuitest}"
 
 echo "Running Tests.."
+
+cd $root
+wait
+echo "Running IO Tests"
+cd src/io;
+make test
+wait
+cd $root
+echo "Running Loader Tests"
+cd src/loader;
+make test
