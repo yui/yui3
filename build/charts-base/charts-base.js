6123a13ac39668e40e63bc709762d4f85a5b941f
Tripp <tripp.bridges@yahoo.com>
2012-05-04 21:17:29 -0700

    Charts and Graphics build files.

--- build/charts-base/charts-base.js
+++ build/charts-base/charts-base.js
@@ -7356,7 +7356,7 @@ Y.CartesianSeries = Y.Base.create("cartesianSeries", Y.Base, [Y.Renderer], {
             yValue = parseFloat(yData[i]);
             if(isNumber(xValue))
             {
-                nextX = Math.round((((xValue - xMin) * xScaleFactor) + leftPadding + xOffset));
+                nextX = (((xValue - xMin) * xScaleFactor) + leftPadding + xOffset);
             }
             else
             {
@@ -7364,7 +7364,7 @@ Y.CartesianSeries = Y.Base.create("cartesianSeries", Y.Base, [Y.Renderer], {
             }
             if(isNumber(yValue))
             {
-			    nextY = Math.round(((dataHeight + topPadding + yOffset) - (yValue - yMin) * yScaleFactor));
+			    nextY = ((dataHeight + topPadding + yOffset) - (yValue - yMin) * yScaleFactor);
             }
             else
             {
@@ -10242,7 +10242,6 @@ Y.PieSeries = Y.Base.create("pieSeries", Y.MarkerSeries, [], {
             {
                 angle = 360 * (value / totalValue);
             }
-            angle = Math.round(angle);
             if(tfc && tfc.length < 1)
             {
                 tfc = fillColors.concat();
