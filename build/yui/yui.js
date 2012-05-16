b9185e3d2bafe25fb4ca80ffb3f2ca363bf0c118
Dav Glass <davglass@gmail.com>
2012-05-16 15:43:30 -0500

    YUI and Loader build for latest Gallery build

--- build/yui/yui.js
+++ build/yui/yui.js
@@ -5628,7 +5628,7 @@ if (!YUI.Env[Y.version]) {
             BUILD = '/build/',
             ROOT = VERSION + BUILD,
             CDN_BASE = Y.Env.base,
-            GALLERY_VERSION = 'gallery-2012.05.09-20-27',
+            GALLERY_VERSION = 'gallery-2012.05.16-20-37',
             TNT = '2in3',
             TNT_VERSION = '4',
             YUI2_VERSION = '2.9.0',
