0314c3a7d73d6ce6646a70d9f6b5966c1b1dc26c
Dav Glass <davglass@gmail.com>
2012-05-29 15:02:22 -0500

    Starting to work on coverage

--- src/common/travis/before.sh
+++ src/common/travis/before.sh
@@ -10,3 +10,19 @@ cd ../../yui;
 wait
 
 make npm
+
+# FUTURE
+#wait
+#cd ../../
+#wait
+#echo "cd: `pwd`"
+#wait
+#echo "Generating Coverage Build"
+#jscoverage --encoding=utf-8 ./build ./build-coverage
+#wait
+#for file in build-coverage/*; do
+#    echo `basename $file`
+#    name=`basename $file`
+#    cp ./build-coverage/$name/$name.js ./build-coverage/$name/$name-min.js
+#    cp ./build-coverage/$name/$name.js ./build-coverage/$name/$name-debug.js
+#done
