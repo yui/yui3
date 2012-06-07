b4eacc3254e74ed1dff87d6ecb1c29da7cb4eadc
jconniff <jconniff@measurehope-l7.ds.corp.yahoo.com>
2012-06-07 14:20:52 -0700

    fixing #2532099 adding more tests to example

--- src/node/docs/assets/store-tests.js
+++ src/node/docs/assets/store-tests.js
@@ -10,12 +10,36 @@ YUI.add('store-tests', function(Y) {
 
             Assert.areEqual(12, imgs.size(), 'Failed to render images');
         },
-        'speech bubbles should be equal': function() {
+        'select/click 4 items from store, and check see if in cart': function() {
+            var storeList = Y.all('.example #demo li'),
+                cartList;
+            storeList.item(0).simulate('click');
+            storeList.item(3).simulate('click');
+            storeList.item(5).simulate('click');
+            storeList.item(8).simulate('click');
+            cartList = Y.all('.example #demo2 li');
 
-            Assert.areEqual(3, 3, '3 doesnt equal 3');
+            // cartList.item(0) is the cart head
+            Assert.isTrue((cartList.item(1).getHTML().indexOf('tomato') > -1),'failed to add tomato soup');
+            Assert.isTrue((cartList.item(2).getHTML().indexOf('rice') > -1),'failed to add rice-a-roni');
+            Assert.isTrue((cartList.item(3).getHTML().indexOf('banana') > -1),'failed to add banana');
+            Assert.isTrue((cartList.item(4).getHTML().indexOf('peanut') > -1),'failed to add peanut butter');
+        },
+        'remove items from cart': function() {
+            var storeList = Y.all('.example #demo li'),
+                cartList = Y.all('.example #demo2 li');
+
+            cartList.item(1).simulate('click');
+            Assert.isTrue((Y.one('.example #demo2').getHTML().indexOf('tomato') === -1), 'failed to remove tomato soup');
+
+            cartList = Y.all('.example #demo2 li'); // reset the cartList nodeList
+            cartList.item(2).simulate('click');
+            Assert.isTrue((Y.one('.example #demo2').getHTML().indexOf('banana') === -1), 'failed to remove banana');
+            Assert.isTrue((Y.one('.example #demo2').getHTML().indexOf('Rice') > -1), 'rice should still be there');
+            Assert.isTrue((Y.one('.example #demo2').getHTML().indexOf('Laura Scudders') > -1), 'peanut butter should still be there');
         }
     }));
 
     Y.Test.Runner.add(suite);
 
-}, '', { requires: [ 'node' ] });
+}, '', { requires: [ 'node', 'node-event-simulate' ] });
