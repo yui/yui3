5ec05035583624c612312968871d75453dc487d6
Dav Glass <davglass@gmail.com>
2012-07-11 09:55:08 -0500

    Fixed test case

--- src/yql/docs/assets/yql-requery-tests.js
+++ src/yql/docs/assets/yql-requery-tests.js
@@ -6,33 +6,42 @@ YUI.add('yql-requery-tests', function (Y) {
     suite.add(new Y.Test.Case({
         name: 'Example tests',
         'example should have an enclosing div': function () {
-            var enclosure = Y.one('#res');
-
-            Assert.isNotNull(enclosure, 'Enclosure missing');
-            Y.assert(!enclosure.hasChildNodes(), 'Enclosure has initial nodes');
+            var test = this;
+            test.wait(function() {
+                var enclosure = Y.one('#res');
+                Assert.isNotNull(enclosure, 'Enclosure missing');
+            }, 5000);
         },
+        'example should render 10 images': function() {
+            var test = this,
+                count = Y.all('#res img').size();
 
+            Assert.areEqual(count, 10, 'Failed to fetch 10 images');
+        },
         'example should return Flickr pictures after queries': function () {
             var test = this,
-                firstNumber;
-            test.wait(function () {
-                var queryNode   = Y.one('#res h2 em'),
-                    content     = queryNode.getHTML();
-                
-                firstNumber = parseInt(content.match(/\d+\.?\d*/g), 10);
+                reg = /\d+\.?\d*/g,
+                firstNumber,
+                getNumber = function(content) {
+                    return parseInt(content.match(reg), 10);
+                };
 
-                Assert.isNumber(firstNumber, 'Query number not found');
-            }, 10000);
+            var queryNode   = Y.one('#res h2 em'),
+                content     = queryNode.getHTML();
+            
+            firstNumber = getNumber(content);
+            Assert.isNumber(firstNumber, 'Query number not found');
 
             test.wait(function () {
                 var queryNode   = Y.one('#res h2 em'),
                     content     = queryNode.getHTML(),
-                    nextNumber  = parseInt(content.math(/\d+\.?d*/g), 10);
+                    nextNumber  = getNumber(content);
 
                 Assert.isNumber(nextNumber, 'Next number not found');
                 Assert.areNotEqual(firstNumber, nextNumber,
                     'Queries have different query numbers');
             }, 10000);
+
         }
     }));
 
