b58c6e69bb2e770cc30853923ba41ea47a5ff433
Tripp <tripp.bridges@yahoo.com>
2012-07-13 20:06:22 -0700

    Fixed issue with pie example functional test.

--- src/graphics/docs/assets/graphics-simple-tests.js
+++ src/graphics/docs/assets/graphics-simple-tests.js
@@ -66,6 +66,31 @@ YUI.add('graphics-simple-tests', function(Y) {
                 return {
                     color: color
                 }
+            },
+            getDimensions: function(shape)
+            {
+                var w,
+                    h,
+                    node = this._node;
+                switch(shape)
+                {
+                    case "circle" :
+                        w = node.getAttribute("r") * 2;
+                        h = w;
+                    break;
+                    case "ellipse" :
+                        w = parseFloat(node.getAttribute("rx")) * 2;
+                        h = parseFloat(node.getAttribute("ry")) * 2;
+                    break;
+                    default :
+                        w = node.get("width");
+                        h = node.get("height");
+                    break;
+                }
+                return {
+                    width: w,
+                    height: h
+                }
             }
         },
         vml: {
@@ -192,9 +217,12 @@ YUI.add('graphics-simple-tests', function(Y) {
                 strokeWeight = 2,
                 fillOpacity = 1,
                 strokeOpacity = 1,
+                width = 150,
+                height = 100,
                 node = Y.ShapeNode.one(shape),
                 fill = node.getFill(),
-                stroke = node.getStroke();
+                stroke = node.getStroke(),
+                dimensions = node.getDimensions("ellipse");
 
             fillColor = toRGBA(TOHEX(fillColor), fillOpacity);
             strokeColor = toRGBA(TOHEX(strokeColor), strokeOpacity);
@@ -202,6 +230,8 @@ YUI.add('graphics-simple-tests', function(Y) {
             Y.Assert.areEqual(fillColor, fill.color, "The fill color should be " + fillColor + ".");
             Y.Assert.areEqual(strokeColor, stroke.color, "The stroke color should be " + strokeColor + ".");
             Y.Assert.areEqual(strokeWeight, stroke.weight, "The stroke weight should be " + strokeWeight + ".");
+            Y.Assert.areEqual(width, dimensions.width, "The width of the shape should be " + width + ".");
+            Y.Assert.areEqual(height, dimensions.height, "The height of the shape should be " + height + ".");
         }
     }));
 
