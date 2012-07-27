28aea59d7f7c02555268a612a46556489c97f401
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2012-07-27 11:41:12 -0400

    Fixing the newbiest bug ever

--- src/parallel/tests/parallel-tests.js
+++ src/parallel/tests/parallel-tests.js
@@ -35,9 +35,9 @@ YUI.add('parallel-tests', function(Y) {
             counter = 0;
 
             for (var i = 1; i <= 15; i++) {
-                setTimeout(stack.add(function() {
-                    return counter++;
-                }), Math.floor(Math.random() * 100));
+                setTimeout(stack.add((function(index) {
+                    return index;
+                }(i))), Math.floor(Math.random() * 100));
             }
 
             stack.done(function(results) {
