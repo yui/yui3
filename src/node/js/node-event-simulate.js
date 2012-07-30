d6c5e16be1663c6c4119fba5cfd6ea2f58157f93
Matt Sweeney <matt.sweeney@yahoo.com>
2012-07-30 12:56:43 -0700

    remove simulateGesture

--- src/node/js/node-event-simulate.js
+++ src/node/js/node-event-simulate.js
@@ -17,22 +17,3 @@ Y.Node.prototype.simulate = function (type, options) {
     Y.Event.simulate(Y.Node.getDOMNode(this), type, options);
 };
 
-
-/**
- * Simulates a gesture on the node. On desktop, single touch gestures
- * such as tap, flick are simulated by using mouse events. A gesture will 
- * generate a set of underlaying low level events asynchronously.
- * @param {String} type The type of supported gesture to simulate 
- *      (i.e., "tap", "flick").
- * @param {Object} options (Optional) Extra options are used to refine the 
- *      gesture behavior.
- * @param {Function} cb The callback to execute when the gesture simulation 
- *      is completed.  
- * @return {void}
- * @for Node
- * @method simulateGesture
- */
-Y.Node.prototype.simulateGesture = function (type, options, cb) {
-
-    Y.Event.simulateGesture(this, type, options, cb);
-};
