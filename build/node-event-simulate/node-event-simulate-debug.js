6ff3d4c454fee6b34a7562a6bd8e2aaa2b775e0c
Matt Sweeney <matt.sweeney@yahoo.com>
2012-07-30 12:57:36 -0700

    build node-event-simulate

--- build/node-event-simulate/node-event-simulate-debug.js
+++ build/node-event-simulate/node-event-simulate-debug.js
@@ -20,24 +20,5 @@ Y.Node.prototype.simulate = function (type, options) {
 };
 
 
-/**
- * Simulates a gesture on the node. On desktop, single touch gestures
- * such as tap, flick are simulated by using mouse events. A gesture will 
- * generate a set of underlaying low level events asynchronously.
- * @param {String} type The type of supported gesture to simulate 
- *      (i.e., "tap", "flick").
- * @param {Object} options (Optional) Extra options are used to refine the 
- *      gesture behavior.
- * @param {Function} cb The callback to execute when the gesture simulation 
- *      is completed.  
- * @return {void}
- * @for Node
- * @method simulateGesture
- */
-Y.Node.prototype.simulateGesture = function (type, options, cb) {
 
-    Y.Event.simulateGesture(this, type, options, cb);
-};
-
-
-}, '@VERSION@' ,{requires:['node-base', 'event-simulate', 'gesture-simulate']});
+}, '@VERSION@' ,{requires:['node-base', 'event-simulate']});
