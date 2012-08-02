d99896a81bc2409f2ec70566ba6f924238d60eed
Dav Glass <davglass@gmail.com>
2012-08-02 11:20:07 -0500

    Converted Widget Position Tests

--- src/widget-position/tests/unit/assets/widget-position-test.js
+++ src/widget-position/tests/unit/assets/widget-position-test.js
@@ -6,7 +6,7 @@ var Assert      = Y.Assert,
     suite, TestWidget;
 
 // -- Suite --------------------------------------------------------------------
-suite      = new Y.Test.Suite('WidgetPosition');
+suite      = new Y.Test.Suite('Widget: Position');
 TestWidget = Y.Base.create('testWidget', Y.Widget, [Y.WidgetPosition]);
 
 // -- Lifecycle ----------------------------------------------------------------
