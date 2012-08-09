155566670c2c58f5ac4d1d8763f86c1c96f40bc8
Dav Glass <davglass@gmail.com>
2012-08-09 15:28:00 -0500

    Fixed issue with coverage filter and added test for this functionality

--- src/loader/js/loader.js
+++ src/loader/js/loader.js
@@ -823,7 +823,8 @@ Y.Loader.prototype = {
                 Y.Array.each(mods, function(mod) {
                     self.filters[mod] = self.FILTER_DEFS.COVERAGE;
                 });
-                self.filter = 'RAW';
+                self.filterName = 'RAW';
+                self.filter = self.FILTER_DEFS[self.filterName];
             }
         }
         
