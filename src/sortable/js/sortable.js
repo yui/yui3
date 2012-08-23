yui/yui3

commit 26a21b3d3b6e68589d5de1044bdfd970d64e0770

Fixes #2531908 - Removed limitation on dropping with only one item

src/sortable/js/sortable.js
@@ -99,9 +99,7 @@
         _onDropOver: function(e) {
             if (!e.drop.get(NODE).test(this.get(NODES))) {
                 var nodes = e.drop.get(NODE).all(this.get(NODES));
-                if (nodes.size() === 0) {
-                    e.drop.get(NODE).append(e.drag.get(NODE));
-                }
+                e.drop.get(NODE).append(e.drag.get(NODE));
             }
         },
         /**

commit 86e473b5f0e1a9ff2688d397fd61113f6a660dae

Fixes #2531908 - Improve new nodes handling
Commit 26a21b3d3b6e68589d5de1044bdfd970d64e0770 caused dragged items
to be appended to the container while dragging over sortable nodes margins.

Nodes only need to be appended on the "drop:enter" event and if they aren't
already present in the container.

src/sortable/js/sortable.js
@@ -67,7 +67,7 @@
                 bubbleTarget: del,
                 groups: del.dd.get('groups')
             });
-            this.drop.on('drop:over', Y.bind(this._onDropOver, this));
+            this.drop.on('drop:enter', Y.bind(this._onDropEnter, this));
             
             del.on({
                 'drag:start': Y.bind(this._onDragStart, this),
@@ -92,14 +92,17 @@
         },
         /**
         * @private
-        * @method _onDropOver
+        * @method _onDropEnter
         * @param Event e The Event Object
-        * @description Handles the DropOver event to append a drop node to an empty target
+        * @description Handles the DropEnter event to append a new node to a target.
         */
-        _onDropOver: function(e) {
-            if (!e.drop.get(NODE).test(this.get(NODES))) {
-                var nodes = e.drop.get(NODE).all(this.get(NODES));
-                e.drop.get(NODE).append(e.drag.get(NODE));
+        _onDropEnter: function(e) {
+            var dropNode = e.drop.get(NODE),
+                dragNode = e.drag.get(NODE);
+
+            if (!dropNode.test(this.get(NODES)) &&
+                !dragNode.get(PARENT_NODE).compareTo(dropNode)) {
+                dropNode.append(dragNode);
             }
         },
         /**