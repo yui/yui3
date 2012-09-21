c83081e5a2c574afb43a163222bc1d22f20dbc71
Dav Glass <davglass@gmail.com>
2012-09-21 16:05:51 -0500

    Seed and Loader build for #2532773

--- build/loader-base/loader-base-debug.js
+++ build/loader-base/loader-base-debug.js
@@ -2344,7 +2344,8 @@ Y.log('Undefined module: ' + mname + ', matched a pattern: ' +
 
         var complete = function(d) {
             actions++;
-            var errs = {}, i = 0, o = 0, u = '', fn;
+            var errs = {}, i = 0, o = 0, u = '', fn,
+                modName, resMods;
 
             if (d && d.errors) {
                 for (i = 0; i < d.errors.length; i++) {
@@ -2387,6 +2388,19 @@ Y.log('Undefined module: ' + mname + ', matched a pattern: ' +
                     if (mods.length) {
                         Y.log('Refetching modules with new meta-data', 'info', 'loader');
                         self.require(mods);
+                        resMods = self.resolve(true);
+                        if (resMods.cssMods.length) {
+                            for (i=0; i <  resMods.cssMods.length; i++) {
+                                modName = resMods.cssMods[i].name;
+                                delete YUI.Env._cssLoaded[modName];
+                                if (self.isCSSLoaded(modName)) {
+                                    self.inserted[modName] = true;
+                                    delete self.required[modName];
+                                }
+                            }
+                            self.sorted = [];
+                            self._sort();
+                        }
                         d = null; //bail
                         self._insert(); //insert the new deps
                     }
