4d4aaa2eb518f812990b0a6f1c2f12a153ce7e32
Dav Glass <davglass@gmail.com>
2012-09-23 12:49:33 -0500

    Added support for shifter builds on Pull Requests

--- src/common/travis/install.sh
+++ src/common/travis/install.sh
@@ -17,3 +17,26 @@ fi
 
 echo "NPM Install Complete"
 echo ""
+
+if [ -n "$TRAVIS_PULL_REQUEST" ]; then
+    ./src/common/travis/get_pull_files.js
+    if [[ $? != 0 ]] ; then
+        exit 1
+    fi
+    echo "Reinstalling node deps after YUI module build"
+    cd ./build-npm;
+
+    echo "NPM Build Dir: `pwd`"
+    wait
+    echo "Installing NPM Modules"
+    npm install -loglevel silent
+    wait
+    cd  ../
+
+    if [ ! -L ./node_modules ]; then
+        ln -s ./build-npm/node_modules ./
+    fi
+
+    echo "NPM Install Complete"
+    echo ""
+fi
