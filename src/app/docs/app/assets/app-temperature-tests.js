5aa0acf9b8581cebddda14167bc3f1e37028b29c
icebox <albertosantini@gmail.com>
2012-09-25 18:06:24 +0200

    All tests passed.
    Adding the focus to the node and waiting a few milliseconds, just to give
    time to the polling of the valuechange to catch the change, the tests are
    fine.

--- src/app/docs/app/assets/app-temperature-tests.js
+++ src/app/docs/app/assets/app-temperature-tests.js
@@ -17,37 +17,43 @@ suite.add(new Y.Test.Case({
     'Converting Fahrenheit temperature': function () {
         Assert.isNotNull(fahrenheitNode, 'Fahrenheit input field not found.');
 
+        fahrenheitNode.focus();
         fahrenheitNode.set('value', '32');
-        fahrenheitNode.simulate('keypress', {keyCode: 48});
 
-        Assert.areSame(0, parseInt(celsiusNode.get('text'), 10),
-            'Conversion from Fahrenheit to Celsius');
-        Assert.areSame(273.15, parseInt(kelvinNode.get('text'), 10),
-            'Conversion from Fahrenheit to Kelvin');
+        this.wait(function () {
+            Assert.areSame(0, parseFloat(celsiusNode.get('value'), 10),
+                'Conversion from Fahrenheit to Celsius');
+            Assert.areSame(273.15, parseFloat(kelvinNode.get('value'), 10),
+                'Conversion from Fahrenheit to Kelvin');
+        }, 500);
     },
 
     'Converting Celsius temperature': function () {
         Assert.isNotNull(celsiusNode, 'Celsius input field not found.');
 
+        celsiusNode.focus();
         celsiusNode.set('value', '30');
-        celsiusNode.simulate('keypress', {keyCode: 48});
 
-        Assert.areSame(86, parseInt(fahrenheitNode.get('text'), 10),
-            'Conversion from Celsius to Fahrenheit');
-        Assert.areSame(303.15, parseInt(kelvinNode.get('text'), 10),
-            'Conversion from Celsius to Kelvin');
+        this.wait(function () {
+            Assert.areSame(86, parseFloat(fahrenheitNode.get('value'), 10),
+                'Conversion from Celsius to Fahrenheit');
+            Assert.areSame(303.15, parseFloat(kelvinNode.get('value'), 10),
+                'Conversion from Celsius to Kelvin');
+        }, 500);
     },
 
     'Converting Kelvin temperature': function () {
         Assert.isNotNull(kelvinNode, 'Kelvin input field not found.');
 
+        kelvinNode.focus();
         kelvinNode.set('value', '273.15');
-        kelvinNode.simulate('keypress', {keyCode: 48});
 
-        Assert.areSame(32, parseInt(fahrenheitNode.get('text'), 10),
-            'Conversion from Kelvin to Fahrenheit');
-        Assert.areSame(0, parseInt(celsiusNode.get('text'), 10),
-            'Conversion from Kelvin to Celsius');
+        this.wait(function () {
+            Assert.areSame(32, parseFloat(fahrenheitNode.get('value'), 10),
+                'Conversion from Kelvin to Fahrenheit');
+            Assert.areSame(0, parseFloat(celsiusNode.get('value'), 10),
+                'Conversion from Kelvin to Celsius');
+        }, 500);
     }
 
 }));
