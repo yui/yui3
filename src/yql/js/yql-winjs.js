43580faf37ed3a71931ed0be329148cb7e2feb97
Dav Glass <davglass@gmail.com>
2012-10-01 18:37:10 -0500

    Next step with WinJS and YQL

--- src/yql/js/yql-winjs.js
+++ src/yql/js/yql-winjs.js
@@ -6,5 +6,11 @@
 
 //Over writes Y.YQLRequest._send to use IO instead of JSONP
 Y.YQLRequest.prototype._send = function (url, o) {
-    Y.io(url, o);
+    Y.io(url, {
+        on: {
+            complete: function (id, e) {
+                o.on.success(Y.JSON.parse(e.responseText));
+            }
+        }
+    });
 };
