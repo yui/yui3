63f824069176362d8bb485f96587b978169966e0
Dav Glass <davglass@gmail.com>
2012-10-01 18:15:14 -0500

    Initial Commit of yql-winjs module

---
+++ src/yql/js/yql-winjs.js
@@ -0,0 +1,10 @@
+/**
+* WinJS plugin for YQL to use XHR to make requests
+* @module yql
+* @submodule yql-winjs
+*/
+
+//Over writes Y.YQLRequest._send to use IO instead of JSONP
+Y.YQLRequest.prototype._send = function (url, o) {
+    Y.io(url, o);
+};
