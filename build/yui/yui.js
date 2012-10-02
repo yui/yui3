17a8660090e285bd3ac363072da7c71600522c8e
Dav Glass <davglass@gmail.com>
2012-10-02 13:03:43 -0500

    Loader build with last weeks meta-data

--- build/yui/yui.js
+++ build/yui/yui.js
@@ -5686,7 +5686,7 @@ if (!YUI.Env[Y.version]) {
             BUILD = '/build/',
             ROOT = VERSION + BUILD,
             CDN_BASE = Y.Env.base,
-            GALLERY_VERSION = 'gallery-2012.09.19-20-07',
+            GALLERY_VERSION = 'gallery-2012.09.26-20-36',
             TNT = '2in3',
             TNT_VERSION = '4',
             YUI2_VERSION = '2.9.0',
