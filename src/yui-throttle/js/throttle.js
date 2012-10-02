c2d9b5c4aefbefadebc280944ce3513fa8b78efa
Dav Glass <davglass@gmail.com>
2012-10-02 14:54:27 -0500

    Lint fixes

--- src/yui-throttle/js/throttle.js
+++ src/yui-throttle/js/throttle.js
@@ -32,18 +32,18 @@ Y.throttle = function(fn, ms) {
     ms = (ms) ? ms : (Y.config.throttleTime || 150);
 
     if (ms === -1) {
-        return (function() {
+        return function() {
             fn.apply(null, arguments);
-        });
+        };
     }
 
     var last = Y.Lang.now();
 
-    return (function() {
+    return function() {
         var now = Y.Lang.now();
         if (now - last > ms) {
             last = now;
             fn.apply(null, arguments);
         }
-    });
+    };
 };
