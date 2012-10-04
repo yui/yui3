ae44e5bf373207be79e89a534aa7fad9820c60a7
Ashik Samad <ashiksmd@gmail.com>
2012-10-04 15:41:27 +0530

    Fixed timezone license info

--- build/timezone/timezone.js
+++ build/timezone/timezone.js
@@ -3135,21 +3135,6 @@ TimezoneLinks = {
     "America/St_Barthelemy": "America/Guadeloupe",
     "America/Marigot": "America/Guadeloupe"
 };
-/*
- * ***** BEGIN LICENSE BLOCK *****
- * Zimbra Collaboration Suite Web Client
- * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
- * 
- * The contents of this file are subject to the Zimbra Public License
- * Version 1.3 ("License"); you may not use this file except in
- * compliance with the License.  You may obtain a copy of the License at
- * http://www.zimbra.com/license.
- * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
- * ***** END LICENSE BLOCK *****
- */
-
 /**
  * Y.TimeZone performs operations on a given timezone string represented in Olson tz database 
  * This module uses parts of zimbra AjxTimezone to handle time-zones
