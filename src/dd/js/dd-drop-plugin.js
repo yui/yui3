383b3fde98f4fc47979c41c882e2bdbc0cea8d74
Anthony Pipkin <a.pipkin@yahoo.com>
2012-10-08 16:54:04 -0400

    Fix #2532824. Scrub for Y.each and @description. Minor performance updates with Y.Object.keys and Y.Array.hash.

--- src/dd/js/dd-drop-plugin.js
+++ src/dd/js/dd-drop-plugin.js
@@ -19,14 +19,14 @@
         };
 
         /**
+        * dd-drop-plugin
         * @property NAME
-        * @description dd-drop-plugin
         * @type {String}
         */
         Drop.NAME = "dd-drop-plugin";
         /**
+        * The Drop instance will be placed on the Node instance under the drop namespace. It can be accessed via Node.drop;
         * @property NS
-        * @description The Drop instance will be placed on the Node instance under the drop namespace. It can be accessed via Node.drop;
         * @type {String}
         */
         Drop.NS = "drop";
