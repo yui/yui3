a32949a275182ee25e555c356da059d156104d0c
Dav Glass <davglass@gmail.com>
2012-10-09 11:58:46 -0500

    Seed and Loader build with new Docs and new MD5 from script fix

--- build/yui-nodejs/yui-nodejs.js
+++ build/yui-nodejs/yui-nodejs.js
@@ -5383,7 +5383,8 @@ Y.Loader.prototype = {
     /**
     * Reset modules in the module cache to a pre-processed state so additional
     * computations with a different skin or language will work as expected.
-    * @private _resetModules
+    * @method _resetModules
+    * @private
     */
     _resetModules: function() {
         var self = this, i, o,
@@ -8280,6 +8281,7 @@ YUI.Env[Y.version].modules = YUI.Env[Y.version].modules || {
         "requires": [
             "datasource-local",
             "plugin",
+            "datatype-xml",
             "dataschema-xml"
         ]
     },
@@ -10099,7 +10101,7 @@ YUI.Env[Y.version].modules = YUI.Env[Y.version].modules || {
         ]
     }
 };
-YUI.Env[Y.version].md5 = '2631b5fb2c08064b4e8385f1142513e5';
+YUI.Env[Y.version].md5 = 'a28e022ad022130f7a4fb4ac77a2f1df';
 
 
 }, '@VERSION@', {"requires": ["loader-base"]});
