dafb59be157600beeadd50c935e4b1b7443edd37
Julien Sanchez <ju.sanchez9@gmail.com>
2012-10-16 22:33:10 +0200

    add parameter 'nesting' for scrollview

--- src/scrollview/js/scrollview-base.js
+++ src/scrollview/js/scrollview-base.js
@@ -45,6 +45,8 @@ var getClassName = Y.ClassNameManager.getClassName,
     EASING = 'easing', 
     FRAME_DURATION = 'frameDuration', 
     BOUNCE_RANGE = 'bounceRange',
+    NESTING = 'nesting',
+
     
     _constrain = function (val, min, max) {
         return Math.min(Math.max(val, min), max);
@@ -142,6 +144,7 @@ Y.ScrollView = Y.extend(ScrollView, Y.Widget, {
         sv._cBounceRange = sv.get(BOUNCE_RANGE);
         sv._cDeceleration = sv.get(DECELERATION);
         sv._cFrameDuration = sv.get(FRAME_DURATION);
+        sv._cNesting = sv.get(NESTING);
     },
 
     /**
@@ -599,7 +602,9 @@ Y.ScrollView = Y.extend(ScrollView, Y.Widget, {
         }
 
         // TODO: Review if neccesary (#2530129)
-        e.stopPropagation();
+		if (this._cNesting){
+        	e.stopPropagation();
+		}
 
         // Reset lastScrolledAmt
         sv.lastScrolledAmt = 0;
@@ -1293,6 +1298,17 @@ Y.ScrollView = Y.extend(ScrollView, Y.Widget, {
          */
         bounceRange: {
             value: 150
+        },
+
+        /**
+         * Prevent scrollview from nesting problems
+         *
+         * @attribute nesting
+         * @type Boolean
+         * @default true
+         */
+        nesting: {
+            value: true
         }
     },
 
