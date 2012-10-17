a16288bd25bebd3f3c3ff431077ce4fee4cd3fb1
Jeroen Versteeg <jeroen@asystance.nl>
2012-10-17 21:58:07 +0200

    Added Dutch translation to calendar module

---
+++ src/calendar/lang/calendar-base_nl.js
@@ -0,0 +1,7 @@
+{
+    weekdays : ["Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"],
+    short_weekdays : ["Zon", "Maan", "Dins", "Woens", "Dond", "Vrij", "Zat"],
+    very_short_weekdays : ["Zo", "Ma", "Di", "Woe", "Do", "Vr", "Za"],
+    first_weekday : 1,
+    weekends : [0,6]
+}
---
+++ src/calendar/lang/calendar_nl.js
@@ -0,0 +1,5 @@
+{
+    weekdays : ["Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"],
+    short_weekdays : ["Zond", "Maan", "Dinsd", "Woensd", "Dond", "Vrij", "Zat"],
+    very_short_weekdays : ["Zo", "Ma", "Di", "Woe", "Do", "Vr", "Za"]
+}
