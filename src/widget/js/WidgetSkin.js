816b873149bbdf23b012f0fb9e7f7795ca5007f7
Arno V <versini@gmail.com>
2012-10-28 16:34:39 -0700

    updated Widget.getSkinName to allow multiple skin prefixes

--- src/widget/js/WidgetSkin.js
+++ src/widget/js/WidgetSkin.js
@@ -8,23 +8,28 @@
 var BOUNDING_BOX = "boundingBox",
     CONTENT_BOX = "contentBox",
     SKIN = "skin",
+    CLASS_NAME_DELIMITER = 'classNameDelimiter',
+    CONFIG = Y.config,
     _getClassName = Y.ClassNameManager.getClassName;
 
 /**
  * Returns the name of the skin that's currently applied to the widget.
  * This is only really useful after the widget's DOM structure is in the
  * document, either by render or by progressive enhancement.  Searches up
- * the Widget's ancestor axis for a class yui3-skin-(name), and returns the
+ * the Widget's ancestor axis for a class yui3-skin-(name) or
+ * prefix-skin-(name) if prefix is supplied, and returns the
  * (name) portion.  Otherwise, returns null.
  *
  * @method getSkinName
  * @for Widget
+ * @param {String} prefix The prefix of the skin to look for (default is yui3)
  * @return {String} the name of the skin, or null (yui3-skin-sam => sam)
  */
 
-Y.Widget.prototype.getSkinName = function () {
+Y.Widget.prototype.getSkinName = function (prefix) {
     var root = this.get( CONTENT_BOX ) || this.get( BOUNDING_BOX ),
-        search = new RegExp( '\\b' + _getClassName( SKIN ) + '-(\\S+)' ),
+        className = ( prefix )? _getClassName( prefix + CONFIG[CLASS_NAME_DELIMITER] + SKIN, true ) : _getClassName( SKIN ),
+        search = new RegExp( '\\b' + className + '-(\\S+)' ),
         match;
 
     if ( root ) {
