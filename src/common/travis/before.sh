c8575e6789cdfa464b8e76ffe66250d6df4b24a7
Dav Glass <davglass@gmail.com>
2012-11-08 10:41:56 -0600

    Make shifter --walk also use --istanbul

--- src/common/travis/before.sh
+++ src/common/travis/before.sh
@@ -16,7 +16,7 @@ if [ -n "$TRAVIS" ]; then
     npm -g install yogi -loglevel silent
     cd ../
     echo "building entire library";
-    shifter --walk --no-lint --no-coverage --cache
+    shifter --walk --no-lint --istanbul
     cd yui;
 else 
     make npm
