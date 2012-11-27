6ae17335049232c521a5703246909b280fd580c1
Arno V <versini@gmail.com>
2012-11-27 14:41:03 -0800

    Removing dependency on classmanager and adding custom delimiter option

--- src/widget/js/WidgetSkin.js
+++ src/widget/js/WidgetSkin.js
@@ -8,29 +8,32 @@
 var BOUNDING_BOX = "boundingBox",
     CONTENT_BOX = "contentBox",
     SKIN = "skin",
+    CLASS_NAME_PREFIX = 'classNamePrefix',
     CLASS_NAME_DELIMITER = 'classNameDelimiter',
-    CONFIG = Y.config,
-    _getClassName = Y.ClassNameManager.getClassName;
+    CONFIG = Y.config;
 
 /**
  * Returns the name of the skin that's currently applied to the widget.
  * This is only really useful after the widget's DOM structure is in the
- * document, either by render or by progressive enhancement.  Searches up
- * the Widget's ancestor axis for a class yui3-skin-(name) or
- * prefix-skin-(name) if prefix is supplied, and returns the
- * (name) portion.  Otherwise, returns null.
+ * document, either by render or by progressive enhancement.
+ *
+ * Searches up the Widget's ancestor axis for a class [prefix][delim]skin[delim]name
+ * and returns the <name> portion.  Otherwise, returns null.
  *
  * @method getSkinName
  * @for Widget
  * @param {String} prefix The prefix of the skin to look for (default is yui3)
+ * @param {String} delim  The delimiter of the skin to look for (default is -)
  * @return {String} the name of the skin, or null (yui3-skin-sam => sam)
  */
 
-Y.Widget.prototype.getSkinName = function (prefix) {
+Y.Widget.prototype.getSkinName = function (prefix, delimiter) {
     var root = this.get( CONTENT_BOX ) || this.get( BOUNDING_BOX ),
-        className = ( prefix )? _getClassName( prefix + CONFIG[CLASS_NAME_DELIMITER] + SKIN, true ) : _getClassName( SKIN ),
-        search = new RegExp( '\\b' + className + '-(\\S+)' ),
-        match;
+      sPrefix = prefix || CONFIG[CLASS_NAME_PREFIX],
+      sDelimiter = delimiter || CONFIG[CLASS_NAME_DELIMITER],
+      className = sPrefix + sDelimiter + SKIN + sDelimiter,
+      search = new RegExp( '\\b' + className + '(\\S+)' ),
+      match;
 
     if ( root ) {
         root.ancestor( function ( node ) {
