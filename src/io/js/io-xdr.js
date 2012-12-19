170e516a277ed3c89be345b6aa7534f54947ec43
Eric Ferraiuolo <eferraiuolo@gmail.com>
2012-12-19 18:58:30 -0500

    Add code comments to io-xdr.

--- src/io/js/io-xdr.js
+++ src/io/js/io-xdr.js
@@ -6,6 +6,10 @@ cross-domain requests.
 @for IO
 **/
 
+// Helpful resources when working with the mess that is XDomainRequest:
+// http://www.cypressnorth.com/blog/web-programming-and-development/internet-explorer-aborting-ajax-requests-fixed/
+// http://blogs.msdn.com/b/ieinternals/archive/2010/05/13/xdomainrequest-restrictions-limitations-and-workarounds.aspx
+
 /**
 Fires when the XDR transport is ready for use.
 @event io:xdrReady
@@ -182,6 +186,8 @@ Y.mix(Y.IO.prototype, {
         else if (xdr) {
             io._ieEvt(o, c);
             o.c.open(c.method || 'GET', uri);
+
+            // Make async to protect against IE 8 oddities.
             setTimeout(function() {
                 o.c.send(c.data);
             }, 0);
