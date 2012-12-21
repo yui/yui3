4c1980b6a0c5a9bb291b2ff1b8bd7038a73a6c1e
Anthony Pipkin <a.pipkin@yahoo.com>
2012-12-21 12:34:46 -0500

    Performance tests for DataTable.

--- src/datatable/tests/benchmark/datatable-benchmark.js
+++ src/datatable/tests/benchmark/datatable-benchmark.js
@@ -1,28 +1,73 @@
-
 YUI.add('datatable-benchmark', function (Y) {
+
+    var dataA = [];
+
+    for (var i=0; i<1000; i++) {
+        dataA.push({ id: 'ga-3475' + i, name: 'gadget', price: '$6.99'});
+    }
 
     var suite = Y.BenchmarkSuite = new Benchmark.Suite();
 
-    // -- Y.Datatable ------------------------------------------------------------------
-    suite.add('Y.Datatable: Instantiate a bare datatable', function () {
-        var dt = new Y.Datatable();
+
+    suite.add('Y.DataTable: Instantiate a bare data table >>', function () {
+        var dt = new Y.DataTable();
     });
 
-    /**
-    suite.add('Y.Model: Subclass and instantiate a bare model', function () {
-        var MyModel = Y.Base.create('myModel', Y.Model, []),
-            model   = new MyModel();
+    suite.add('Y.DataTable: Instantiate a bare data table rendered >>', function () {
+        var dt = new Y.DataTable();
+        dt.render();
+        dt.destroy();
     });
 
-    // -- Y.View -------------------------------------------------------------------
-    suite.add('Y.View: Instantiate a bare view', function () {
-        var view = new Y.View();
+    // 1 x 3
+    suite.add('Y.DataTable: Instantiate with 1 x 3 data >>', function() {
+        var dt = new Y.DataTable({
+            columns: ['id', 'name', 'price'],
+            data: [{ id: 'ga-3475', name: 'gadget', price: '$6.99'}]
+        });
     });
 
-    suite.add('Y.View: Instantiate and subclass a bare view', function () {
-        var MyView = Y.Base.create('myView', Y.View, []),
-            view   = new MyView();
+    suite.add('Y.DataTable: Instantiate with 1 x 3 data rendered >>', function() {
+        var dt = new Y.DataTable({
+            columns: ['id', 'name', 'price'],
+            data: [{ id: 'wi-0650', name: 'widget',   price: '$4.25'}]
+        });
+        dt.render();
     });
-    //*/
+
+    suite.add('Y.DataTable: Instantiate with 1 x 3 data rendered and destroyed >>', function() {
+        var dt = new Y.DataTable({
+            columns: ['id', 'name', 'price'],
+            data: [{ id: 'wi-0650', name: 'widget',   price: '$4.25'}]
+        });
+        dt.render();
+        dt.destroy();
+    });
+
+    // 1000 x 3
+    suite.add('Y.DataTable: Instantiate with 1000 x 3 data >>', function() {
+        var dt = new Y.DataTable({
+            columns: ['id', 'name', 'price'],
+            data: dataA
+        });
+    });
+
+    suite.add('Y.DataTable: Instantiate with 1000 x 3 data rendered >>', function() {
+        var dt = new Y.DataTable({
+            columns: ['id', 'name', 'price'],
+            data: dataA
+        });
+        dt.render();
+    });
+
+    suite.add('Y.DataTable: Instantiate with 1000 x 3 data rendered and destroyed >>', function() {
+        var dt = new Y.DataTable({
+            columns: ['id', 'name', 'price'],
+            data: dataA
+        });
+        dt.render();
+        dt.destroy();
+    });
+
 
 }, '@VERSION@', {requires: ['datatable']});
