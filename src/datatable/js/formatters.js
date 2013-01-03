5a12cfcee59f3d1fd3a7769ad33b494ef66b147e
Daniel <satyam@satyam.com.ar>
2013-01-03 22:42:59 +0100

    Added the datatable-formatters module

---
+++ src/datatable/js/formatters.js
@@ -0,0 +1,400 @@
+/**
+Adds predefined cell formatters to `Y.DataTable` (or other built class).
+
+@module datatable
+@submodule datatable-formatters
+@since 3.8.0
+**/
+var Lang = Y.Lang,
+isValue = Lang.isValue,
+subs = Lang.sub,
+escape = Y.Escape.html,
+
+getCName = Y.ClassNameManager.getClassName,
+cName = function (name) {
+    return getCName('datatable', name);
+},
+stringValue = function (value, def) {
+    return (isValue(value) ? escape(value.toString()) : def || '');
+};
+/**
+ Registry of cell formatting functions, enables names to be used in Column
+ definition formatter value (i.e., {key:"myColumn", formatter:"date"}).
+@class DataTable.Formatters
+@since 3.8.0
+**/
+Y.namespace('DataTable').Formatters = {
+
+    /**
+    Formats a BUTTON element using the value as the label.
+
+    Applies the CSS className `button` to the cell.
+
+    @method Formatters.button
+    @param o {Object} As provided by [BodyView](DataTable.BodyView.html)
+    @param o.value {any} The raw value from the record Model to populate this cell.
+         Equivalent to `o.record.get(o.column.key)` or `o.data[o.column.key]`.
+    @param o.data {Object} The Model data for this row in simple object format.
+    @param o.column {Object} The column configuration object.
+    @param o.record {Y.Model} The Model for this row.
+    @param o.className {String} A string of class names to add `<td class="HERE">`
+           in addition to the column class and any classes in the column's className configuration.
+    @param o.rowIndex {Number} The index of the current Model in the ModelList.
+           Typically correlates to the row index as well.
+    @param o.rowClass {String} A string of css classes to add `<tr class="HERE"><td....`
+    @return {String} the markup for the button.
+    @static
+    **/
+    button : function(o) {
+        o.className = cName('button');
+        return '<button>' + stringValue(o.value, 'Click') + '</button>';
+    },
+
+    /**
+    Formats a CHECKBOX element based on the <i>truthy</i> value of the cell.
+
+    Applies the CSS className `checkbox` to the cell.
+
+    @method Formatters.checkbox
+    @param o {Object} As provided by [BodyView](DataTable.BodyView.html)
+    @param o.value {any} The raw value from the record Model to populate this cell.
+         Equivalent to `o.record.get(o.column.key)` or `o.data[o.column.key]`.
+    @param o.data {Object} The Model data for this row in simple object format.
+    @param o.column {Object} The column configuration object.
+    @param o.record {Y.Model} The Model for this row.
+    @param o.className {String} A string of class names to add `<td class="HERE">`
+           in addition to the column class and any classes in the column's className configuration.
+    @param o.rowIndex {Number} The index of the current Model in the ModelList.
+           Typically correlates to the row index as well.
+    @param o.rowClass {String} A string of css classes to add `<tr class="HERE"><td....`
+    @return {String} the markup for the checkbox.
+    @static
+    **/
+    checkbox : function(o) {
+        o.className = cName('checkbox');
+        return '<input type="checkbox"' + (o.value ? ' checked="checked"' : '')  +'/>';
+    },
+
+    /**
+    Formats values as currency using the `Y.Number.format` method.
+    It looks for the format to apply in the `currencyFormat` property of the column
+    or in the `currencyFormat` attribute of the whole table.
+
+        {key: "amount", formatter: "currency", currencyFormat: {
+            decimalPlaces:2,
+            decimalSeparator: ",",
+            thousandsSeparator: ",",
+            suffix: "&euro;"
+         }}
+
+    Applies the CSS className `currency` to the cell.
+    @method Formatters.currency
+    @param o {Object} As provided by [BodyView](DataTable.BodyView.html)
+    @param o.value {any} The raw value from the record Model to populate this cell.
+         Equivalent to `o.record.get(o.column.key)` or `o.data[o.column.key]`.
+    @param o.data {Object} The Model data for this row in simple object format.
+    @param o.column {Object} The column configuration object.
+    @param o.record {Y.Model} The Model for this row.
+    @param o.className {String} A string of class names to add `<td class="HERE">`
+           in addition to the column class and any classes in the column's className configuration.
+    @param o.rowIndex {Number} The index of the current Model in the ModelList.
+           Typically correlates to the row index as well.
+    @param o.rowClass {String} A string of css classes to add `<tr class="HERE"><td....`
+    @return {String} the formatted value.
+    @static
+    **/
+    currency : function(o) {
+        o.className = cName('currency');
+        return Y.Number.format(o.value, o.column.currencyFormat || this.get("currencyFormat"));
+    },
+
+    /**
+    Formats JavaScript Dates.
+    It looks for the format to apply in the `dateFormat` property of the column
+    or in the `dateFormat` attribute of the whole table.
+
+        {key: "DOB", formatter: "date", dateFormat: "%I:%M:%S %p"}
+
+    Applies the CSS className `date` to the cell.
+    @method Formatters.date
+    @param o {Object} As provided by [BodyView](DataTable.BodyView.html)
+    @param o.value {any} The raw value from the record Model to populate this cell.
+         Equivalent to `o.record.get(o.column.key)` or `o.data[o.column.key]`.
+    @param o.data {Object} The Model data for this row in simple object format.
+    @param o.column {Object} The column configuration object.
+    @param o.record {Y.Model} The Model for this row.
+    @param o.className {String} A string of class names to add `<td class="HERE">`
+           in addition to the column class and any classes in the column's className configuration.
+    @param o.rowIndex {Number} The index of the current Model in the ModelList.
+           Typically correlates to the row index as well.
+    @param o.rowClass {String} A string of css classes to add `<tr class="HERE"><td....`
+    @return {String} the formatted date.
+    @static
+    **/
+    'date' : function(o) {
+        o.className = cName('date');
+        return Y.Date.format(o.value, {
+            format: o.column.dateFormat || this.get('dateFormat')
+        });
+    },
+
+    /**
+    Formats SELECT elements.
+
+    It looks for the options to offer in the `dropdownOptions` property of the column.
+    The `dropdownOptions` must be an array either of plain values,
+    which will be used as both the value and the label or objects with
+    `value` and `label` properties.
+
+        {key: "color", formatter: "dropdown", dropdownOptions: [
+             {value:"", label:"-none selected"},
+             "blue",
+             "red",
+             "green"
+        ]}
+
+
+    Applies the CSS className `dropdown` to the cell.
+
+    @method Formatters.dropdown
+    @param o {Object} As provided by [BodyView](DataTable.BodyView.html)
+    @param o.value {any} The raw value from the record Model to populate this cell.
+         Equivalent to `o.record.get(o.column.key)` or `o.data[o.column.key]`.
+    @param o.data {Object} The Model data for this row in simple object format.
+    @param o.column {Object} The column configuration object.
+    @param o.record {Y.Model} The Model for this row.
+    @param o.className {String} A string of class names to add `<td class="HERE">`
+           in addition to the column class and any classes in the column's className configuration.
+    @param o.rowIndex {Number} The index of the current Model in the ModelList.
+           Typically correlates to the row index as well.
+    @param o.rowClass {String} A string of css classes to add `<tr class="HERE"><td....`
+    @return {String} the markup for the dropdown box.
+    **/
+    dropdown : function(o) {
+        var s = '<select>',
+        options = o.column.dropdownOptions || [],
+        template = '<option {selected} value="{value}">{label}</option>';
+
+        o.className = cName('dropdown');
+        Y.Array.each(options, function (option) {
+            if (Lang.isObject(option)) {
+                o.selected = (option.value === o.value)?'selected':'';
+                s.push(subs(template, option));
+            } else {
+                s.push(subs(template, {
+                    value:option,
+                    label:option,
+                    selected: (option === o.value)?'selected':''
+                }));
+            }
+        });
+        return s.join('') + '</select>';
+    },
+
+    /**
+    Formats emails links.
+    If the column definition contains a property `linkFrom` it will use the value
+    in that column for the link, otherwise, the same column value will be used for both
+    link and text.
+
+        {key: "contact", formatter: "email", linkFrom: "contactEmail"}
+
+    Applies the CSS className `email` to the cell.
+    @method Formatters.email
+    @param o {Object} As provided by [BodyView](DataTable.BodyView.html)
+    @param o.value {any} The raw value from the record Model to populate this cell.
+         Equivalent to `o.record.get(o.column.key)` or `o.data[o.column.key]`.
+    @param o.data {Object} The Model data for this row in simple object format.
+    @param o.column {Object} The column configuration object.
+    @param o.record {Y.Model} The Model for this row.
+    @param o.className {String} A string of class names to add `<td class="HERE">`
+           in addition to the column class and any classes in the column's className configuration.
+    @param o.rowIndex {Number} The index of the current Model in the ModelList.
+           Typically correlates to the row index as well.
+    @param o.rowClass {String} A string of css classes to add `<tr class="HERE"><td....`
+    @return {String} the markup for the address.
+    @static
+    **/
+    email : function(o) {
+        o.className = cName('email');
+        var value = escape(o.value),
+        link = (o.column.linkFrom?escape(o.data[o.column.linkFrom]):value);
+        return '<a href="mailto:' + link + '">' + value + '</a>';
+    },
+
+    /**
+    Formats links.
+    If the column definition contains a property `linkFrom` it will use the value
+    in that column for the link, otherwise, the same column value will be used for both
+    link and text.
+
+        {key: "companyName", formatter: "link", linkFrom: "CompanyWebSite"}
+
+    Applies the CSS className `link` to the cell.
+    @method Formatters.link
+    @param o {Object} As provided by [BodyView](DataTable.BodyView.html)
+    @param o.value {any} The raw value from the record Model to populate this cell.
+         Equivalent to `o.record.get(o.column.key)` or `o.data[o.column.key]`.
+    @param o.data {Object} The Model data for this row in simple object format.
+    @param o.column {Object} The column configuration object.
+    @param o.record {Y.Model} The Model for this row.
+    @param o.className {String} A string of class names to add `<td class="HERE">`
+           in addition to the column class and any classes in the column's className configuration.
+    @param o.rowIndex {Number} The index of the current Model in the ModelList.
+           Typically correlates to the row index as well.
+    @param o.rowClass {String} A string of css classes to add `<tr class="HERE"><td....`
+    @return {String} the markup for the address.
+    @static
+    **/
+    link : function(o) {
+        o.className = cName('link');
+        var value = escape(o.value),
+        link = (o.column.linkFrom?escape(o.data[o.column.linkFrom]):value);
+        return '<a href="' + link + '">' + value + '</a>';
+    },
+
+    /**
+    Formats values as a number using the `Y.Number.format` method.
+    It looks for the format to apply in the `numberFormat` property of the column
+    or in the `numberFormat` attribute of the whole table.
+
+        {key: "weight", formatter: "number", numberFormat: {
+            decimalPlaces:2,
+            decimalSeparator: ",",
+            thousandsSeparator: ",",
+            suffix: "kg"
+        }}
+
+    Applies the CSS className `number` to the cell.
+    @method Formatters.number
+    @param o {Object} As provided by [BodyView](DataTable.BodyView.html)
+    @param o.value {any} The raw value from the record Model to populate this cell.
+         Equivalent to `o.record.get(o.column.key)` or `o.data[o.column.key]`.
+    @param o.data {Object} The Model data for this row in simple object format.
+    @param o.column {Object} The column configuration object.
+    @param o.record {Y.Model} The Model for this row.
+    @param o.className {String} A string of class names to add `<td class="HERE">`
+           in addition to the column class and any classes in the column's className configuration.
+    @param o.rowIndex {Number} The index of the current Model in the ModelList.
+           Typically correlates to the row index as well.
+    @param o.rowClass {String} A string of css classes to add `<tr class="HERE"><td....`
+    @return {String} the formatted value.
+    @static
+    **/
+    'number' : function(o) {
+        o.className = cName('number');
+        return Y.Number.format(o.value, o.column.numberFormat || this.get("numberFormat"));
+    },
+
+    /**
+    Formats a CHECKBOX element based on the <i>truthy</i> value of the cell.
+    All the radios on the same column share a unique name so they are mutually exclusive
+
+    Applies the CSS className `radio` to the cell.
+    @method Formatters.radio
+    @param o {Object} As provided by [BodyView](DataTable.BodyView.html)
+    @param o.value {any} The raw value from the record Model to populate this cell.
+         Equivalent to `o.record.get(o.column.key)` or `o.data[o.column.key]`.
+    @param o.data {Object} The Model data for this row in simple object format.
+    @param o.column {Object} The column configuration object.
+    @param o.record {Y.Model} The Model for this row.
+    @param o.className {String} A string of class names to add `<td class="HERE">`
+           in addition to the column class and any classes in the column's className configuration.
+    @param o.rowIndex {Number} The index of the current Model in the ModelList.
+           Typically correlates to the row index as well.
+    @param o.rowClass {String} A string of css classes to add `<tr class="HERE"><td....`
+    @return {String} the markup for the radio.
+    @static
+    **/
+    radio : function(o) {
+        o.className = cName('radio');
+        return '<input type="radio"' + (o.value ? ' checked="checked"' : '') +
+        ' name="'+ this.get('boundingBox').get('id') + o.column.key + '" />';
+    },
+
+    /**
+    Formats text strings.
+    It escapes HTML reserved values and turns nulls and undefineds into a single blank.
+
+    Applies the CSS className `text` to the cell.
+    @method Formatters.text
+    @param o {Object} As provided by [BodyView](DataTable.BodyView.html)
+    @param o.value {any} The raw value from the record Model to populate this cell.
+         Equivalent to `o.record.get(o.column.key)` or `o.data[o.column.key]`.
+    @param o.data {Object} The Model data for this row in simple object format.
+    @param o.column {Object} The column configuration object.
+    @param o.record {Y.Model} The Model for this row.
+    @param o.className {String} A string of class names to add `<td class="HERE">`
+           in addition to the column class and any classes in the column's className configuration.
+    @param o.rowIndex {Number} The index of the current Model in the ModelList.
+           Typically correlates to the row index as well.
+    @param o.rowClass {String} A string of css classes to add `<tr class="HERE"><td....`
+    @return {String} the escaped string
+    @static
+    **/
+    text : function(o) {
+        o.className = cName('text');
+        return stringValue(o.value);
+    },
+
+    /**
+    It produces a TEXTAREA filled with the escaped value of the cell.
+
+    Applies the CSS className `textarea` to the cell.
+    @method Formatters.textarea
+    @param o {Object} As provided by [BodyView](DataTable.BodyView.html)
+    @param o.value {any} The raw value from the record Model to populate this cell.
+         Equivalent to `o.record.get(o.column.key)` or `o.data[o.column.key]`.
+    @param o.data {Object} The Model data for this row in simple object format.
+    @param o.column {Object} The column configuration object.
+    @param o.record {Y.Model} The Model for this row.
+    @param o.className {String} A string of class names to add `<td class="HERE">`
+           in addition to the column class and any classes in the column's className configuration.
+    @param o.rowIndex {Number} The index of the current Model in the ModelList.
+           Typically correlates to the row index as well.
+    @param o.rowClass {String} A string of css classes to add `<tr class="HERE"><td....`    @return {String} the markup for the textarea
+    @static
+    **/
+    textarea : function(o) {
+        o.className = cName('textarea');
+        return '<textarea>' + stringValue(o.value) + '</textarea>';
+    },
+
+    /**
+    It produces a TEXTBOX filled with the escaped value of the cell.
+
+    Applies the CSS className `textbox` to the cell.
+    @method Formatters.textbox
+    @param o {Object} As provided by [BodyView](DataTable.BodyView.html)
+    @param o.value {any} The raw value from the record Model to populate this cell.
+         Equivalent to `o.record.get(o.column.key)` or `o.data[o.column.key]`.
+    @param o.data {Object} The Model data for this row in simple object format.
+    @param o.column {Object} The column configuration object.
+    @param o.record {Y.Model} The Model for this row.
+    @param o.className {String} A string of class names to add `<td class="HERE">`
+           in addition to the column class and any classes in the column's className configuration.
+    @param o.rowIndex {Number} The index of the current Model in the ModelList.
+           Typically correlates to the row index as well.
+    @param o.rowClass {String} A string of css classes to add `<tr class="HERE"><td....`
+    @return {String} the markup for the textbox
+    @static
+    **/
+    textbox : function(o) {
+        o.className = cName('text');
+        return '<input type="text" value="' + stringValue(o.value) + '"/>';
+    }
+
+};
+
+if (Lang.isFunction(Y.DataTable)) {
+    Y.Base.mix(Y.DataTable, [Formatters]);
+}
+/**
+Hash of formatting functions for cell contents.
+See: [DataTable.Formatters](./DataTable.Formatters.html)
+@property Formatters
+@for DataTable
+@type Object
+@since 3.8.0
+@static
+**/
