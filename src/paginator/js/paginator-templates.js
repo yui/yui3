c2344203627e6afb8075ae0983e04fe2fd14d93e
Anthony Pipkin <a.pipkin@yahoo.com>
2013-01-13 13:01:12 -0500

    Add simple template. Add classnames and binding.

--- src/paginator/js/paginator-templates.js
+++ src/paginator/js/paginator-templates.js
@@ -1,3 +1,9 @@
-var list = {
-    control: '<a href="<%= controlLink %>" title="<%= controlTitle %>"><%= controlText =></a>'
-}
+var control = '<a href="<%= link %>" title="<%= title %>" class="<%= classname %>"><%= label =></a>',
+    page = '<a href="<%= link %>" title="<%= title %>" class="<%= classname %>"><%= label =></a>',
+    list = '<ul class="<%= classname %>"><%= first %><%= prev %><ul class="<%= pagesClass %>"><%= pages %></ul><%= next %><%= last %=></ul>';
+
+Y.Paginator.Templates = {
+    control: control
+    page: page
+    list: list
+};
