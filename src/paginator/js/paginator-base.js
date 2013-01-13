0555ab7790ba38b62de1b54855adc058c2ded8ae
Anthony Pipkin <a.pipkin@yahoo.com>
2013-01-13 13:01:12 -0500

    Add simple template. Add classnames and binding.

--- src/paginator/js/paginator-base.js
+++ src/paginator/js/paginator-base.js
@@ -1,13 +1,12 @@
 var Paginator = Y.Base.create('paginator', Y.View, [Y.Paginator.Core, Y.Paginator.Url], {
 
+    prefix: null,
+
     template: '{firstTitle} {page} of {pages}',
 
     initializer: function () {
-        var subscriptions = [];
-
-        subscriptions.push(this.on('pageChange', this._pageChange));
-
-        this._subs = subscriptions;
+        this.generateClassNames();
+        this.bind();
     },
 
     destructor: function () {
@@ -35,6 +34,69 @@ var Paginator = Y.Base.create('paginator', Y.View, [Y.Paginator.Core, Y.Paginato
         return this;
     },
 
+    bind: function () {
+        var subscriptions = [],
+            classNames = this.classNames,
+            container = this.get('container');
+
+        subscriptions.push(
+            this.on('pageChange', this._onPageChange),
+            container.delegate('click', this._onControlClick, '.' + classNames.control, this),
+            container.delegate('click', this._onPageClick, '.' + classNames.page, this),
+            container.delegate('change', this._onPageInputChange, '.' + classNames.pageInput, this),
+            container.delegate('change', this._onPageSelectChange, '.' + classNames.pageSelect, this),
+            container.delegate('change', this._onItemsSelectChange, '.' + classNames.itemsSelect, this)
+        );
+
+        this._subs = subscriptions;
+
+        return this;
+    },
+
+    generateClassNames: function () {
+        var _getClassName = this._getClassName,
+            classNames = {
+                container: _getClassName('container'),
+                controls: _getClassName('controls'),
+                control: _getClassName('control'),
+                pages: _getClassName('pages'),
+                page: _getClassName('page'),
+                pageSelect: _getClassName('page','select'),
+                pageInput: _getClassName('page', 'input'),
+                itemsSelect: _getClassName('items','select')
+            };
+
+        this.classNames = classNames;
+    },
+
+    /////////////////////////////////
+    // E V E N T   C A L L B A C K S
+    /////////////////////////////////
+    _onPageChange: function(e) {
+        console.log('_pageChange');
+    },
+
+    _onControlClick: function() {
+        console.log('control was clicked');
+    },
+
+    _onPageClick: function() {
+        console.log('page was clicked');
+    },
+
+    _onPageInputChange: function() {
+        console.log('page input was changed');
+    },
+
+    _onPageSelectChange: function() {
+        console.log('page select was changed');
+    },
+
+    _onItemsSelectChange: function() {
+        console.log('items select was changed');
+    },
+
+
     /////////////////////
     // P R O T E C T E D
     /////////////////////
@@ -48,13 +110,9 @@ var Paginator = Y.Base.create('paginator', Y.View, [Y.Paginator.Core, Y.Paginato
         if (arguments && arguments.length > 0) {
             args = Array.prototype.slice.call(arguments);
         }
-        args.unshift('paginator');
+        args.unshift(this.prefix || 'paginator');
 
         return Y.ClassNameManager.getClassName.apply(this, args);
-    },
-
-    _pageChange: function(e) {
-        console.log('_pageChange');
     },
 
     _defContainerFn: function () {
