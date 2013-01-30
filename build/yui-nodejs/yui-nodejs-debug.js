a8b8636706bb85905ea52c7f1d31db42802d5b54
Dav Glass <davglass@gmail.com>
2013-01-30 15:04:49 -0600

    Seed and Loader build with new Gallery tag

--- build/yui-nodejs/yui-nodejs-debug.js
+++ build/yui-nodejs/yui-nodejs-debug.js
@@ -4977,7 +4977,7 @@ if (!YUI.Env[Y.version]) {
             BUILD = '/build/',
             ROOT = VERSION + BUILD,
             CDN_BASE = Y.Env.base,
-            GALLERY_VERSION = 'gallery-2013.01.23-21-59',
+            GALLERY_VERSION = 'gallery-2013.01.30-21-00',
             TNT = '2in3',
             TNT_VERSION = '4',
             YUI2_VERSION = '2.9.0',
