17e1630aee968e61983b3d6b79fcd3741a561074
dpnishant <nishant.dp@gmail.com>
2013-02-08 15:18:45 +0530

    Added Unit Test Cases for JS and URI methods
    Added Unit Test Cases for JS and URI methods:
    string escape tests and non-string to string coercing tests

--- src/escape/tests/unit/assets/escape-test.js
+++ src/escape/tests/unit/assets/escape-test.js
@@ -42,7 +42,40 @@ Y.Test.Runner.add(new Y.Test.Case({
 
     'regexp() should be an alias for regex()': function () {
         Assert.areSame(Escape.regex, Escape.regexp);
+    },
+    
+    'js() should escape JS characters': function () {
+        Assert.areSame('\\x26\\x3C\\x3E\\x22\\x27\\x2F\\x60', Escape.js('&<>"\'/`'));
+        Assert.areSame('\\x22\\x20onmouseover\\x3Dalert\\x281\\x29', Escape.js('" onmouseover=alert(1)'));
+        Assert.areSame('alert', Escape.js('alert'));
+        Assert.areSame('34234234', Escape.js('34234234'));
+        Assert.areSame('foo', Escape.js('foo'));
+        Assert.areSame('\\u0190\\u0122\\u016A\\u019B', Escape.js(String.fromCharCode(400,290,362,411)));
+    },
+    
+    'js() should coerce non-strings to strings': function () {
+        Assert.areSame('1', Escape.js(1));
+        Assert.areSame('false', Escape.js(false));
+        Assert.areSame('null', Escape.js(null));
+        Assert.areSame('undefined', Escape.js());
+    },
+    
+    'uri() should escape uri': function () {
+        Assert.areSame('/article/show.php', Escape.uri('/article/show.php'));
+        Assert.areSame('http://yuilibrary.com', Escape.uri('http://yuilibrary.com'));
+        Assert.areSame('https://yuilibrary.com', Escape.uri('https://yuilibrary.com'));
+        Assert.areSame('http://javascript:alert(1)', Escape.uri('javascript:alert(1)'));
+        Assert.areSame('http://data:text/html;<html>', Escape.uri('data:text/html;<html>'));
+        Assert.areSame('%2Farticle%2Fshow.php', Escape.uri('%2Farticle%2Fshow.php'));
+    },
+    
+    'uri() should coerce non-strings to strings': function () {
+        Assert.areSame('http://1', Escape.uri(1));
+        Assert.areSame('http://false', Escape.uri(false));
+        Assert.areSame('http://null', Escape.uri(null));
+        Assert.areSame('http://undefined', Escape.uri());
     }
+
 }));
 
 }, '@VERSION@', {requires:['escape', 'test']});
