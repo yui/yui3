06d49b0408883afb53e567687280fdfe59e3093b
tripp <tripp.bridges@yahoo.com>
2013-03-04 10:36:15 -0800

    Capitalized variables referencing classes in graphics for jslint.

--- src/graphics/js/SVGGraphic.js
+++ src/graphics/js/SVGGraphic.js
@@ -440,8 +440,8 @@ Y.extend(SVGGraphic, Y.GraphicBase, {
         {
             cfg.visible = false;
         }
-        var shapeClass = this._getShapeClass(cfg.type),
-            shape = new shapeClass(cfg);
+        var ShapeClass = this._getShapeClass(cfg.type),
+            shape = new ShapeClass(cfg);
         this._appendShape(shape);
         return shape;
     },
