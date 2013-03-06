dd585e6574997e7f18efe6c0c6d095fbb0d9932a
tripp <tripp.bridges@yahoo.com>
2013-03-05 19:38:34 -0800

    Potential fix for trac2533149. Testing indicates this will fix all parse errors in android 2.3.4.

--- src/json/js/parse.js
+++ src/json/js/parse.js
@@ -1,5 +1,8 @@
 var _JSON = Y.config.global.JSON;
 
 Y.namespace('JSON').parse = function () {
+    if(typeof arguments[0] !== 'string') {
+        arguments[0] = arguments[0] + '';
+    }
     return _JSON.parse.apply(_JSON, arguments);
 };
