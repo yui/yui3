dbde99362897e91ee681a79e960742cf48fa30e3
Jeroen Versteeg <jeroen@asystance.nl>
2013-03-27 16:21:37 +0100

    Made `Y.WidgetPosition` functions `move` and `syncXY` chainable Bonus: added test for `syncXY`

--- src/widget-position/tests/unit/assets/widget-position-test.js
+++ src/widget-position/tests/unit/assets/widget-position-test.js
@@ -29,13 +29,17 @@ suite.add(new Y.Test.Case({
     },
 
     '`move()` should move the widget to the new `xy`': function () {
+        var returnVal;
+
         this.widget = new TestWidget();
         this.widget.render('#test');
 
         Assert.areSame(0, this.widget.get('x'), '`x` is not 0.');
         Assert.areSame(0, this.widget.get('y'), '`y` is not 0.');
 
-        this.widget.move(10, 10);
+        returnVal = this.widget.move(10, 10);
+
+        Assert.areSame(this.widget, returnVal, 'method is not chainable');
 
         Assert.areSame(10, this.widget.get('x'), '`x` is not 0.');
         Assert.areSame(10, this.widget.get('y'), '`y` is not 0.');
@@ -45,6 +49,23 @@ suite.add(new Y.Test.Case({
 
         Assert.areSame(20, this.widget.get('x'), '`x` is not 0.');
         Assert.areSame(20, this.widget.get('y'), '`y` is not 0.');
+    },
+
+    '`syncXY` should synchronize x, y, xy with boundingBox': function () {
+        var bb,
+            returnVal;
+
+        this.widget = new TestWidget();
+        this.widget.render('#test');
+        bb = this.widget.get('boundingBox');
+
+        bb.setXY([10, 20]);
+        returnVal = this.widget.syncXY();
+
+        Assert.areSame(this.widget, returnVal, 'method is not chainable');
+
+        Assert.areSame(bb.getX(), this.widget.get('x'), '`x` is not in sync with boundingBox.');
+        Assert.areSame(bb.getY(), this.widget.get('y'), '`y` is not in sync with boundingBox.');
     }
 }));
 
