9cd1004385748224810676e23310f803f69338f4
Ryan Grove <ryan@wonko.com>
2013-04-29 16:52:46 -0700

    Remove Flash xdr tests.

--- src/io/tests/unit/js/transport-tests.js
+++ src/io/tests/unit/js/transport-tests.js
@@ -2,15 +2,7 @@ YUI.add('transport-tests', function(Y) {
 
     var suite = new Y.Test.Suite('Transport Tests');
 
-    Y.io.transport( { id:'flash', src:'../../../build/io-xdr/io.swf' } );
     Y.IO.transports.iframe = function() { return {}; };
-
-    suite.add(new Y.Test.Case({
-        name: 'Flash Transport Test',
-        'test': function() {
-            Y.Assert.isObject(Y.IO.transports.flash);
-        }
-    }));
 
     suite.add(new Y.Test.Case({
         name: 'iframe Transport Test',
