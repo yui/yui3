820407fab2a369d3aef814c52957d8e8a61baba5
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2013-05-10 10:47:37 -0300

    Restore batch and deprecate it
    Follow the correct deprecation policy. Change the mentions to batch in
    the User Guide.

--- src/promise/HISTORY.md
+++ src/promise/HISTORY.md
@@ -7,6 +7,7 @@ Promise Change History
 * Split the module into promise-core and promise-extras
 * then() no longer returns an instance of this.constructor
 * Added combinators, factories and extra methods
+* [!] Deprecated Y.batch() in favor of Y.Promise.every()
 * The first function received as parameter of the Promise init function is now
   a reference to resolve() instead of accept()
 * Changing the use of fulfill() to accept() to follow the naming of DOMFuture
--- src/promise/js/extras/batch.js
+++ src/promise/js/extras/batch.js
@@ -11,5 +11,35 @@ it will be wrapped in a new promise.
 @deprecated @SINCE@
 **/
 Y.batch = function () {
-    return Promise.every(slice.call(arguments));
+    Y.log('batch() was deprecated in YUI @SINCE@. Use Promise.every()', 'warn');
+
+    var funcs     = slice.call(arguments),
+        remaining = funcs.length,
+        i         = 0,
+        length    = funcs.length,
+        results   = [];
+
+    return new Y.Promise(function (fulfill, reject) {
+        var allDone = this;
+
+        function oneDone(index) {
+            return function (value) {
+                results[index] = value;
+
+                remaining--;
+
+                if (!remaining && allDone.getStatus() !== 'rejected') {
+                    fulfill(results);
+                }
+            };
+        }
+
+        if (length < 1) {
+            return fulfill(results);
+        }
+
+        for (; i < length; i++) {
+            Y.when(funcs[i], oneDone(i), reject);
+        }
+    });
 };
