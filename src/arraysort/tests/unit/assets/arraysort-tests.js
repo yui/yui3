d84970aa8f39649967ce5fcb10fb4a670787042e
Ryan Grove <ryan@wonko.com>
2013-05-16 13:30:10 -0700

    Fix lint errors in ArraySort tests.

--- src/arraysort/tests/unit/assets/arraysort-tests.js
+++ src/arraysort/tests/unit/assets/arraysort-tests.js
@@ -1,6 +1,5 @@
 YUI.add('arraysort-tests', function(Y) {
-    var Assert      = Y.Assert,
-        ArrayAssert = Y.ArrayAssert,
+    var ArrayAssert = Y.ArrayAssert,
 
         suite = new Y.Test.Suite('ArraySort');
 
@@ -9,19 +8,19 @@ YUI.add('arraysort-tests', function(Y) {
 
         'should compare numbers': function () {
             var array = [2,1,3,5,4];
-            array.sort(Y.ArraySort.compare)
+            array.sort(Y.ArraySort.compare);
             ArrayAssert.itemsAreSame([1,2,3,4,5], array, "Expected sorted numbers.");
         },
 
         'should compare strings': function () {
             var array = ["caa", "baa", "bba", "aba", "cba", "aaa", "abc"];
-            array.sort(Y.ArraySort.compare)
+            array.sort(Y.ArraySort.compare);
             ArrayAssert.itemsAreSame(["aaa","aba","abc","baa","bba","caa","cba"], array, "Expected sorted strings.");
         },
 
         'should compare mixed alpha and numeric strings': function() {
             var array = ["attic", "Aardvark", "1", "0", "Zoo", "zebra"];
-            array.sort(Y.ArraySort.compare)
+            array.sort(Y.ArraySort.compare);
             ArrayAssert.itemsAreSame(["0", "1", "Aardvark","attic","zebra","Zoo"], array, "Expected sorted mixed strings.");
         }
     }));
