b9e1c4266b3a57923eedb801afc6d8b91a8a9596
Andrew Wooldridge <triptych@gmail.com>
2013-05-29 16:02:59 -0700

    Build files - changed files due to Profiler being removed

--- build/yui-nodejs/yui-nodejs-debug.js
+++ build/yui-nodejs/yui-nodejs-debug.js
@@ -10028,11 +10028,6 @@ Y.mix(YUI.Env[Y.version].modules, {
             "pluginhost-base"
         ]
     },
-    "profiler": {
-        "requires": [
-            "yui-base"
-        ]
-    },
     "promise": {
         "requires": [
             "timers"
@@ -10828,7 +10823,7 @@ Y.mix(YUI.Env[Y.version].modules, {
         ]
     }
 });
-YUI.Env[Y.version].md5 = '3d12f1513239db8e2ba60dc472a4dcaf';
+YUI.Env[Y.version].md5 = 'd7ced98e3907d3c3c0655a93c6ac6867';
 
 
 }, '@VERSION@', {"requires": ["loader-base"]});
