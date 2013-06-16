3b30ccf3587e4641fc3425ec94faa8f8d4a18dc4
Jeroen Versteeg <jeroen@asystance.nl>
2013-06-16 13:14:37 +0200

    Removed unused lang from calendar/calendar.js calendar-base uses its lang, calendar had unused copies of those files

--- src/calendar/lang/calendar_nb-NO.js
+++
@@ -1,6 +0,0 @@
-{
-    weekdays : ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"],
-    very_short_weekdays : ["Sø", "Ma", "Ti", "On", "To", "Fr", "Lø"],
-    first_weekday : 1,
-    weekends : [0,6]
-}
