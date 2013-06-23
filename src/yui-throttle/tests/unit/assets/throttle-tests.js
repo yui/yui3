4322199a6234735001be352c348df15178c06113
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2013-06-23 15:33:03 -0300

    Clean up throttle tests

--- src/yui-throttle/tests/unit/assets/throttle-tests.js
+++ src/yui-throttle/tests/unit/assets/throttle-tests.js
@@ -4,148 +4,126 @@ YUI.add('throttle-tests', function(Y) {
 
     var suite = new Y.Test.Suite("Throttle");
 
-    var testCase = new Y.Test.Case({
+    suite.add(new Y.Test.Case({
         name: "Throttle Tests",
 
-        setUp: function () {
-            var time = Y.Lang.now();
+        setUp : function() {
+            var test = this;
 
-            this._now = Y.Lang.now;
+            // Start with real time to use numbers closer to reality
+            test.tNow = Y.Lang.now();
+            test.tDelta = 1;
 
-            Y.Lang.now = function () {
-                return ++time;
+            test.originalNow = Y.Lang.now;
+
+            // Mock. First call will be at real time,
+            // and increment in tDelta chunks.
+            // So by default we're mocking/ invocation every 1ms,
+            // but the tests can mess with it as they need.
+            Y.Lang.now = function() {
+                var now = test.tNow;
+                test.tNow += test.tDelta;
+
+                return now;
             };
         },
+
         tearDown: function () {
-            Y.Lang.now = this._now;
+            Y.Lang.now = this.originalNow;
         },
 
         test_throttle: function() {
             var counter = 0,
                 out = 0,
-                i = 0, fn;
+                i = 0, throttledFn;
+
+            function count() {
+                counter++;
+            }
 
             // Y.Lang.now() returns always previousTime + 1, so if we throttle
             // the function by 75ms and we call the returned function 76 times
             // the throttled function should be called once
-            fn = Y.throttle(function() {
-                counter++;
-            }, 75);
+            throttledFn = Y.throttle(count, 75);
 
             // Calling the result function 100 times means the throttled function
             // will be called once. So `out` will be 100 and `counter` will be 1
             for (i; i < 100; i++) {
                 out++;
-                fn();
+                throttledFn();
             }
             
-            var fn1 = function() {
-                counter++;
-            };
-
-            fn1.name = 'foo';
-
-            fn = Y.throttle(fn1, 10);
-
-            Assert.isFunction(fn, 'Y.Throttle failed to return a function');
-            Assert.areNotSame(fn1, fn, 'Y.Throttle failed to return a new function');
-            Assert.isTrue((counter < out), 'Y.Throttle did not throttle the function call');
-            Assert.isTrue((counter > 0), 'Y.Throttle did not throttle the function call (no exec)');
+            Assert.isFunction(throttledFn, 'Y.throttle failed to return a function');
+            Assert.areNotSame(count, throttledFn, 'Y.throttle failed to return a new function');
+            Assert.isTrue(counter < out, 'Y.throttle did not throttle the function call');
+            Assert.isTrue(counter > 0, 'Y.throttle did not throttle the function call (no exec)');
+            Assert.areSame(1, counter, 'throttled function was executed more than expected');
         },
         test_throttle_negative: function() {
             var counter = 0,
                 out = 0,
-                i = 0, fn;
+                i = 0, throttledFn;
 
-            fn = Y.throttle(function() {
+            function count() {
                 counter++;
-            }, -1);
+            }
 
-            for (i; i< 3500; i++) {
+            throttledFn = Y.throttle(count, -1);
+
+            for (i; i < 100; i++) {
                 out++;
-                fn();
+                throttledFn();
             }
             
-            var fn1 = function() {
-                counter++;
-            };
-
-            fn1.name = 'foo';
-
-            fn = Y.throttle(fn1, 10);
-
-            Assert.isFunction(fn, 'Y.Throttle failed to return a function');
-            Assert.areNotSame(fn1, fn, 'Y.Throttle failed to return a new function');
-            Assert.areEqual(out, counter, 'Y.Throttle DID throttle the function call');
+            Assert.isFunction(throttledFn, 'Y.throttle failed to return a function');
+            Assert.areNotSame(count, throttledFn, 'Y.throttle failed to return a new function');
+            Assert.areEqual(out, counter, 'Y.throttle DID throttle the function call');
         },
         'test no throttle time': function() {
             var counter = 0,
-                out = 0,
                 i = 0, fn;
 
-            fn = Y.throttle(function() {
+            fn = Y.throttle(function () {
                 counter++;
             });
 
-            // The default throttle threshold is 150 ms, so calling the result
-            // function 100 times will never call the throttled function because
-            // of how the mock Y.Lang.now() works
-            for (i; i< 100; i++) {
-                out++;
-                fn();
-            }
+            // Call twice, once after the default throttle time of 150ms
+            this.tDelta = 150;
+            fn();
+            this.tDelta = 1;
+            fn();
+
+            Assert.isFalse(counter < 1, 'throttled function with no throttle time never called');
+            Assert.isFalse(counter > 1, 'throttled function with no throttle time called more than once');
+        },
+        'test throttled function never called after throttle time': function () {
+            var counter = 0,
+                fn = Y.throttle(function () {
+                    counter++;
+                }, 10);
+
+            // call once before the 10ms threshold (as if it were only 5ms)
+            this.tDelta = 5;
+            fn();
             
-            var fn1 = function() {
-                counter++;
+            Assert.areEqual(0, counter, 'throttled function called before throttle time');
+        },
+        '`this` is not modified in throttled function': function () {
+            var obj = {
+                fn: Y.throttle(function () {
+                    Assert.areSame(obj, this, 'wrong value for `this` in function with canceled throttle');
+                }, 1)
             };
 
-            fn1.name = 'foo';
-
-            fn = Y.throttle(fn1, 10);
-
-            Assert.isFunction(fn, 'Y.Throttle failed to return a function');
-            Assert.areNotSame(fn1, fn, 'Y.Throttle failed to return a new function');
-            Assert.areEqual(0, counter, 'Y.Throttle DID NOT throttle the function call');
+            // Adjust delta to be larger the throttle time so the function is called
+            this.tDelta = 10;
+            obj.fn();
         },
-        'test `this` in throttled function': function () {
+        'test `this` in function with disabled throttle': function () {
             var obj = {
                 fn: Y.throttle(function () {
                     Assert.areSame(obj, this, 'wrong value for `this` in function with canceled throttle');
                 }, -1)
-            };
-
-            obj.fn();
-        }
-
-    });
-    
-    suite.add(testCase);
-
-    // Test the case when the timeout is > -1 by rewriting Lang.now() so it
-    // does not need setTimeout to fake the behavior. This avoids flaky results
-    // when testing in slow computers or VMs
-    suite.add(new Y.Test.Case({
-        name: 'Throttle tests with mock Lang.now()',
-
-        setUp: function () {
-            var time = Y.Lang.now();
-
-            this._now = Y.Lang.now;
-
-            Y.Lang.now = function () {
-                return time += 50;
-            };
-        },
-
-        tearDown: function () {
-            Y.Lang.now = this._now;
-        },
-
-        '`this` is not modified': function () {
-            var obj = {
-                fn: Y.throttle(function () {
-                    Assert.areSame(obj, this, 'wrong value for `this` in function with canceled throttle');
-                }, 10)
             };
 
             obj.fn();
