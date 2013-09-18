76314d104578d62d3af84b3645f82fa2fd22d5fc
Eugene Kashida <ekashida@gmail.com>
2013-09-18 12:08:30 -0700

    YUI and Loader build for latest Gallery build

--- build/loader/loader.js
+++ build/loader/loader.js
@@ -11,7 +11,7 @@ YUI.add('loader-base', function (Y, NAME) {
         BUILD = '/build/',
         ROOT = VERSION + '/',
         CDN_BASE = Y.Env.base,
-        GALLERY_VERSION = 'gallery-2013.09.12-21-28',
+        GALLERY_VERSION = 'gallery-2013.09.18-18-49',
         TNT = '2in3',
         TNT_VERSION = '4',
         YUI2_VERSION = '2.9.0',
