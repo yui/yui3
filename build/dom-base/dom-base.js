938651d47cecb4cd2702f29d473c9f81e7a164ee
tripp <tripp.bridges@yahoo.com>
2013-10-07 13:22:18 -0700

    Add dom-base build files.

--- build/dom-base/dom-base.js
+++ build/dom-base/dom-base.js
@@ -314,7 +314,9 @@ var re_tag = /<([a-z]+)/i,
     re_tbody = /(?:\/(?:thead|tfoot|tbody|caption|col|colgroup)>)+\s*<tbody/,
 
     TABLE_OPEN = '<table>',
-    TABLE_CLOSE = '</table>';
+    TABLE_CLOSE = '</table>', 
+    
+    selectedIndex;
 
 Y.mix(Y.DOM, {
     _fragClones: {},
@@ -382,8 +384,7 @@ Y.mix(Y.DOM, {
             create = Y_DOM._create,
             custom = creators,
             ret = null,
-            creator,
-            tag, nodes;
+            creator, tag, node, nodes;
 
         if (html != undefined) { // not undefined or null
             if (m && m[1]) {
@@ -394,16 +395,19 @@ Y.mix(Y.DOM, {
                     tag = creator;
                 }
             }
-
-            nodes = create(html, doc, tag).childNodes;
+            
+            node = create(html, doc, tag);
+            nodes = node.childNodes;
 
             if (nodes.length === 1) { // return single node, breaking parentNode ref from "fragment"
-                ret = nodes[0].parentNode.removeChild(nodes[0]);
+                ret = node.removeChild(nodes[0]);
             } else if (nodes[0] && nodes[0].className === 'yui3-big-dummy') { // using dummy node to preserve some attributes (e.g. OPTION not selected)
+                selectedIndex = node.selectedIndex;
+                
                 if (nodes.length === 2) {
                     ret = nodes[0].nextSibling;
                 } else {
-                    nodes[0].parentNode.removeChild(nodes[0]);
+                    node.removeChild(nodes[0]);
                     ret = Y_DOM._nl2frag(nodes, doc);
                 }
             } else { // return multiple nodes as a fragment
@@ -513,6 +517,12 @@ Y.mix(Y.DOM, {
             node.appendChild(newNode);
         }
 
+        // `select` elements are the only elements with `selectedIndex`.
+        // Don't grab the dummy `option` element's `selectedIndex`.
+        if (node.nodeName == "SELECT" && selectedIndex > 0) {
+            node.selectedIndex = selectedIndex - 1;
+        }
+        
         return ret;
     },
 
