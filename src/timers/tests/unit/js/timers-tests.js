fd066f81462203694be4fc674744eeee710822ba
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2013-11-11 14:45:31 -0300

    [timers] Test asynchrony of Y.soon

--- src/timers/tests/unit/js/timers-tests.js
+++ src/timers/tests/unit/js/timers-tests.js
@@ -1,6 +1,8 @@
 YUI.add('timers-tests', function (Y) {
     'use strict';
 
+    var ArrayAssert = Y.Test.ArrayAssert;
+
     var suite = new Y.Test.Suite('timers');
 
     suite.add(new Y.Test.Case({
@@ -9,6 +11,21 @@ YUI.add('timers-tests', function (Y) {
             Y.Assert.isFunction(Y.soon, 'Y.soon should be a function.');
             Y.Assert.isFunction(Y.soon._asynchronizer, 'Y.soon._asynchronizer should be a function.');
             Y.Assert.isString(Y.soon._impl, 'Y.soon._impl should be a string.');
+        },
+        'enforce asynchronicity': function () {
+            var test = this,
+                reference + [];
+
+            reference.push('a');
+            Y.soon(function () {
+                reference.push('c');
+                test.resume(function () {
+                    ArrayAssert.itemsAreSame(['a', 'b', 'c'], reference, 'Y.soon should not execute synchronously');
+                });
+            });
+            reference.push('b');
+
+            test.wait();
         },
         'test:002-asyncCallbackFunction': function () {
             var count = 0,
