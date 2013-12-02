8ea86825dd731ddd7f32e815c815720b79beaf0c
Ezequiel <ezequiel@yahoo-inc.com>
2013-12-02 15:17:42 -0800

    WidgetModality: Build.

--- build/widget-modality/widget-modality-debug.js
+++ build/widget-modality/widget-modality-debug.js
@@ -433,7 +433,7 @@ var WIDGET       = 'widget',
                 }
 
                 else {
-                    Y.Log('focusOn ATTR Error: The event with name "'+o.ev+'" could not be attached.');
+                    Y.log('focusOn ATTR Error: The event with name "'+o.ev+'" could not be attached.');
                 }
 
             }
