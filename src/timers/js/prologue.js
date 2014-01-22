b5f9445c7c2bcb7617bed27869200e523fa4d6fe
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2014-01-22 15:08:16 -0300

    [timers] Mention asap.js in the module's description

--- src/timers/js/prologue.js
+++ src/timers/js/prologue.js
@@ -1,10 +1,13 @@
 /**
- * Provides utilities for timed asynchronous callback execution.
- * Y.soon is a setImmediate/process.nextTick/setTimeout wrapper.
- *
- * @module timers
- * @author Steven Olmsted
- */
+Provides utilities for timed asynchronous callback execution.
+Y.soon is a setImmediate/process.nextTick/setTimeout wrapper.
+
+This module includes [asap.js](https://github.com/kriskowal/asap) for scheduling
+asynchronous tasks.
+
+@module timers
+@author Steven Olmsted
+**/
 var module = {},
     global = Y.config.global;
 
