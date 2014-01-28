f3997c6306b5f308171a8f636a5db0e06465b388
tripp <tripp.bridges@yahoo.com>
2014-01-27 16:54:07 -0800

    YUI and Loader build for latest Gallery build

--- build/yui/yui.js
+++ build/yui/yui.js
@@ -5852,7 +5852,7 @@ YUI.add('loader-base', function (Y, NAME) {
         BUILD = '/build/',
         ROOT = VERSION + '/',
         CDN_BASE = Y.Env.base,
-        GALLERY_VERSION = 'gallery-2014.01.22-18-38',
+        GALLERY_VERSION = 'gallery-2014.01.28-00-45',
         TNT = '2in3',
         TNT_VERSION = '4',
         YUI2_VERSION = '2.9.0',
