d2b425bf223ef478204af3f90f8051bc04fce229
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2014-02-11 15:07:00 -0300

    [timers] Explain how the prologue works

--- src/timers/js/prologue.js
+++ src/timers/js/prologue.js
@@ -8,9 +8,13 @@ asynchronous tasks.
 @module timers
 @author Steven Olmsted
 **/
+
+// Hack. asap.js is written as a Node module and expects require, module and
+// global to be available in the module's scope.
 var module = {},
     global = Y.config.global;
 
+// `asap` only requires a `queue` module that is bundled into this same file.
 function require(mod) {
     return Queue;
 }
