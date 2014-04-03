87558dc091849a54aa52cbc4c46d4557f702c4b0
Maira Bello <maira.araujo@liferay.com>
2014-04-03 10:09:29 -0300

    Fix #1731: Y.Calendar.selectDates fails when passed the maximumDate with minutes/seconds
    Fixing the issue explained in #1731, which causes selectDates to fail when the user passes the maximumDate with minutes or seconds to it. The calendar code is already resetting the hours part of the date's time, but this diff resets everything to make sure that date selection doesn't take the date's time into account anymore.

--- src/calendar/js/calendar-base.js
+++ src/calendar/js/calendar-base.js
@@ -409,7 +409,7 @@ Y.CalendarBase = Y.extend( CalendarBase, Y.Widget, {
      * @private
      */
     _addDateToSelection : function (oDate, index) {
-        oDate.setHours(12);
+        oDate = this._normalizeTime(oDate);
 
         if (this._canBeSelected(oDate)) {
 
