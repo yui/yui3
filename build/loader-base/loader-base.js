e0026c207750bab547ece843a0b870ac06388135
tripp <tripp.bridges@yahoo.com>
2014-05-29 09:12:09 -0700

    YUI and Loader build for latest Gallery build
    Conflicts:
    	build/loader-base/loader-base-coverage.js
    Resolve conflicts with updated gallery tag.

--- build/loader-base/loader-base.js
+++ build/loader-base/loader-base.js
@@ -11,7 +11,7 @@ YUI.add('loader-base', function (Y, NAME) {
         BUILD = '/build/',
         ROOT = VERSION + '/',
         CDN_BASE = Y.Env.base,
-        GALLERY_VERSION = 'gallery-2014.04.02-20-01',
+        GALLERY_VERSION = 'gallery-2014.05.29-15-46',
         TNT = '2in3',
         TNT_VERSION = '4',
         YUI2_VERSION = '2.9.0',
