ef1f227d83049541a8bfa0660070e3d6acf66997
Ryuichi Okumura <okuryu@okuryu.com>
2014-06-14 22:48:27 +0900

    [timers] Specify @module and @submodule for API docs
    It needs to shows the correct provided module name.
    
    Fix #1669.

--- src/timers/js/epilogue.js
+++ src/timers/js/epilogue.js
@@ -1,4 +1,9 @@
 /**
+@module yui
+@submodule timers
+**/
+
+/**
 Y.soon accepts a callback function.  The callback function will be called
 once in a future turn of the JavaScript event loop.  If the function
 requires a specific execution context or arguments, wrap it with Y.bind.
