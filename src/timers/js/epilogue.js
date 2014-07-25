441ae847287afced4b46abe31ca82e5b8e3b58c1
Ryuichi Okumura <okuryu@okuryu.com>
2014-07-25 22:16:24 +0900

    [timers] prologue.js uses @main syntax

--- src/timers/js/epilogue.js
+++ src/timers/js/epilogue.js
@@ -1,12 +1,5 @@
 /**
-Provides utilities for timed asynchronous callback execution.
-Y.soon is a setImmediate/process.nextTick/setTimeout wrapper.
-
-This module includes [asap.js](https://github.com/kriskowal/asap) for scheduling
-asynchronous tasks.
-
 @module timers
-@author Steven Olmsted
 **/
 
 /**
--- src/timers/js/prologue.js
+++ src/timers/js/prologue.js
@@ -1,3 +1,15 @@
+/**
+Provides utilities for timed asynchronous callback execution.
+Y.soon is a setImmediate/process.nextTick/setTimeout wrapper.
+
+This module includes [asap.js](https://github.com/kriskowal/asap) for scheduling
+asynchronous tasks.
+
+@module timers
+@main timers
+@author Steven Olmsted
+**/
+
 // Hack. asap.js is written as a Node module and expects require, module and
 // global to be available in the module's scope.
 var module = {},
