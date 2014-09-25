d6255b1b2cb16f32658716659be0e0ff2e7b4da5
tripp <tripp.bridges@yahoo.com>
2014-09-24 23:33:40 -0700

    Address issue in which slashes weren't being appropriately trimmed for pathogen encoded combos.
    	modified:   js/loader-pathogen-combohandler.js

--- src/loader/js/loader-pathogen-combohandler.js
+++ src/loader/js/loader-pathogen-combohandler.js
@@ -171,10 +171,10 @@ Y.mix(Y.Loader.prototype, {
                 key = meta.root;
 
                 // Trim '/' from both ends.
-                if (key[0] === '/') {
+                if (key.indexOf('/') === 0) {
                     key = key.slice(1);
                 }
-                if (key[key.length - 1] === '/') {
+                if (key.lastIndexOf('/') === key.length - 1) {
                     key = key.slice(0, -1);
                 }
 
@@ -191,7 +191,7 @@ Y.mix(Y.Loader.prototype, {
                     name = meta.root + name;
                 }
 
-                if (name[0] === '/') {
+                if (name.indexOf('/') === 0) {
                     name = name.slice(1);
                 }
 
