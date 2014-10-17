0b2a33bef293115caa2d6facbf950e1ae199891d
tripp <tripp.bridges@yahoo.com>
2014-10-17 15:55:06 -0700

    Roll back pr 1883. It breaks dd in ie, 7. Fixes #1971
    	modified:   js/drag.js

--- src/dd/js/drag.js
+++ src/dd/js/drag.js
@@ -614,12 +614,12 @@
         */
         _invalids: null,
         /**
-        * A private hash of the default invalid selector strings: {'textarea': true, 'input': true, 'a': true, 'button': true, 'select': true, '[contenteditable]': true}
+        * A private hash of the default invalid selector strings: {'textarea': true, 'input': true, 'a': true, 'button': true, 'select': true}
         * @private
         * @property _invalidsDefault
         * @type {Object}
         */
-        _invalidsDefault: {'textarea': true, 'input': true, 'a': true, 'button': true, 'select': true, '[contenteditable]': true },
+        _invalidsDefault: {'textarea': true, 'input': true, 'a': true, 'button': true, 'select': true },
         /**
         * Private flag to see if the drag threshhold was met
         * @private
